ee9d0313713219bf5f3e267cd529826c010002d4
CSHTML5 <contact@cshtml5.com>
2019-04-12 19:50:34 +0200

    Add "BackEase" to work-in-progress files

---
+++ src/CSHTML5.Runtime/Windows.UI.Xaml.Media.Animation/WORKINPROGRESS/BackEase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+#if MIGRATION
+namespace System.Windows.Media.Animation
+#else
+namespace Windows.UI.Xaml.Media.Animation
+#endif
+{
+
+#if WORKINPROGRESS
+    #region Not Supported yet
+
+    /// <summary>
+    /// This class implements an easing function that backs up before going to the destination.
+    /// </summary>
+    public class BackEase : EasingFunctionBase
+    {
+        public double Amplitude { get; set; }
+    }
+
+    #endregion
+#endif
+}
