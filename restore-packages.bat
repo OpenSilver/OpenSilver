3aa7202bbc4ee663d6a0c8553c2debbf8c2e0658
Sylvain Maillard <maillard.sy@gmail.com>
2019-04-15 19:09:45 +0200

    Update NuGet package from "2.0.0-alpha25-040" to "2.0.0-alpha26-041"

--- restore-packages.bat
+++ restore-packages.bat
@@ -1,3 +1,3 @@
-build\nuget\nuget.exe install CSHTML5 -Version 2.0.0-alpha25-040 -OutputDirectory src\packages
-build\nuget\nuget.exe install CSHTML5.Migration -Version 2.0.0-alpha25-040 -OutputDirectory src\packages
+build\nuget\nuget.exe install CSHTML5 -Version 2.0.0-alpha26-041 -OutputDirectory src\packages
+build\nuget\nuget.exe install CSHTML5.Migration -Version 2.0.0-alpha26-041 -OutputDirectory src\packages
 pause
