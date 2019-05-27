8488de25c973476912991dd1a91c4bf8a0985b62
JulRu77 <JulRu77@users.noreply.github.com>
2019-05-27 12:57:43 +0200

    Revert "Add the properties "XNamespace.Xmlns" and "XNamespace.Xml"."
    This reverts commit f286e4eca4fe3da9f403902cf6f81ffab57e4ad9.

--- src/CSHTML5.Runtime/System.Xml.Linq/XNamespace.cs
+++ src/CSHTML5.Runtime/System.Xml.Linq/XNamespace.cs
@@ -1,4 +1,5 @@
-﻿//===============================================================================
+﻿
+//===============================================================================
 //
 //  IMPORTANT NOTICE, PLEASE READ CAREFULLY:
 //
@@ -13,6 +14,14 @@
 //  => Copyright 2019 Userware/CSHTML5. This code is part of the CSHTML5 product (cshtml5.com).
 //
 //===============================================================================
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace System.Xml.Linq
 {
@@ -31,31 +40,6 @@ namespace System.Xml.Linq
         public static XName operator +(XNamespace ns, string localName)
         {
             return ns.GetName(localName);
-        }
-
-        internal const string xmlPrefixNamespace = "http://www.w3.org/XML/1998/namespace";
-        internal const string xmlnsPrefixNamespace = "http://www.w3.org/2000/xmlns/";
-
-        /// <summary>
-        /// Gets the XNamespace object that corresponds to the xml uri (http://www.w3.org/XML/1998/namespace).
-        /// </summary>
-        public static XNamespace Xml
-        {
-            get
-            {
-                return XNamespace.Get(xmlPrefixNamespace);
-            }
-        }
-
-        /// <summary>
-        /// Gets the XNamespace object that corresponds to the xmlns uri (http://www.w3.org/2000/xmlns/).
-        /// </summary>
-        public static XNamespace Xmlns
-        {
-            get
-            {
-                return XNamespace.Get(xmlnsPrefixNamespace);
-            }
         }
 
         /// <summary>
