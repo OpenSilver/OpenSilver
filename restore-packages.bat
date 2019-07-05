1739d95f244444037a2b00ad7d9fdca6feb43997
CSHTML5 <contact@cshtml5.com>
2019-07-05 16:20:31 +0200

    Update NuGet package reference from CSHTML5 version "2.0.0-alpha35-050" to "2.0.0-alpha36-051"

--- restore-packages.bat
+++ restore-packages.bat
@@ -1,4 +1,4 @@
-build\nuget\nuget.exe install CSHTML5 -Version 2.0.0-alpha35-050 -OutputDirectory src\packages
-build\nuget\nuget.exe install CSHTML5.Migration -Version 2.0.0-alpha35-050 -OutputDirectory src\packages
-build\nuget\nuget.exe install CSHTML5.Migration.WorkInProgress -Version 2.0.0-alpha35-050 -OutputDirectory src\packages
+build\nuget\nuget.exe install CSHTML5 -Version 2.0.0-alpha36-051 -OutputDirectory src\packages
+build\nuget\nuget.exe install CSHTML5.Migration -Version 2.0.0-alpha36-051 -OutputDirectory src\packages
+build\nuget\nuget.exe install CSHTML5.Migration.WorkInProgress -Version 2.0.0-alpha36-051 -OutputDirectory src\packages
 pause
