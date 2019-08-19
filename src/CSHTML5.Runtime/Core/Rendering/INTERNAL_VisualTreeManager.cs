OpenSilver/OpenSilver

commit 8b5a14472a24257a9244ef9e962428a2ac78e42e

Performance improvement: add the option "Application.Current.Host.Settings.EnableOptimizationWhereCollapsedControlsAreNotLoaded" to avoid loading the controls that are Collapsed until they become Visible.

src/CSHTML5.Runtime/Core/Rendering/INTERNAL_VisualTreeManager.cs
@@ -51,6 +51,7 @@ namespace CSHTML5.Internal
     public static class INTERNAL_VisualTreeManager
     {
         internal static bool EnablePerformanceLogging;
+        internal static bool EnableOptimizationWhereCollapsedControlsAreNotLoaded;
 
         public static void DetachVisualChildIfNotNull(UIElement child, UIElement parent)
         {
@@ -166,6 +167,7 @@ namespace CSHTML5.Internal
             element.INTERNAL_VisualParent = null;
             element.INTERNAL_VisualChildrenInformation = null;
             element.INTERNAL_AdditionalOutsideDivForMargins = null;
+            element.INTERNAL_DeferredLoadingWhenControlBecomesVisible = null;
         }
 
         public static void AttachVisualChildIfNotAlreadyAttached(UIElement child, UIElement parent)
@@ -328,7 +330,7 @@ namespace CSHTML5.Internal
                     //get the list of setters of the currently applied style on the element and of the default style:
                     Dictionary<DependencyProperty, Setter> normalStyleDictionary = null;
                     bool childHasStyle = childAsFrameworkElement.Style != null;
-                    if(childHasStyle)
+                    if (childHasStyle)
                     {
                         normalStyleDictionary = childAsFrameworkElement.Style.GetDictionaryOfSettersFromStyle();
                     }
@@ -343,7 +345,7 @@ namespace CSHTML5.Internal
 #endif
                         )
                     {
-                        if(!childHasStyle || !normalStyleDictionary.ContainsKey(prop))
+                        if (!childHasStyle || !normalStyleDictionary.ContainsKey(prop))
                         {
                             Setter setter = defaultStyleDictionary[prop];
 
@@ -384,6 +386,44 @@ namespace CSHTML5.Internal
 #if PERFSTAT
             Performance.Counter("VisualTreeManager: Prepare the parent", t0);
 #endif
+
+            //--------------------------------------------------------
+            // CONTINUE WITH THE OTHER STEPS (ONLY IF THE CONTROL IS VISIBLE, IF OPTIMIZATION IS ENABLED):
+            //--------------------------------------------------------
+
+            if (EnableOptimizationWhereCollapsedControlsAreNotLoaded && child.Visibility != Visibility.Collapsed)
+            {
+                AttachVisualChild_Private_FinalStepsOnlyIfControlIsVisible(
+                    child,
+                    parent,
+                    doesParentRequireToCreateAWrapperForEachChild,
+                    innerDivOfWrapperForChild,
+                    domElementWhereToPlaceChildStuff,
+                    wrapperForChild);
+            }
+            else
+            {
+                child.INTERNAL_DeferredLoadingWhenControlBecomesVisible = () =>
+                {
+                    AttachVisualChild_Private_FinalStepsOnlyIfControlIsVisible(
+                        child,
+                        parent,
+                        doesParentRequireToCreateAWrapperForEachChild,
+                        innerDivOfWrapperForChild,
+                        domElementWhereToPlaceChildStuff,
+                        wrapperForChild);
+                };
+            }
+        }
+        static void AttachVisualChild_Private_FinalStepsOnlyIfControlIsVisible(
+            UIElement child,
+            UIElement parent,
+            bool doesParentRequireToCreateAWrapperForEachChild,
+            object innerDivOfWrapperForChild,
+            object domElementWhereToPlaceChildStuff,
+            object wrapperForChild
+            )
+        {
 
             //--------------------------------------------------------
             // CREATE THE DIV FOR THE MARGINS (OPTIONAL):
@@ -684,122 +724,6 @@ namespace CSHTML5.Internal
 #if PERFSTAT
             Performance.Counter("VisualTreeManager: Raise Loaded event", t11);
 #endif
-
-#region PREVIOUS VERSION
-            //// Prepare the parent DOM structure so that it is ready to contain the child (for example, in case of a grid, we need to (re)create the rows and columns where to place the elements):
-            //parent.INTERNAL_UpdateDomStructureIfNecessary();
-            //dynamic domElementWhereToPlaceChildStuff = (parent.GetDomElementWhereToPlaceChild(child) ?? parent.INTERNAL_InnerDomElement);
-            //dynamic innerDivOfWrapperForChild;
-            //dynamic wrapperForChild = parent.CreateDomChildWrapper(out innerDivOfWrapperForChild);
-
-            //// Set the "Parent" property of the Child (note: we need to do that before child.CreateDomElement because the type of the parent is used to display the child correctly):
-            //child.INTERNAL_VisualParent = parent;
-
-            //// Create the DOM structure for the Child:
-            //dynamic domElementWhereToPlaceGrandChildren = null;
-            //dynamic additionalOutsideDivForMargins = null;
-            //dynamic outerDomElement;
-            //if (child.INTERNAL_HtmlRepresentation == null)
-            //    outerDomElement = child.CreateDomElement(out domElementWhereToPlaceGrandChildren);
-            //else
-            //    outerDomElement = INTERNAL_HtmlDomManager.CreateDomFromString(child.INTERNAL_HtmlRepresentation);
-
-            //// Initialize the "Width" and "Height" of the child DOM structure:
-            //if (child is FrameworkElement)
-            //    FrameworkElement.INTERNAL_InitializeOuterDomElementWidthAndHeight(((FrameworkElement)child), outerDomElement);
-
-            //// Surround the "outerDomElement" with an additional DIV to handle the "Margin" property of a control (unless we are in a Canvas):
-            //if (!(parent is Canvas))
-            //{
-            //    additionalOutsideDivForMargins = INTERNAL_HtmlDomManager.CreateDomElement("div");
-            //    additionalOutsideDivForMargins.style.boxSizing = "border-box";
-            //    if (child is FrameworkElement && ((FrameworkElement)child).HorizontalAlignment == HorizontalAlignment.Stretch)
-            //        additionalOutsideDivForMargins.style.width = "100%";
-            //    if (child is FrameworkElement && ((FrameworkElement)child).VerticalAlignment == VerticalAlignment.Stretch)
-            //        additionalOutsideDivForMargins.style.height = "100%";
-            //    INTERNAL_HtmlDomManager.AppendChild(additionalOutsideDivForMargins, outerDomElement);
-            //}
-            //else
-            //{
-            //    // In a Canvas, we don't want to add the additional DIV because there are no margins and we don't want to interfere with the pointer events by creating an additional DIV.
-            //    // So we don't create an additional DIV: we use the outer element directly.
-            //    additionalOutsideDivForMargins = outerDomElement;
-
-            //    // We make sure the childDomElement.style.position = absolute
-            //    additionalOutsideDivForMargins.style.position = "absolute"; //todo: test if this works properly
-            //}
-
-            //// Update the DOM structure of the Child (for example, if the child is a Grid, this will render its rows and columns):
-            //child.INTERNAL_UpdateDomStructureIfNecessary();
-
-            //// Remember all information:
-            //child.INTERNAL_OuterDomElement = outerDomElement;
-            //child.INTERNAL_InnerDomElement = domElementWhereToPlaceGrandChildren;
-            //child.INTERNAL_AdditionalOutsideDivForMargins = additionalOutsideDivForMargins;
-
-            //// Add the child DOM structure to the parent DOM structure:
-            //bool isWrapperForChildNull = (wrapperForChild == null);
-            //bool isInnerDivOfWrapperForChildNull = (innerDivOfWrapperForChild == null);
-            //bool doesParentRequireToCreateAWrapperForEachChild = (!isWrapperForChildNull && !isInnerDivOfWrapperForChildNull); // Note: this is "True" for complex structures such as tables, false otherwise (cf. documentation in "INTERNAL_VisualChildInformation" class).
-            //if (doesParentRequireToCreateAWrapperForEachChild)
-            //{
-            //    // Add the wrapper to the parent's children placeholder:
-            //    INTERNAL_HtmlDomManager.AppendChild(domElementWhereToPlaceChildStuff, wrapperForChild);
-
-            //    // Add the child itself:
-            //    INTERNAL_HtmlDomManager.AppendChild(innerDivOfWrapperForChild, additionalOutsideDivForMargins);
-            //}
-            //else
-            //{
-            //    // Add the child directly to the parent's children placeholder:
-            //    //INTERNAL_HtmlDomManager.AppendChild(domElementWhereToPlaceChildStuff, newDiv);
-            //    INTERNAL_HtmlDomManager.AppendChild(domElementWhereToPlaceChildStuff, additionalOutsideDivForMargins);
-            //}
-            //if (!(parent is Canvas)) //this test is here because when parent is a Canvas, outsideDomElementForMargins = newdiv; see #OUTSIDEDOMELEMENTFORMARGINS
-            //{
-            //    //Add the child to the additional Div used for the margins:
-            //    INTERNAL_HtmlDomManager.AppendChild(additionalOutsideDivForMargins, outerDomElement);
-            //}
-
-            //// Remember the information about the "VisualChildren":
-            //if (parent.INTERNAL_VisualChildrenInformation == null)
-            //    parent.INTERNAL_VisualChildrenInformation = new Dictionary<UIElement, INTERNAL_VisualChildInformation>();
-            //parent.INTERNAL_VisualChildrenInformation.Add(child,
-            //    new INTERNAL_VisualChildInformation()
-            //    {
-            //        INTERNAL_UIElement = child,
-            //        INTERNAL_OptionalChildWrapper_OuterDomElement = wrapperForChild,
-            //        INTERNAL_OptionalChildWrapper_ChildWrapperInnerDomElement = innerDivOfWrapperForChild
-            //    });
-
-            //// Register events if any:
-            //child.INTERNAL_AttachToDomEvents();
-
-            //// Get the Inherited properties and pass them to the direct children:
-            //if (parent.INTERNAL_AllInheritedProperties != null)
-            //{
-            //    foreach (DependencyProperty dependencyProperty in parent.INTERNAL_AllInheritedProperties.Keys)
-            //    {
-            //        bool recursively = false; // We don't want a recursion here because the "Attach" method is already recursive due to the fact that we raise property changed on the Children property, which causes to reattach the subtree.
-            //        child.SetInheritedValue(dependencyProperty, INTERNAL_PropertyStore.GetValue(parent.INTERNAL_AllInheritedProperties[dependencyProperty]), recursively);
-            //    }
-            //}
-
-            //// For debugging purposes (to better read the output html), add a class to the outer DIV that tells us the corresponding type of the element (Border, StackPanel, etc.):
-            //INTERNAL_HtmlDomManager.SetDomElementClassAttribute(outerDomElement, child.GetType().ToString());
-
-            //child.INTERNAL_OnAttachedToVisualTree(); // IMPORTANT: Must be done BEFORE "RaiseChangedEventOnAllDependencyProperties" (for example, the ItemsControl uses this to initialize its visual)
-
-            //// Render the control:
-            //RaiseChangedEventOnAllDependencyProperties(child); // Note: this causes a recursion of the AttachVisualChild method because the "Children" properties of the controls get refreshed.
-            ////element.INTERNAL_Render();
-
-            //child.INTERNAL_UpdateBindingsSource();
-
-            //// Call the "Loaded" event: (note: in XAML, the "loaded" event of the children is called before the loaded" event of the parent)
-            //if (child is FrameworkElement)
-            //    ((FrameworkElement)child).INTERNAL_RaiseLoadedEvent();
-#endregion
         }
 
         public static bool IsElementInVisualTree(UIElement child)

src/CSHTML5.Runtime/Windows.UI.Xaml/Settings.cs
@@ -55,5 +55,18 @@ namespace System
 
             }
         }
+
+        private bool _enableOptimizationWhereCollapsedControlsAreNotLoaded;
+
+        public bool EnableOptimizationWhereCollapsedControlsAreNotLoaded
+        {
+            get { return _enableOptimizationWhereCollapsedControlsAreNotLoaded; }
+            set
+            {
+                _enableOptimizationWhereCollapsedControlsAreNotLoaded = value;
+                INTERNAL_VisualTreeManager.EnableOptimizationWhereCollapsedControlsAreNotLoaded = true;
+            }
+        }
+
     }
 }

src/CSHTML5.Runtime/Windows.UI.Xaml/UIElement.cs
@@ -94,6 +94,8 @@ namespace Windows.UI.Xaml
         internal int INTERNAL_lastClickDate; //this is used in the PointerPressed event to fill the ClickCount Property.
         public string XamlSourcePath; //this is used by the Simulator to tell where this control is defined. It is non-null only on root elements, that is, elements which class has "InitializeComponent" method. This member is public because it needs to be accessible via reflection.
         internal bool _isLoaded;
+        internal Action INTERNAL_DeferredLoadingWhenControlBecomesVisible;
+
         /// <summary>
         /// Dictionary that helps link the validationErrors to the BindingExpressions for managing the errors.
         /// </summary>
@@ -411,44 +413,56 @@ namespace Windows.UI.Xaml
             var uiElement = (UIElement)d;
             Visibility newValue = (Visibility)e.NewValue;
 
-            if (INTERNAL_VisualTreeManager.IsElementInVisualTree(uiElement))
+            // Finish loading the element if it was not loaded yet because it was Collapsed (and optimization was enabled in the Settings):
+            if (uiElement.INTERNAL_DeferredLoadingWhenControlBecomesVisible != null
+                && newValue != Visibility.Collapsed)
             {
-                // Get a reference to the most outer DOM element to show/hide:
-                dynamic mostOuterDomElement = null;
-                if (uiElement.INTERNAL_VisualParent is UIElement)
-                    mostOuterDomElement = ((UIElement)uiElement.INTERNAL_VisualParent).INTERNAL_VisualChildrenInformation[uiElement].INTERNAL_OptionalChildWrapper_OuterDomElement; // Note: this is useful for example inside a Grid, where we want to hide the whole child wrapper in order to ensure that it doesn't capture mouse clicks thus preventing users from clicking on other elements in the Grid.
-                if (mostOuterDomElement == null)
-                    mostOuterDomElement = uiElement.INTERNAL_AdditionalOutsideDivForMargins ?? uiElement.INTERNAL_OuterDomElement;
-                dynamic style = INTERNAL_HtmlDomManager.GetDomElementStyleForModification(mostOuterDomElement);
-
-                // Apply the visibility:
-                if (newValue == Visibility.Collapsed)
+                Action deferredLoadingWhenControlBecomesVisible = uiElement.INTERNAL_DeferredLoadingWhenControlBecomesVisible;
+                uiElement.INTERNAL_DeferredLoadingWhenControlBecomesVisible = null;
+                deferredLoadingWhenControlBecomesVisible();
+            }
+            else
+            {
+                // Set the CSS to make the DOM element visible/collapsed:
+                if (INTERNAL_VisualTreeManager.IsElementInVisualTree(uiElement))
                 {
-                    // Remember the current value of the CSS property "display" so that we can later revert to it:
-                    string previousValueOfDisplayCssProperty = style.display;
-                    if (previousValueOfDisplayCssProperty != "none")
-                        uiElement._previousValueOfDisplayCssProperty = previousValueOfDisplayCssProperty;
+                    // Get a reference to the most outer DOM element to show/hide:
+                    dynamic mostOuterDomElement = null;
+                    if (uiElement.INTERNAL_VisualParent is UIElement)
+                        mostOuterDomElement = ((UIElement)uiElement.INTERNAL_VisualParent).INTERNAL_VisualChildrenInformation[uiElement].INTERNAL_OptionalChildWrapper_OuterDomElement; // Note: this is useful for example inside a Grid, where we want to hide the whole child wrapper in order to ensure that it doesn't capture mouse clicks thus preventing users from clicking on other elements in the Grid.
+                    if (mostOuterDomElement == null)
+                        mostOuterDomElement = uiElement.INTERNAL_AdditionalOutsideDivForMargins ?? uiElement.INTERNAL_OuterDomElement;
+                    dynamic style = INTERNAL_HtmlDomManager.GetDomElementStyleForModification(mostOuterDomElement);
 
-                    // Hide the DOM element (or its wrapper if any):
-                    style.display = "none";
-                }
-                else
-                {
-                    // Show the DOM element (or its wrapper if any) by reverting the CSS property "display" to its previous value:
-                    if (style.display == "none")
-                        style.display = uiElement._previousValueOfDisplayCssProperty;
-
-                    // The alignment was not calculated when the object was hidden, so we need to calculate it now:
-                    if (uiElement is FrameworkElement && uiElement.INTERNAL_VisualParent != null) // Note: The "INTERNAL_VisualParent" can be "null" for example if we are changing the visibility of a "PopupRoot" control.
+                    // Apply the visibility:
+                    if (newValue == Visibility.Collapsed)
                     {
-                        FrameworkElement.INTERNAL_ApplyHorizontalAlignmentAndWidth((FrameworkElement)uiElement, ((FrameworkElement)uiElement).HorizontalAlignment); //todo-perfs: only call the relevant portion of the code?
-                        FrameworkElement.INTERNAL_ApplyVerticalAlignmentAndHeight((FrameworkElement)uiElement, ((FrameworkElement)uiElement).VerticalAlignment); //todo-perfs: only call the relevant portion of the code?
-                    }
-                }
-                INTERNAL_WorkaroundIE11IssuesWithScrollViewerInsideGrid.RefreshLayoutIfIE();
+                        // Remember the current value of the CSS property "display" so that we can later revert to it:
+                        string previousValueOfDisplayCssProperty = style.display;
+                        if (previousValueOfDisplayCssProperty != "none")
+                            uiElement._previousValueOfDisplayCssProperty = previousValueOfDisplayCssProperty;
 
-                // Notify any listeners that the visibility has changed (this can be useful for example to redraw the Path control when it becomes visible, due to the fact that drawing on a hidden HTML canvas is not persisted):
-                INTERNAL_VisibilityChangedNotifier.NotifyListenersThatVisibilityHasChanged(uiElement);
+                        // Hide the DOM element (or its wrapper if any):
+                        style.display = "none";
+                    }
+                    else
+                    {
+                        // Show the DOM element (or its wrapper if any) by reverting the CSS property "display" to its previous value:
+                        if (style.display == "none")
+                            style.display = uiElement._previousValueOfDisplayCssProperty;
+
+                        // The alignment was not calculated when the object was hidden, so we need to calculate it now:
+                        if (uiElement is FrameworkElement && uiElement.INTERNAL_VisualParent != null) // Note: The "INTERNAL_VisualParent" can be "null" for example if we are changing the visibility of a "PopupRoot" control.
+                        {
+                            FrameworkElement.INTERNAL_ApplyHorizontalAlignmentAndWidth((FrameworkElement)uiElement, ((FrameworkElement)uiElement).HorizontalAlignment); //todo-perfs: only call the relevant portion of the code?
+                            FrameworkElement.INTERNAL_ApplyVerticalAlignmentAndHeight((FrameworkElement)uiElement, ((FrameworkElement)uiElement).VerticalAlignment); //todo-perfs: only call the relevant portion of the code?
+                        }
+                    }
+                    INTERNAL_WorkaroundIE11IssuesWithScrollViewerInsideGrid.RefreshLayoutIfIE();
+
+                    // Notify any listeners that the visibility has changed (this can be useful for example to redraw the Path control when it becomes visible, due to the fact that drawing on a hidden HTML canvas is not persisted):
+                    INTERNAL_VisibilityChangedNotifier.NotifyListenersThatVisibilityHasChanged(uiElement);
+                }
             }
         }
 

commit 6b04725563cf41c5d34bfcd354dcc8505471d5d1

Fix regression introduced in changeset 8b5a1447 that causes an exception on application load when the option to optimize performance by not loading collapsed UI elements is not enabled.

src/CSHTML5.Runtime/Core/Rendering/INTERNAL_VisualTreeManager.cs
@@ -388,10 +388,10 @@ namespace CSHTML5.Internal
 #endif
 
             //--------------------------------------------------------
-            // CONTINUE WITH THE OTHER STEPS (ONLY IF THE CONTROL IS VISIBLE, IF OPTIMIZATION IS ENABLED):
+            // CONTINUE WITH THE OTHER STEPS (OR DEFER TO WHEN THE ELEMENT IS VISIBLE, IF THE OPTIMIZATION TO NOT LOAD COLLAPSED CONTROLS IS ENABLED):
             //--------------------------------------------------------
 
-            if (EnableOptimizationWhereCollapsedControlsAreNotLoaded && child.Visibility != Visibility.Collapsed)
+            if (!EnableOptimizationWhereCollapsedControlsAreNotLoaded || child.Visibility != Visibility.Collapsed)
             {
                 AttachVisualChild_Private_FinalStepsOnlyIfControlIsVisible(
                     child,