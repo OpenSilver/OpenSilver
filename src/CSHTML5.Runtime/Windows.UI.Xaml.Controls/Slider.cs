bcc2089f4c754a66e45d1a94d2f4d7af418a6d35
Sylvain <t0ast@hotmail.fr>
2019-08-27 14:48:58 +0200

    Improve the Slider Control by adding the possibility to click on the sides of the thumb to make Large Changes to the value and using the keyboard keys for small changes to the Value.

--- src/CSHTML5.Runtime/Windows.UI.Xaml.Controls/Slider.cs
+++ src/CSHTML5.Runtime/Windows.UI.Xaml.Controls/Slider.cs
@@ -47,11 +47,15 @@ public class Slider : RangeBase
         Canvas _horizontalRoot;
         Thumb _horizontalThumb;
         Rectangle _horizontalRail;
+        Button _horizontalLargeDecrease;
+        Button _horizontalLargeIncrease;
 
         // Vertical elements:
         Canvas _verticalRoot;
         Thumb _verticalThumb;
         Rectangle _verticalRail;
+        Button _verticalLargeDecrease;
+        Button _verticalLargeIncrease;
 
         /// <summary>
         /// Initializes a new instance of the Slider class.
@@ -95,10 +99,14 @@ public class Slider : RangeBase
             _horizontalRoot = this.GetTemplateChild("HorizontalRoot") as Canvas;
             _horizontalThumb = this.GetTemplateChild("HorizontalThumb") as Thumb;
             _horizontalRail = this.GetTemplateChild("HorizontalRail") as Rectangle;
+            _horizontalLargeDecrease = this.GetTemplateChild("HorizontalLargeDecrease") as Button;
+            _horizontalLargeIncrease = this.GetTemplateChild("HorizontalLargeIncrease") as Button;
 
             _verticalRoot = this.GetTemplateChild("VerticalRoot") as Canvas;
             _verticalThumb = this.GetTemplateChild("VerticalThumb") as Thumb;
             _verticalRail = this.GetTemplateChild("VerticalRail") as Rectangle;
+            _verticalLargeDecrease = this.GetTemplateChild("VerticalLargeDecrease") as Button;
+            _verticalLargeIncrease = this.GetTemplateChild("VerticalLargeIncrease") as Button;
 
             //----------------------------
             // Register the events:
@@ -108,14 +116,26 @@ public class Slider : RangeBase
                 _horizontalThumb.DragStarted += HorizontalThumb_DragStarted;
                 _horizontalThumb.DragDelta += HorizontalThumb_DragDelta;
                 _horizontalThumb.DragCompleted += HorizontalThumb_DragCompleted;
+                _horizontalThumb.GotFocus += HorizontalThumb_GotFocus;
+                _horizontalThumb.LostFocus += HorizontalThumb_LostFocus;
             }
             if (_verticalThumb != null)
             {
                 _verticalThumb.DragStarted += VerticalThumb_DragStarted;
                 _verticalThumb.DragDelta += VerticalThumb_DragDelta;
                 _verticalThumb.DragCompleted += VerticalThumb_DragCompleted;
+                _verticalThumb.GotFocus += VerticalThumb_GotFocus;
+                _verticalThumb.LostFocus += VerticalThumb_LostFocus;
             }
 
+            if (_horizontalLargeDecrease != null)
+                _horizontalLargeDecrease.Click += LargeDecrease_Click;
+            if (_verticalLargeDecrease != null)
+                _verticalLargeDecrease.Click += LargeDecrease_Click;
+            if (_horizontalLargeIncrease != null)
+                _horizontalLargeIncrease.Click += LargeIncrease_Click;
+            if (_verticalLargeIncrease != null)
+                _verticalLargeIncrease.Click += LargeIncrease_Click;
 
             //----------------------------
             // Display the horizontal or vertical root depending on the orientation:
@@ -128,6 +148,40 @@ public class Slider : RangeBase
             if (_horizontalRoot != null)
             {
                 _horizontalRoot.Visibility = (this.Orientation == Orientation.Horizontal ? Visibility.Visible : Visibility.Collapsed);
+            }
+        }
+
+        private void VerticalThumb_LostFocus(object sender, RoutedEventArgs e)
+        {
+            _verticalThumb.KeyDown -= Thumb_KeyDown;
+        }
+
+        private void VerticalThumb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            _verticalThumb.KeyDown -= Thumb_KeyDown;
+            _verticalThumb.KeyDown += Thumb_KeyDown;
+        }
+
+        private void HorizontalThumb_LostFocus(object sender, RoutedEventArgs e)
+        {
+            _horizontalThumb.KeyDown -= Thumb_KeyDown;
+        }
+
+        private void HorizontalThumb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            _horizontalThumb.KeyDown -= Thumb_KeyDown;
+            _horizontalThumb.KeyDown += Thumb_KeyDown;
+        }
+
+        private void Thumb_KeyDown(object sender, Input.KeyRoutedEventArgs e)
+        {
+            if(e.Key == System.VirtualKey.Right || e.Key == System.VirtualKey.Up)
+            {
+                OnClickOnSmallOrLargeDecreaseIncreaseButtons(SmallChange, ScrollEventType.SmallIncrement);
+            }
+            else if(e.Key == System.VirtualKey.Left || e.Key == System.VirtualKey.Down)
+            {
+                OnClickOnSmallOrLargeDecreaseIncreaseButtons(-SmallChange, ScrollEventType.SmallDecrement);
             }
         }
 
@@ -153,6 +207,36 @@ public class Slider : RangeBase
 
             // Refresh the position of the thumb:
             UpdateThumbPositionAndSize();
+        }
+
+        void LargeIncrease_Click(object sender, RoutedEventArgs e)
+        {
+            OnClickOnSmallOrLargeDecreaseIncreaseButtons(this.LargeChange, ScrollEventType.LargeIncrement);
+        }
+
+        void LargeDecrease_Click(object sender, RoutedEventArgs e)
+        {
+            OnClickOnSmallOrLargeDecreaseIncreaseButtons(-this.LargeChange, ScrollEventType.LargeDecrement);
+        }
+
+        void OnClickOnSmallOrLargeDecreaseIncreaseButtons(double amountByWhichToIncreaseOrDecreaseTheValue, ScrollEventType scrollEventType)
+        {
+            double newValue = CoerceValue(this.Value + amountByWhichToIncreaseOrDecreaseTheValue);
+            if (newValue != this.Value)
+            {
+                this.SetLocalValue(RangeBase.ValueProperty, newValue); // Note: we do not use "this.Value = newValue" because it deletes any bindings that the user may have set to the scrollbar with <ScrollBar Value="{Binding...}"/>.
+                if (this.Scroll != null)
+                    this.Scroll(this, new ScrollEventArgs(newValue, scrollEventType));
+            }
+
+            if(Orientation == Orientation.Horizontal)
+            {
+                _horizontalThumb.Focus();
+            }
+            else
+            {
+                _verticalThumb.Focus();
+            }
         }
 
         void VerticalThumb_DragCompleted(object sender, DragCompletedEventArgs e)
@@ -189,6 +273,11 @@ public class Slider : RangeBase
         {
             // Reset the "ValueWithoutCoercion" (read the note near the definition of that variable to better understand what it is used for):
             _valueWithoutCoercion = this.Value;
+
+            if (_horizontalLargeDecrease != null)
+                _horizontalLargeDecrease.IsHitTestVisible = false;
+            if (_horizontalLargeIncrease != null)
+                _horizontalLargeIncrease.IsHitTestVisible = false;
         }
 
         void OnDragCompleted()
@@ -196,6 +285,11 @@ public class Slider : RangeBase
             // Call the "Scroll" event passing the "EndScroll" argument:
             if (this.Scroll != null)
                 this.Scroll(this, new ScrollEventArgs(this.Value, ScrollEventType.EndScroll));
+
+            if (_horizontalLargeDecrease != null)
+                _horizontalLargeDecrease.IsHitTestVisible = true;
+            if (_horizontalLargeIncrease != null)
+                _horizontalLargeIncrease.IsHitTestVisible = true;
         }
 
         void ChangeValueBasedOnPointerMovement(double pointerMovementInPixels)
@@ -274,21 +368,35 @@ public class Slider : RangeBase
 
                     if (_verticalRoot != null
                         && _verticalThumb != null
-                        && _verticalRail != null)
+                        && _verticalRail != null
+                        && _verticalLargeIncrease != null
+                        && _verticalLargeDecrease != null)
                     {
                         //--------------------------------
                         // Update the size of the UI elements:
                         //--------------------------------
-                        _verticalThumb.Width = totalControlSize.Width;
+                        _verticalThumb.Width
+                            = _verticalLargeDecrease.Height
+                            = _verticalLargeIncrease.Height
+                            = totalControlSize.Width;
 
-                        double buttonsHeight = totalControlSize.Width; // Deliberately square.
 
                         //--------------------------------
                         // Update the position of the thumb and of the large increment/decrement areas:
                         //--------------------------------
                         UpdateThumbPositionAndSize(totalControlSize.Height);
+
+                        //Move the different elements so that they are properly centered horizontally:
+                        double thumbLeftSidePosition = (totalControlSize.Width - _verticalThumb.ActualWidth) / 2;
                         Canvas.SetLeft(_verticalRail, (totalControlSize.Width - _verticalRail.ActualWidth) / 2);
-                        Canvas.SetLeft(_verticalThumb, (totalControlSize.Width - _verticalThumb.ActualWidth) / 2);
+                        Canvas.SetLeft(_verticalThumb, thumbLeftSidePosition);
+                        Canvas.SetLeft(_verticalLargeDecrease, thumbLeftSidePosition);
+                        Canvas.SetLeft(_verticalLargeIncrease, thumbLeftSidePosition);
+
+                        // Set the Buttons' width so it is the same as the Thumb's:
+                        double thumbWidth = _verticalThumb.ActualWidth;
+                        _verticalLargeDecrease.Width = thumbWidth;
+                        _verticalLargeIncrease.Width = thumbWidth;
 
                         return true;
                     }
@@ -301,21 +409,35 @@ public class Slider : RangeBase
 
                     if (_horizontalRoot != null
                         && _horizontalThumb != null
-                        && _horizontalRail != null)
+                        && _horizontalRail != null
+                        && _horizontalLargeIncrease != null
+                        && _horizontalLargeDecrease != null)
                     {
                         //--------------------------------
                         // Update the size of the UI elements:
                         //--------------------------------
-                        _horizontalThumb.Height = totalControlSize.Height;
-
-                        double buttonsWidth = totalControlSize.Height; // Deliberately square.
+                        _horizontalThumb.Height
+                            = _horizontalLargeDecrease.Height
+                            = _horizontalLargeIncrease.Height
+                            = totalControlSize.Height;
 
                         //--------------------------------
                         // Update the position of the thumb and of the large increment/decrement areas:
                         //--------------------------------
                         UpdateThumbPositionAndSize(totalControlSize.Width);
+
+                        //Move the different elements so that they are properly centered vertically:
+                        double thumbTopSidePosition = (totalControlSize.Height - _horizontalThumb.ActualHeight) / 2;
                         Canvas.SetTop(_horizontalRail, (totalControlSize.Height - _horizontalRail.ActualHeight) / 2);
-                        Canvas.SetTop(_horizontalThumb, (totalControlSize.Height - _horizontalThumb.ActualHeight) / 2);
+                        Canvas.SetTop(_horizontalThumb, thumbTopSidePosition);
+                        Canvas.SetTop(_horizontalLargeDecrease, thumbTopSidePosition);
+                        Canvas.SetTop(_horizontalLargeIncrease, thumbTopSidePosition);
+
+                        // Set the Buttons' width so it is the same as the Thumb's:
+                        double thumbHeight = _horizontalThumb.ActualHeight;
+                        _horizontalLargeDecrease.Height = thumbHeight;
+                        _horizontalLargeIncrease.Height = thumbHeight;
+
                         return true;
                     }
                 }
@@ -403,25 +525,49 @@ public class Slider : RangeBase
             double maxMinusMin = maxValue - minValue;
 
             double thumbCenterPosition = (maxMinusMin > 0 ? (thumbSize / 2) + (scrollableSize * (value - Minimum) / maxMinusMin) : trackSize / 2); // This is the X or Y position of the thumb (respectively for horizontal or vertical orientation).
+            if(Orientation ==  Orientation.Vertical)
+            {
+                thumbCenterPosition = trackSize - thumbCenterPosition;
+            }
             double thumbTopLeftPosition = thumbCenterPosition - (thumbSize / 2);
             double thumbBottomRightPosition = thumbCenterPosition + (thumbSize / 2);
 
             if (Orientation == Orientation.Vertical)
             {
-                if (_verticalThumb != null)
+                if (_verticalThumb != null
+                    && _verticalLargeDecrease != null
+                    && _verticalLargeIncrease != null)
                 {
                     // Position the thumb:
                     _verticalThumb.Height = thumbSize;
                     Canvas.SetTop(_verticalThumb, thumbTopLeftPosition);
+
+                    // Position the large decrease button:
+                    _verticalLargeDecrease.Height = Math.Max(1d, totalControlSize - thumbBottomRightPosition); //todo: replace 1d with 0d when fixed bug with Height=0.
+                    Canvas.SetTop(_verticalLargeDecrease, thumbBottomRightPosition);
+
+                    // Position the large increase button:
+                    _verticalLargeIncrease.Height = Math.Max(1d, thumbTopLeftPosition); //todo: replace 1d with 0d when fixed bug with Height=0.
+                    Canvas.SetTop(_verticalLargeIncrease, 0d);
                 }
             }
             else
             {
-                if (_horizontalThumb != null)
+                if (_horizontalThumb != null
+                    && _horizontalLargeDecrease != null
+                    && _horizontalLargeIncrease != null)
                 {
                     // Position the thumb:
                     _horizontalThumb.Width = thumbSize;
                     Canvas.SetLeft(_horizontalThumb, thumbTopLeftPosition);
+
+                    // Position the large decrease button:
+                    _horizontalLargeDecrease.Width = Math.Max(1d, thumbTopLeftPosition); //todo: replace 1d with 0d when fixed bug with Width=0.
+                    Canvas.SetLeft(_horizontalLargeDecrease, 0d);
+
+                    // Position the large increase button:
+                    _horizontalLargeIncrease.Width = Math.Max(1d, totalControlSize - thumbBottomRightPosition); //todo: replace 1d with 0d when fixed bug with Width=0.
+                    Canvas.SetLeft(_horizontalLargeIncrease, thumbBottomRightPosition);
                 }
             }
         }
