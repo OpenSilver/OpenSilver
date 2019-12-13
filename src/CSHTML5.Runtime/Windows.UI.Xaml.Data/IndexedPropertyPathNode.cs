73e157cde570eabc05a0a6b45c6169aa4160e4ae
Pierre Barjon <barjon.pierre@gmail.com>
2019-12-13 12:55:36 +0100

    Fix IndexedPropertyPathNode Index is now converted to the type of the indexer's parameter. For now, only single parameter indexers are supported.

--- src/CSHTML5.Runtime/Windows.UI.Xaml.Data/IndexedPropertyPathNode.cs
+++ src/CSHTML5.Runtime/Windows.UI.Xaml.Data/IndexedPropertyPathNode.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNetForHtml5.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -11,7 +12,7 @@ namespace System.Windows.Data
 namespace Windows.UI.Xaml.Data
 #endif
 {
-    class IndexedPropertyPathNode : PropertyPathNode
+    internal class IndexedPropertyPathNode : PropertyPathNode
     {
         //Note: limitations of this class (that I could think of)
         //      - If the source object's type defines this[...] in different ways, it will not work (see comments in the GetMethod method in this class).
@@ -22,36 +23,29 @@ namespace Windows.UI.Xaml.Data
         //  For now, I have decided to only support one parameter, otherwise it would become much more complicated (index comes as a string so we would need to check whether there are commas, how many, and I think it could still be considered as only one parameter: a string with commas in it).
 
         //Note: The BindsDirectlyToSource from the StandardPropertyPathNode class would always be considered true in this class.
-        string _index = null;
+        readonly string _index = null;
+        object _parsedIndex = null;
         MethodInfo _setMethod = null;
         MethodInfo _getMethod = null;
+
         internal IndexedPropertyPathNode(string index)
         {
-            _index = index;
+            this._index = index;
         }
 
         internal override void SetValue(object value)
         {
-            if (_setMethod != null)
+            if (this._setMethod != null)
             {
-                bool attemptParsedIndex = false; //todo-perf: keep attemptParsedIndex, parsedIndex, as well as a boolean saying whether we should use the parsedIndex. That way we will only call TryInvokeMethod once when needed and won't need to retry to parse the index.
-                int parsedIndex = -1;
-                if (int.TryParse(_index, out parsedIndex))
-                {
-                    attemptParsedIndex = true;
-                }
-                if (!TryInvokeSetMethod(parsedIndex, value)) //we attempt to invoke the method with an int as parameter but there is no guarantee that what is expected is an int so we keep the option of a string as a Backup.
-                {
-                    TryInvokeSetMethod(_index, value);
-                }
+                this.TryInvokeSetMethod(this._parsedIndex, value);
             }
         }
 
-        bool TryInvokeSetMethod(object index, object value)
+        private bool TryInvokeSetMethod(object index, object value)
         {
             try
             {
-                _setMethod.Invoke(this.Source, new object[] { index, value });
+                this._setMethod.Invoke(this.Source, new object[] { index, value });
                 return true;
             }
             catch
@@ -62,36 +56,22 @@ namespace Windows.UI.Xaml.Data
 
         internal override void UpdateValue()
         {
-            if (_getMethod != null)
+            if (this._getMethod != null)
             {
-                bool getMethodInvokedSuccessfully = false;
-                bool attemptParsedIndex = false; //todo-perf: keep attemptParsedIndex, parsedIndex, as well as a boolean saying whether we should use the parsedIndex. That way we will only call TryInvokeMethod once when needed and won't need to retry to parse the index.
-                int parsedIndex = -1;
                 object result;
-                if (int.TryParse(_index, out parsedIndex))
+                if (this.TryInvokeGetMethod(this._parsedIndex, out result))
                 {
-                    attemptParsedIndex = true;
-                }
-                getMethodInvokedSuccessfully = TryInvokeGetMethod(parsedIndex, out result);
-                if (!getMethodInvokedSuccessfully) //we attempt to invoke the method with an int as parameter but there is no guarantee that what is expected is an int so we keep the option of a string as a Backup.
-                {
-                    getMethodInvokedSuccessfully = TryInvokeGetMethod(_index, out result);
-                }
-
-                if (getMethodInvokedSuccessfully)
-                {
-                    this.UpdateValueAndIsBroken(result, CheckIsBroken(true));
+                    this.UpdateValueAndIsBroken(result, this.CheckIsBroken(true));
                 }
                 // else should we do something to say it is broken ?
-
             }
         }
 
-        bool TryInvokeGetMethod(object index, out object result)
+        private bool TryInvokeGetMethod(object index, out object result)
         {
             try
             {
-                result = _getMethod.Invoke(this.Source, new object[] { index });
+                result = this._getMethod.Invoke(this.Source, new object[] { index });
                 return true;
             }
             catch
@@ -104,44 +84,62 @@ namespace Windows.UI.Xaml.Data
         internal override void OnSourceChanged(object oldvalue, object newValue)
         {
             //todo: (?) find out how to have a listener here since it is a method and not a DependencyProperty (get_Item and set_Item). I guess it would be nice to be able to attach to calls on set_item and handle it from there.
-
-            //var listener = _dependencyPropertyListener;
-            //if (listener != null)
-            //{
-            //    listener.Detach();
-            //    _dependencyPropertyListener = listener = null;
-            //}
-
-            if (Source == null)
-                return;
-
-            if (newValue != null)
+            if (this.Source != null)
             {
-                Type sourceType = newValue.GetType();
-
-                _getMethod = GetMethod(sourceType, "get_Item");
-                _setMethod = GetMethod(sourceType, "set_Item");
+                if (newValue != null)
+                {
+                    this.GetIndexerInfo(newValue.GetType());
+                }
             }
         }
 
-        MethodInfo GetMethod(Type sourceType, string methodName)
+        private PropertyInfo GetIndexer(Type type)
         {
+            PropertyInfo singleParameterIndexer = type.GetProperties(BindingFlags.Public | BindingFlags.Instance).FirstOrDefault(property =>
+            {
+                return property.GetMethod != null && property.GetIndexParameters().Length == 1;
+            });
+            if (singleParameterIndexer == null)
+            {
+                throw new NotSupportedException("Indexers with multiple parameters are not supported yet for Binding.");
+            }
+            return singleParameterIndexer;
+        }
+
+        private void GetIndexerInfo(Type sourceType)
+        {
+            PropertyInfo indexer = this.GetIndexer(sourceType);
+            this.PrepareIndexForIndexer(indexer);
+            if (indexer != null)
+            {
+                this._getMethod = indexer.GetMethod;
+                this._setMethod = indexer.SetMethod;
+            }
+            else
+            {
+                this._getMethod = null;
+                this._setMethod = null;
+            }
+        }
+
+        private void PrepareIndexForIndexer(PropertyInfo indexer)
+        {
+            Type indexType = indexer.GetIndexParameters().First().ParameterType;
             try
             {
-                //Note: returns null if not found. 
-                return sourceType.GetMethod(methodName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static); //This will throw an ambiguous thingy exception if this[...] is defined more than once in the type.
+                this._parsedIndex = Convert.ChangeType(this._index, indexType);
             }
-            catch //todo: make it so that there is no need for the try/catch (so fix the comments in the catch.
+            catch (Exception)
             {
-                //we can have a System.Reflection.AmbiguousMatchException here if the type has more than one implementation for this[..]
-                //for example:
-                //      -public int this[int index]
-                //      -public int this[int index1, int index2]
-
-                //Note: We cannot use Type.GetMethod(string name, BindingFlags bindingAttr, Type[] parameterTypes) because it does not exist in Bridge yet. (we could probably find out which one is the correct one using the format of _index).
-                return null;
+                if (TypeFromStringConverters.CanTypeBeConverted(indexType))
+                {
+                    this._parsedIndex = TypeFromStringConverters.ConvertFromInvariantString(indexType, this._index); //todo: maybe in try / catch ?
+                }
+                else
+                {
+                    this._parsedIndex = this._index; //todo: maybe null ?
+                }
             }
         }
-
     }
 }
