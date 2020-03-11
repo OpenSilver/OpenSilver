8b2719f4c2829a7903d7dda7faf352c2f3a94aed
CSHTML5 <contact@cshtml5.com>
2020-03-11 01:43:56 +0100

    Update license header

--- src/CSHTML5.Runtime/Windows.UI.Xaml.Media/GeometryParser.cs
+++ src/CSHTML5.Runtime/Windows.UI.Xaml.Media/GeometryParser.cs
@@ -1,4 +1,19 @@
-﻿//credits : https://github.com/dotnet/wpf/blob/master/src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/ParsersCommon.cs
+﻿
+
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+
+//credits : https://github.com/dotnet/wpf/blob/master/src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/ParsersCommon.cs
 
 using System;
 using System.Collections.Generic;
