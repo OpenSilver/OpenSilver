OpenSilver/OpenSilver

commit a0102672d983d3a3c061b0db535a7e3b011ad800

Fixed casting errors in GetActualValue() and added IsArray checks when setting or getting ArrayIndex in class INTERNAL_JSObjectReference

src/CSHTML5.Runtime/PublicAPI/Interop/INTERNAL_JSObjectReference.cs
@@ -19,7 +19,7 @@ using Bridge;
 using JSIL.Meta;
 #endif
 
-#if !BUILDINGDOCUMENTATION && !CSHTML5NETSTANDARD// We don't have the references to the "DotNetBrowser" web browser control when building the documentation.
+#if !BUILDINGDOCUMENTATION// We don't have the references to the "DotNetBrowser" web browser control when building the documentation.
 using DotNetBrowser;
 #endif
 
@@ -38,7 +38,23 @@ namespace CSHTML5.Types
         public object Value { get; set; }
         public string ReferenceId { get; set; }
         public bool IsArray { get; set; }
-        public int ArrayIndex { get; set; } // Note: this property applies only if "IsArray" is true.
+
+        private int _arrayIndex;
+        public int ArrayIndex
+        {
+            get
+            {
+                if (!IsArray)
+                    throw new InvalidOperationException("Cannot get index of non-array item");
+                return _arrayIndex;
+            }
+            set
+            {
+                if (!IsArray)
+                    throw new InvalidOperationException("Cannot set index of non-array item");
+                _arrayIndex = value;
+            }
+        } // Note: this property applies only if "IsArray" is true.
 
 #if BRIDGE
         [External] //we exclude this method
@@ -47,39 +63,23 @@ namespace CSHTML5.Types
 #endif
         public object GetActualValue()
         {
-            object result = null;
-
 #if !BUILDINGDOCUMENTATION // We don't have the references to the "DotNetBrowser" web browser control when building the documentation.
-            if (this.IsArray)
+            switch (Value)
             {
-#if CSHTML5NETSTANDARD
-                result = ((object[])this.Value)[this.ArrayIndex];
-#else
-                result = ((JSArray)this.Value)[this.ArrayIndex];
-#endif
-            }
-            else
-            {
-                result = this.Value;
-            }
-
-#if !CSHTML5NETSTANDARD
-            if (result is JSNumber)
-            {
-                result = ((JSNumber)result).GetNumber(); // This prevents the "InvalidCastException" with message "Unable to cast object of type 'DotNetBrowser.JSNumber' to type 'System.IConvertible'" that happened in the method "ToDouble" below.
-            }
-            else if (result is JSBoolean)
-            {
-                result = ((JSBoolean)result).GetBool();
-            }
-            else if (result is JSString)
-            {
-                result = ((JSString)result).GetString();
+                case JSArray castedValue:
+                    return castedValue[ArrayIndex];
+                case object[] castedValue:
+                    return castedValue[ArrayIndex];
+                case JSNumber castedValue:
+                    return castedValue.GetNumber(); // This prevents the "InvalidCastException" with message "Unable to cast object of type 'DotNetBrowser.JSNumber' to type 'System.IConvertible'" that happened in the method "ToDouble" below.
+                case JSBoolean castedValue:
+                    return castedValue.GetBool();
+                case JSString castedValue:
+                    return castedValue.GetString();
+                default:
+                    return Value;
             }
 #endif
-#endif
-
-            return result;
         }
 
         public bool IsUndefined()

commit 49f2587e0ba6f040952eb6d3031b438a53a2419d

Fixed regression created by the previous commit (a010267)

src/CSHTML5.Runtime/PublicAPI/Interop/INTERNAL_JSObjectReference.cs
@@ -64,20 +64,37 @@ namespace CSHTML5.Types
         public object GetActualValue()
         {
 #if !BUILDINGDOCUMENTATION // We don't have the references to the "DotNetBrowser" web browser control when building the documentation.
-            switch (Value)
+            object result;
+            
+            if (IsArray)
             {
-                case JSArray castedValue:
-                    return castedValue[ArrayIndex];
-                case object[] castedValue:
-                    return castedValue[ArrayIndex];
-                case JSNumber castedValue:
-                    return castedValue.GetNumber(); // This prevents the "InvalidCastException" with message "Unable to cast object of type 'DotNetBrowser.JSNumber' to type 'System.IConvertible'" that happened in the method "ToDouble" below.
-                case JSBoolean castedValue:
-                    return castedValue.GetBool();
-                case JSString castedValue:
-                    return castedValue.GetString();
+                switch (Value)
+                {
+                    case JSArray castedValue:
+                        result = castedValue[ArrayIndex];
+                        break;
+                    case object[] castedValue:
+                        result = castedValue[ArrayIndex];
+                        break;
+                    default:
+                        throw new InvalidOperationException("Value is marked as array but is neither an object[] nor a JSArray");
+                }
+            }
+            else
+            {
+                result = Value;
+            }
+            
+            switch (result)
+            {
+                case JSNumber castedResult:
+                    return castedResult.GetNumber(); // This prevents the "InvalidCastException" with message "Unable to cast object of type 'DotNetBrowser.JSNumber' to type 'System.IConvertible'" that happened in the method "ToDouble" below.
+                case JSBoolean castedResult:
+                    return castedResult.GetBool();
+                case JSString castedResult:
+                    return castedResult.GetString();
                 default:
-                    return Value;
+                    return result;
             }
 #endif
         }