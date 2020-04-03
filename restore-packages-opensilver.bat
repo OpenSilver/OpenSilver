5585543db31443a11af28a6928cf9660d89b8c8b
Maxime Duriez <maxime.duriez@cshtml5.com>
2020-04-03 14:28:54 +0200

    [OpenSilver] Updated OpenSilver version used for the project and made some QoL changes to the csproj

--- restore-packages-opensilver.bat
+++ restore-packages-opensilver.bat
@@ -1,4 +1,4 @@
-build\nuget\nuget.exe install OpenSilver -Version 1.0.0-alpha-002 -OutputDirectory src\packages
-build\nuget\nuget.exe install OpenSilver.UwpCompatible -Version 1.0.0-alpha-002 -OutputDirectory src\packages
-build\nuget\nuget.exe install OpenSilver.WorkInProgress -Version 1.0.0-alpha-002 -OutputDirectory src\packages
+build\nuget\nuget.exe install OpenSilver -Version 1.0.0-alpha-003 -OutputDirectory src\packages
+build\nuget\nuget.exe install OpenSilver.UwpCompatible -Version 1.0.0-alpha-003 -OutputDirectory src\packages
+build\nuget\nuget.exe install OpenSilver.WorkInProgress -Version 1.0.0-alpha-003 -OutputDirectory src\packages
 pause
