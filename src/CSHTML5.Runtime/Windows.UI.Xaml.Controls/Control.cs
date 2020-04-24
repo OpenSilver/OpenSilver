e00f30d595e90e4c8205568b39d47ed562c8ea8c
Pierre Barjon <barjon.pierre@gmail.com>
2020-04-24 19:13:36 +0200

    Fix incorrect using statement while in non-migration configuration

--- src/CSHTML5.Runtime/Windows.UI.Xaml.Controls/Control.cs
+++ src/CSHTML5.Runtime/Windows.UI.Xaml.Controls/Control.cs
@@ -19,15 +19,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Data;
 #if MIGRATION
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using System.Windows.Data;
 #else
 using Windows.UI.Xaml.Media;
 using Windows.UI.Text;
+using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Input;
 #endif
