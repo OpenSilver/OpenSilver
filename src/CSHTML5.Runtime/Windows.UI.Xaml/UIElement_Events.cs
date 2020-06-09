4c3b9a2b5628cda2562fe562dfd149a23c07133d
Sylvain <t0ast@hotmail.fr>
2020-06-09 18:20:36 +0200

    Revert "Fix (un)checking CheckBoxes and RadioButtons through touch, as well as scrolling that starts on them on mobile devices."
    This reverts commit 0b9271ee1255aff447adecadcba7180c28160fff.

--- src/CSHTML5.Runtime/Windows.UI.Xaml/UIElement_Events.cs
+++ src/CSHTML5.Runtime/Windows.UI.Xaml/UIElement_Events.cs
@@ -193,8 +193,7 @@ namespace Windows.UI.Xaml
 #else
                     _pointerPressedEventManager = new INTERNAL_EventManager<PointerEventHandler, PointerRoutedEventArgs>(() => this.INTERNAL_OuterDomElement, eventsNames, (jsEventArg) =>
                     {
-                        
-                        ProcessPointerEvent(jsEventArg, (Action<PointerRoutedEventArgs>)OnPointerPressed, (Action<PointerRoutedEventArgs>)OnPointerPressed_ForHandledEventsToo, preventTextSelectionWhenPointerIsCaptured: false, checkForDivsThatAbsorbEvents: true, refreshClickCount: true);
+                        ProcessPointerEvent(jsEventArg, (Action<PointerRoutedEventArgs>)OnPointerPressed, (Action<PointerRoutedEventArgs>)OnPointerPressed_ForHandledEventsToo, preventTextSelectionWhenPointerIsCaptured: true, checkForDivsThatAbsorbEvents: true, refreshClickCount: true);
                     });
 #endif
                 }
