58090df91de76d053386a8b0c8b43958dbdda086
Pierre Barjon <barjon.pierre@gmail.com>
2020-06-12 21:39:10 +0200

    Added stubs for PointAnimationUsingKeyFrames.

---
+++ src/CSHTML5.Runtime/Windows.UI.Xaml.Media.Animation/WORKINPROGRESS/PointAnimationUsingKeyFrames.cs
@@ -0,0 +1,47 @@
+﻿#if WORKINPROGRESS
+
+using System.Windows.Markup;
+
+#if MIGRATION
+namespace System.Windows.Media.Animation
+#else
+namespace Windows.UI.Xaml.Media.Animation
+#endif
+{
+    [ContentProperty("KeyFrames")]
+    public sealed class PointAnimationUsingKeyFrames : Timeline
+    {
+        private PointKeyFrameCollection _keyFrames;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PointAnimationUsingKeyFrames"/>
+        /// class.
+        /// </summary>
+        public PointAnimationUsingKeyFrames()
+        {
+
+        }
+        
+        /// <summary>
+        /// Gets the collection of <see cref="PointKeyFrame"/> objects that
+        /// define the animation.
+        /// </summary>
+        /// <returns>
+        /// The collection of <see cref="PointKeyFrame"/> objects that define
+        /// the animation. The default is an empty collection.
+        /// </returns>
+        public PointKeyFrameCollection KeyFrames
+        {
+            get 
+            {  
+                if (this._keyFrames == null)
+                {
+                    this._keyFrames = new PointKeyFrameCollection();
+                }
+                return this._keyFrames;
+            }
+        }
+    }
+}
+
+#endif
---
+++ src/CSHTML5.Runtime/Windows.UI.Xaml.Media.Animation/WORKINPROGRESS/PointKeyFrameCollection.cs
@@ -0,0 +1,48 @@
+﻿#if WORKINPROGRESS
+
+#if MIGRATION
+namespace System.Windows.Media.Animation
+#else
+namespace Windows.UI.Xaml.Media.Animation
+#endif
+{
+    public class PointKeyFrameCollection : PresentationFrameworkCollection<PointKeyFrame>
+    {
+        internal override void AddOverride(PointKeyFrame value)
+        {
+            this.AddDependencyObjectInternal(value);
+        }
+
+        internal override void ClearOverride()
+        {
+            this.ClearDependencyObjectInternal();
+        }
+
+        internal override PointKeyFrame GetItemOverride(int index)
+        {
+            return this.GetItemInternal(index);
+        }
+
+        internal override void InsertOverride(int index, PointKeyFrame value)
+        {
+            this.InsertDependencyObjectInternal(index, value);
+        }
+
+        internal override void RemoveAtOverride(int index)
+        {
+            this.RemoveAtDependencyObjectInternal(index);
+        }
+
+        internal override bool RemoveOverride(PointKeyFrame value)
+        {
+            return this.RemoveDependencyObjectInternal(value);
+        }
+
+        internal override void SetItemOverride(int index, PointKeyFrame value)
+        {
+            this.SetItemDependencyObjectInternal(index, value);
+        }
+    }
+}
+
+#endif
