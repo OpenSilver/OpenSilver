609577ffdcfdf9c2066d881b8548dd30aabc5011
Maxime Duriez <maxime.duriez@cshtml5.com>
2020-06-19 17:56:46 +0200

    Fixed typo in ProgressIndicatorNamespace namespace

--- src/Simulator/Simulator/ProgressIndicator.cs
+++ src/Simulator/Simulator/ProgressIndicator.cs
@@ -6,7 +6,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Threading;
 
-namespace ProgessIndicatorNamespace
+namespace ProgressIndicatorNamespace
 {
     [TemplatePart(Name = ElementCanvas, Type = typeof(Canvas))]
     public class ProgressIndicator : RangeBase
