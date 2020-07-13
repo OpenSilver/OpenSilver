e2152f6e3729c64beae72ae0c94d618020f216d0
Maxime Duriez <maxime.duriez@cshtml5.com>
2020-07-13 17:49:50 +0200

    Changed CSHTML5 projects configurations to match the solution configurations. Changed CSHTML5 assembly names to match the OpenSilver naming scheme in order to have an universal NuGet generation script scheme. Adapted the CSHTML5 nuspec to the new NuGet generation system and the new file structure.

---
+++ build/Nuget.CSHTML5.bat
@@ -0,0 +1,34 @@
+@echo off
+rem define the escape character for colored text
+for /F %%a in ('"prompt $E$S & echo on & for %%b in (1) do rem"') do set "ESC=%%a"
+
+set /p PackageVersion="%ESC%[92mCSHTML5 version:%ESC%[0m 2.0.0-alpha"
+
+rem echo. 
+rem echo %ESC%[95mBuilding %ESC%[0mMigration %ESC%[95mconfiguration%ESC%[0m
+rem echo. 
+rem msbuild ../src/CSHTML5.sln -p:Configuration=Migration -restore
+rem echo. 
+rem echo %ESC%[95mPacking %ESC%[0mCSHTML5.Migration %ESC%[95mNuGet package%ESC%[0m
+rem echo. 
+rem nuget.exe pack data\CSHTML5.nuspec -OutputDirectory "output" -Properties "PackageId=CSHTML5.Migration;PackageVersion=2.0.0-alpha%PackageVersion%;Configuration=Migration;AssembliesPrefix=CSHTML5.Migration"
+
+rem echo. 
+rem echo %ESC%[95mBuilding %ESC%[0mMigration.WorkInProgress %ESC%[95mconfiguration%ESC%[0m
+rem echo. 
+rem msbuild ../src/CSHTML5.sln -p:Configuration=Migration.WorkInProgress -restore
+rem echo. 
+rem echo %ESC%[95mPacking %ESC%[0mCSHTML5.Migration.WorkInProgress %ESC%[95mNuGet package%ESC%[0m
+rem echo. 
+rem nuget.exe pack data\CSHTML5.nuspec -OutputDirectory "output" -Properties "PackageId=CSHTML5.Migration.WorkInProgress;PackageVersion=2.0.0-alpha%PackageVersion%;Configuration=Migration.WorkInProgress;AssembliesPrefix=CSHTML5.Migration"
+
+echo. 
+echo %ESC%[95mBuilding %ESC%[0mDebug %ESC%[95mconfiguration%ESC%[0m
+echo. 
+msbuild ../src/CSHTML5.sln -p:Configuration=Debug -restore
+echo. 
+echo %ESC%[95mPacking %ESC%[0mCSHTML5 %ESC%[95mNuGet package%ESC%[0m
+echo. 
+nuget.exe pack data\CSHTML5.nuspec -OutputDirectory "output" -Properties "PackageId=CSHTML5;PackageVersion=2.0.0-alpha%PackageVersion%;Configuration=Debug;AssembliesPrefix=CSHTML5"
+
+pause
---
+++ build/data/install.ps1
@@ -0,0 +1,83 @@
+﻿#First some common params, delivered by the nuget package installer
+param($installPath, $toolsPath, $package, $project)
+
+Write-Host ("CSHTML5 is configuring " + $project.ProjectName)
+
+# Remove all references of object
+ForEach ($item in $project.Object.References)
+{
+    if ($item.Identity.ToLower().StartsWith("system") -or $item.Identity.ToLower().StartsWith("microsoft") -or $item.Identity.ToLower().StartsWith("csharpxamlforhtml5") -or $item.Identity.ToLower().StartsWith("slmigration.csharpxamlforhtml5") -or $item.Identity.ToLower().StartsWith("jsil.meta"))
+    {
+        $name = $item.Identity
+        Try
+        {
+            $item.Remove()
+            Write-Host ("Removed Reference to " + $name)
+        }
+        Catch
+        {
+            Write-Host ("Failed to remove Reference to " + $name)
+        }
+    }
+}
+
+# Sets the NoStdLib setting to True for every project configuration.
+$project.ConfigurationManager | ForEach-Object {
+
+  # Check for <NoStdLib>
+  $nostdlib_setting = $_.Properties.Item("NoStdLib")
+
+  if (-not $nostdlib_setting.Value) 
+  {
+    $nostdlib_setting.Value = $true
+  }
+
+
+  # Check for AddAdditionalExplicitAssemblyReferences?
+  # Check for AdditionalExplicitAssemblyReferences?
+}
+
+#------------------------------------------------------------------------
+# Remove the import to the target of the legacy CSHTML5 1.x (JSIL-based):
+#------------------------------------------------------------------------
+
+$targetsFileName = 'CSharpXamlForHtml5.Build.targets';
+
+# Need to load MSBuild assembly if it's not loaded yet.
+Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+
+# Grab the loaded MSBuild project for the project
+$msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+
+# Remove imports to .targets
+$msbuild.Xml.Imports | Where-Object {$_.Project.ToLowerInvariant().EndsWith($targetsFileName.ToLowerInvariant()) } | Foreach { 
+	$_.Parent.RemoveChild( $_ ) 
+	[string]::Format( "Removed import of '{0}'" , $_.Project )
+}
+
+#------------------------------------------------------------------------
+# Set the "StartProgram" to point to the path of the Simulator:
+#------------------------------------------------------------------------
+
+$configurationManager = $project.ConfigurationManager
+foreach ($name in $configurationManager.ConfigurationRowNames)
+{
+    $projectConfigurations = $configurationManager.ConfigurationRow($name)
+    foreach ($projectConfiguration in $projectConfigurations)
+    {
+		$newStartAction = 1
+        [String]$newStartProgram = $toolsPath + "\simulator\CSharpXamlForHtml5.Simulator.exe"
+		Write-Host "Changing project start action to " $newStartAction
+        Write-Host "Changing project start program to " $newStartProgram              
+		$projectConfiguration.Properties.Item("StartAction").Value = $newStartAction
+        $projectConfiguration.Properties.Item("StartProgram").Value = $newStartProgram
+    }
+}
+#$project.Save
+Write-Host ("Configured start program")
+
+#------------------------------------------------------------------------
+# Final message:
+#------------------------------------------------------------------------
+
+Write-Host ("CSHTML5 package installation was successful")
