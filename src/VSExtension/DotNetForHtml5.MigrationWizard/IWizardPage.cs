483363665b0834576c448843a40185fdbca64908
Maxime Duriez <maxime.duriez@cshtml5.com>
2020-07-15 23:45:55 +0200

    Added projects for building CSHTML5 & OpenSilver Visual Studio extensions.

---
+++ src/VSExtension/DotNetForHtml5.MigrationWizard/IWizardPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DotNetForHtml5.MigrationWizard
+{
+    interface IWizardPage
+    {
+        bool TryCreateNextPage(Context context, out Page nextPage);
+
+        bool CanGoBack { get; }
+    }
+}
---
+++ src/VSExtension/DotNetForHtml5.VisualStudioExtension.ShellIntegration/PkgCmdID.cs
@@ -0,0 +1,12 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace Userware.DotNetForHtml5_VisualStudioExtension_ShellIntegration
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidMigrateSilverlight =        0x100;
+
+    };
+}
