6e0b10a295d752a149d873e45cd368e9e4e33213
cshtml5 <contact@cshtml5.com>
2020-08-11 18:32:51 +0200

    Add license header to the Compiler projects

--- src/Compiler/Compiler/2_ConvertingXamlToCSharp/GettingInformationAboutAXamlType.cs
+++ src/Compiler/Compiler/2_ConvertingXamlToCSharp/GettingInformationAboutAXamlType.cs
@@ -1,4 +1,21 @@
-﻿#if !BRIDGE && !CSHTML5BLAZOR
+﻿
+
+/*===================================================================================
+* 
+*   Copyright (c) Userware (OpenSilver.net, CSHTML5.com)
+*      
+*   This file is part of both the OpenSilver Compiler (https://opensilver.net), which
+*   is licensed under the MIT license (https://opensource.org/licenses/MIT), and the
+*   CSHTML5 Compiler (http://cshtml5.com), which is dual-licensed (MIT + commercial).
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+
+
+#if !BRIDGE && !CSHTML5BLAZOR
 extern alias custom;
 #endif
 extern alias wpf;
--- src/Compiler/Compiler/6_CreatingAndCopyingSupportHtmlAndJavaScriptFiles/WrappingOutputFiles_BridgeVersion.cs
+++ src/Compiler/Compiler/6_CreatingAndCopyingSupportHtmlAndJavaScriptFiles/WrappingOutputFiles_BridgeVersion.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿
+
+/*===================================================================================
+* 
+*   Copyright (c) Userware (OpenSilver.net, CSHTML5.com)
+*      
+*   This file is part of both the OpenSilver Compiler (https://opensilver.net), which
+*   is licensed under the MIT license (https://opensource.org/licenses/MIT), and the
+*   CSHTML5 Compiler (http://cshtml5.com), which is dual-licensed (MIT + commercial).
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
