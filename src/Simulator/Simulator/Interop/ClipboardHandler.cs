0616e7719d4780b8bd280c351a09909429ab4362
cshtml5 <contact@cshtml5.com>
2020-08-11 18:42:20 +0200

    Add license header to Simulator project

--- src/Simulator/Simulator/Interop/ClipboardHandler.cs
+++ src/Simulator/Simulator/Interop/ClipboardHandler.cs
@@ -1,4 +1,21 @@
-﻿using System.Windows;
+﻿
+
+/*===================================================================================
+* 
+*   Copyright (c) Userware (OpenSilver.net, CSHTML5.com)
+*      
+*   This file is part of both the OpenSilver Simulator (https://opensilver.net), which
+*   is licensed under the MIT license (https://opensource.org/licenses/MIT), and the
+*   CSHTML5 Simulator (http://cshtml5.com), which is dual-licensed (MIT + commercial).
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+
+
+using System.Windows;
 
 namespace DotNetForHtml5.EmulatorWithoutJavascript
 {
