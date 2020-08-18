b9d3e68c2650bdf9716ac6eb10d2cedcb185c5b3
cshtml5 <contact@cshtml5.com>
2020-08-18 14:57:34 +0200

    Add method "CSHTML5.Internal.WebRequestsHelper.MakeRequest" to make low-level web calls

---
+++ src/Runtime/Runtime/PublicAPI/Internal/WebRequestsHelper.cs
@@ -0,0 +1,29 @@
+﻿
+
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace CSHTML5.Internal
+{
+    public static class WebRequestsHelper
+    {
+        public static string MakeRequest(string address, string method, string body, Dictionary<string, string> headers = null, bool isAsync = false)
+        {
+            return new INTERNAL_WebRequestHelper_JSOnly().MakeRequest(new Uri(address), method,  null, headers, body, null, isAsync);
+        }
+    }
+}
