cfbca24950f4a60f90131ab0ab2234023abddbb4
cshtml5 <contact@cshtml5.com>
2020-09-01 01:42:07 +0200

    Update license header of the script files

--- src/Runtime/Scripts/cshtml5.js
+++ src/Runtime/Scripts/cshtml5.js
@@ -1,8 +1,17 @@
-//******************************************
-//           C#/XAML for HTML5
-//    Copyright 2019 Userware/CSHTML5
-//        http://www.cshtml5.com
-//******************************************
+
+
+/*===================================================================================
+*
+*   Copyright (c) Userware (OpenSilver.net, CSHTML5.com)
+*
+*   This file is part of both the OpenSilver Runtime (https://opensilver.net), which
+*   is licensed under the MIT license (https://opensource.org/licenses/MIT), and the
+*   CSHTML5 Runtime (http://cshtml5.com), which is dual-licensed (MIT + commercial).
+*
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*
+\*====================================================================================*/
 
 
 
