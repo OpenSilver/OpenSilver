143b4b5330a039aa345f11fd307c775d0f9b6fd8
Pierre Barjon <barjon.pierre@gmail.com>
2020-09-03 13:25:43 +0200

    Updated OpenSilver package's version used to build OpenSilver.

--- restore-packages-opensilver.bat
+++ restore-packages-opensilver.bat
@@ -1,4 +1,4 @@
-build\nuget.exe install OpenSilver -Version 1.0.0-alpha-005 -OutputDirectory src\packages
-build\nuget.exe install OpenSilver.UwpCompatible -Version 1.0.0-alpha-005 -OutputDirectory src\packages
-build\nuget.exe install OpenSilver.WorkInProgress -Version 1.0.0-alpha-005 -OutputDirectory src\packages
+build\nuget.exe install OpenSilver -Version 1.0.0-alpha-006 -OutputDirectory src\packages
+build\nuget.exe install OpenSilver.UwpCompatible -Version 1.0.0-alpha-006 -OutputDirectory src\packages
+build\nuget.exe install OpenSilver.WorkInProgress -Version 1.0.0-alpha-006 -OutputDirectory src\packages
 pause
