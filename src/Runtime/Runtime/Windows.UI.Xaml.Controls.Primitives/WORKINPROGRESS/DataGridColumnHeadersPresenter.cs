b4dff6d292d8c4830b79d97d575a7ffe05c31bee
Iakov Lilo <jlilo@hwdtech.ru>
2020-10-30 13:16:36 +0600

    Add DataGrid primitives
    * DataGridCellsPresenter
    * DataGridColumnHeadersPresenter
    * DataGridDetailsPresenter
    * DataGridFrozenGrid
    * DataGridRowHeader
    * DataGridRowsPresenter

---
+++ src/Runtime/Runtime/Windows.UI.Xaml.Controls.Primitives/WORKINPROGRESS/DataGridColumnHeadersPresenter.cs
@@ -0,0 +1,31 @@
+﻿
+
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+
+#if WORKINPROGRESS
+
+#if MIGRATION
+namespace System.Windows.Controls.Primitives
+#else
+namespace Windows.UI.Xaml.Controls.Primitives
+#endif
+{
+    /// <summary>
+    /// Used within the template of a <see cref="T:System.Windows.Controls.DataGrid" /> to specify the location in the control's visual tree where the column headers are to be added.
+    /// </summary>
+    public sealed class DataGridColumnHeadersPresenter : Panel
+    {
+    }
+}
+#endif
