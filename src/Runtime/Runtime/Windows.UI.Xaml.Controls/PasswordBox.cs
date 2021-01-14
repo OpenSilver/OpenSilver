71764d206e41c8ee00917f83a8d7f588d9281df8
Sylvain Maillard <maillard.sy@gmail.com>
2021-01-14 15:38:51 +0100

    Ticket #1942: Fix appearance of PasswordBox with implicit styles sometimes broken. + Tabbing through a templated PasswordBox now goes through all the "TabStop" elements in the Template. This also fixes the tabbing skipping to the end of the tabbing order when templated PasswordBoxes got the focus.

--- src/Runtime/Runtime/Windows.UI.Xaml.Controls/PasswordBox.cs
+++ src/Runtime/Runtime/Windows.UI.Xaml.Controls/PasswordBox.cs
@@ -169,12 +169,50 @@ namespace Windows.UI.Xaml.Controls
             var passwordFieldStyle = INTERNAL_HtmlDomManager.CreateDomElementAppendItAndGetStyle("input", parentRef, this, out passwordField);
             //dynamic passwordField = INTERNAL_HtmlDomManager.CreateDomElementAndAppendIt("input", parentRef, this);
 
+            this.INTERNAL_OptionalSpecifyDomElementConcernedByFocus = passwordField;
+
             _passwordInputField = passwordField;
 
             domElementWhereToPlaceChildren = passwordField; // Note: this value is used by the Padding_Changed method to set the padding of the PasswordBox.
 
             if (isTemplated) //When templated, we remove the outlining that apears when the element has the focus:
             {
+                //if the child of this.INTERNAL_AdditionalOutsideDivForMargins has the tagname "INPUT", replace it with a div:
+                var additionalDivForMargins = this.INTERNAL_AdditionalOutsideDivForMargins;
+                
+                dynamic newOuterDomElement = Interop.ExecuteJavaScript(@"(function() {
+    var formerInputElement = $0.firstChild;
+    var outerDomElement = formerInputElement;
+    if(formerInputElement.tagName == 'INPUT') {
+        var replacement = document.createElement('div');
+        outerDomElement = replacement;
+        // copy the attributes:
+        for(var i=0; i<formerInputElement.attributes.length; ++i) {
+            var at = formerInputElement.attributes[i];
+            if(at.name != 'type' && at.name != 'tabIndex') //removing the type (which is now irrelevant) and the tabIndex (which would add the div to the tabbing sequence)
+            {
+                replacement.setAttribute(at.name, at.value);
+            }
+        }
+        // move the potential children to the replacement node:
+        while(formerInputElement.firstChild)
+        {
+            replacement.appendChild(formerInputElement.firstChild);
+        }
+        // replace the element:
+        formerInputElement.replaceWith(replacement);
+    }
+    return outerDomElement;
+})()", additionalDivForMargins);
+
+                if(!Interop.IsRunningInTheSimulator)
+                {
+                    //Note: we replaced the former <input> with a <div> in the DOM tree because it was created before knowing that there was a Template.
+                    //      In the Simulator, there is no need to do anything because INTERNAL_OuterDomElement is only linked to the DOM element through its id (which was copied in the replacement <div>).
+                    //      In the browser, INTERNAL_OuterDomElement is the Dom element itself so we need to replace it, so we do it here.
+                    INTERNAL_OuterDomElement = newOuterDomElement;
+                }
+
                 passwordFieldStyle.border = "transparent"; // This removes the border. We do not need it since we are templated
                 passwordFieldStyle.outline = "solid transparent"; // Note: this is to avoind having the weird border when it has the focus. I could have used outlineWidth = "0px" but or some reason, this causes the caret to not work when there is no text.
                 passwordFieldStyle.backgroundColor = "transparent";
@@ -185,6 +223,7 @@ namespace Windows.UI.Xaml.Controls
             passwordFieldStyle.height = "100%";
 
             INTERNAL_HtmlDomManager.SetDomElementAttribute(passwordField, "type", "password", forceSimulatorExecuteImmediately: true);
+
             //passwordField.type = "password";
 
             //-----------------------
@@ -243,15 +282,14 @@ namespace Windows.UI.Xaml.Controls
                 OnAfterApplyHorizontalAlignmentAndWidth();
                 OnAfterApplyVerticalAlignmentAndWidth();
 
-                //register to focusin events on the OuterDomElement so that we can "reroute" the focus on the contentEditable element.
-                CSHTML5.Interop.ExecuteJavaScript(@"$0.tabIndex = 32767; $0.addEventListener('focusin', $1)", this.INTERNAL_OuterDomElement, (Action<object>)PasswordBox_GotFocus);
-                //Note on the line above: 32767 is the maximum value commonly allowed in browsers (and can be considered the default value)
-                if (INTERNAL_HtmlDomManager.IsInternetExplorer())
-                {
-                    //workaround due to IE setting the focus at the end of the click, (or at least after the focusin event), which cancels the work done during focusin.
-                    //if I'm not mistaken, the click event happens even when we click on a child of the element so we're all good. (We had to fix the case where a Button had a TextBox inside of it, which is why I assumed that).
-                    CSHTML5.Interop.ExecuteJavaScript(@"$0.addEventListener('click', $1)", this.INTERNAL_OuterDomElement, (Action<object>)PasswordBox_GotFocus);
-                }
+                //Note about tabbing: In WPF and SL, the elements in the template other than the input field can have focus but the input field will get any keypress not handled by them
+                //                    For example, you can set the focus on a button included in the template by clicking on it and pressing space will cause a button press, but any character will be added to the Text of the PasswordBox (but the button retains the focus)
+                //                    WPF and SL are different in that in SL, every focusable control in the template can get focus through tabbing while in WPF, the whole control is considered as a single tab stop (tabbing into the PasswordBox will directly put the focus on the input element)
+                //                    BUT in SL, the input will be first in tabOrder (unless maybe if tabIndex is specifically set, I didn't try that) so if the template goes <Stackpanel><Button/><ContentPresenter/></StackPanel>, by tabbing it will go ContentPresenter first then the Button (example simplified without the names, and also WPF and SL do not use a ContentPresenter but a ScrollViewer or Decorator in which to put the input area)
+                //
+                //                    In our case, tabbing will go through the elements accessible through tabbing, without changing the order, and text will only be added when the <input> has focus. On click, the focus will be redirected to the <input>, unless the click was on an element that absorbs pointer events.
+               
+                CSHTML5.Interop.ExecuteJavaScript(@"$0.addEventListener('click', $1)", this.INTERNAL_OuterDomElement, (Action<object>)PasswordBox_GotFocus);
             }
 
             return passwordField;
@@ -259,13 +297,17 @@ namespace Windows.UI.Xaml.Controls
 
         void PasswordBox_GotFocus(object e)//object sender, RoutedEventArgs e)
         {
-            if (_passwordInputField != null)
+            bool ignoreEvent = Convert.ToBoolean(CSHTML5.Interop.ExecuteJavaScript("document.checkForDivsThatAbsorbEvents($0)", e));
+            if (!ignoreEvent)
             {
-                CSHTML5.Interop.ExecuteJavaScript(@"
+                if (_passwordInputField != null)
+                {
+                    CSHTML5.Interop.ExecuteJavaScript(@"
 if($1.target != $0) {
 $0.focus()
 }", _passwordInputField, e);
-                //NEW_SET_SELECTION(_tempSelectionStartIndex, _tempSelectionStartIndex + _tempSelectionLength);
+                    //NEW_SET_SELECTION(_tempSelectionStartIndex, _tempSelectionStartIndex + _tempSelectionLength);
+                }
             }
         }
 
