7760be3f27aa3b3d79084a2a6ee75b50fa3aa1a7
ndh <ndhelix@gmail.com>
2021-01-21 15:27:24 +0300

    SpinDirection

---
+++ src/Runtime/Runtime/Windows.UI.Xaml.Controls/WORKINPROGRESS/SpinDirection.cs
@@ -0,0 +1,26 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+using System.Windows;
+
+namespace System.Windows.Controls
+{
+    /// <summary>
+    /// Represents spin directions that could be initiated by the end-user.
+    /// </summary>
+    /// <QualityBand>Preview</QualityBand>
+    public enum SpinDirection
+    {
+        /// <summary>
+        /// Represents a spin initiated by the end-user in order to Increase a value.
+        /// </summary>
+        Increase = 0,
+
+        /// <summary>
+        /// Represents a spin initiated by the end-user in order to Decrease a value.
+        /// </summary>
+        Decrease = 1
+    }
+}
---
+++ src/Runtime/Runtime/Windows.UI.Xaml.Controls/WORKINPROGRESS/SpinEventArgs.cs
@@ -0,0 +1,32 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+using System.Windows;
+
+namespace System.Windows.Controls
+{
+    /// <summary>
+    /// Provides data for the Spinner.Spin event.
+    /// </summary>
+    /// <QualityBand>Preview</QualityBand>
+    public partial class SpinEventArgs : RoutedEventArgs
+    {
+        /// <summary>
+        /// Gets the SpinDirection for the spin that has been initiated by the 
+        /// end-user.
+        /// </summary>
+        public SpinDirection Direction { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the SpinEventArgs class.
+        /// </summary>
+        /// <param name="direction">Spin direction.</param>
+        public SpinEventArgs(SpinDirection direction)
+            : base()
+        {
+            Direction = direction;
+        }
+    }
+}
