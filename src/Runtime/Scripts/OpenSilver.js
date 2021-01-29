OpenSilver/OpenSilver

commit be6924f9350531046b9855bd9254b929670e2734

Add (the beginning of) an implementation for the UnhandledException event.

src/Runtime/Scripts/OpenSilver.js
@@ -64,8 +64,17 @@ window.onCallBack = {}
 window.onCallBack.OnCallbackFromJavaScript = function (callbackId, idWhereCallbackArgsAreStored, callbackArgsObject) {
 	try {
 		DotNet.invokeMethod("OpenSilver", "OnCallbackFromJavaScript", callbackId, idWhereCallbackArgsAreStored, callbackArgsObject);
-	} catch (e)	{
-		DotNet.invokeMethod("OpenSilver.UWPCompatible", "OnCallbackFromJavaScript", callbackId, idWhereCallbackArgsAreStored, "");
+    } catch (e) {
+        if (e.message.startsWith("System.ArgumentException: There is no loaded assembly with the name")) {
+            try {
+                DotNet.invokeMethod("OpenSilver.UWPCompatible", "OnCallbackFromJavaScript", callbackId, idWhereCallbackArgsAreStored, "");
+            } catch (ex) {
+                throw ex;
+            }
+        }
+        else {
+            throw e;
+        }
 	}	
 };
 window.onCallBack.OnCallbackFromJavaScriptError = function (idWhereCallbackArgsAreStored) {

src/Runtime/Runtime/Core/Main/INTERNAL_SimulatorExecuteJavaScript.cs
@@ -26,6 +26,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DotNetForHtml5.Core;
+using System.Windows;
 
 namespace CSHTML5.Internal
 {
@@ -275,32 +276,43 @@ namespace CSHTML5.Internal
         /// <param name="action">The action to execute before flushing the pending JS code.</param>
         public static void RunActionThenExecutePendingAsyncJSCodeExecutedDuringThatAction(Action action)
         {
+#if WORKINPROGRESS
+            try
+            { 
+#endif
             if (_isInsideMethodToRunAnActionAndThenExecuteItsPendingJS)
-            {
-                //-----------------------------
-                // This means that we have already planned an auto-flush, so we do
-                // not need to plan it again: we just execute the provided "action".
-                // This is usually the case when we are in a nested call, which
-                // means that this method is being execute from within itself.
-                //-----------------------------
+                {
+                    //-----------------------------
+                    // This means that we have already planned an auto-flush, so we do
+                    // not need to plan it again: we just execute the provided "action".
+                    // This is usually the case when we are in a nested call, which
+                    // means that this method is being execute from within itself.
+                    //-----------------------------
 
-                action();
-            }
-            else
-            {
-                _isInsideMethodToRunAnActionAndThenExecuteItsPendingJS = true;
+                    action();
+                }
+                else
+                {
+                    _isInsideMethodToRunAnActionAndThenExecuteItsPendingJS = true;
 
-                action();
+                    action();
 
-                _isInsideMethodToRunAnActionAndThenExecuteItsPendingJS = false;
+                    _isInsideMethodToRunAnActionAndThenExecuteItsPendingJS = false;
 
 #if OPTIMIZATION_LOG
                 Console.WriteLine("[OPTIMIZATION] Auto-flush");
 #endif
-                // After the action has finished execution, let's flush all the pending JavaScript calls if any:
-                ExecutePendingJavaScriptCode("AUTO-FLUSH");
+                    // After the action has finished execution, let's flush all the pending JavaScript calls if any:
+                    ExecutePendingJavaScriptCode("AUTO-FLUSH");
+                }
+#if WORKINPROGRESS
             }
+            catch (Exception e)
+            {
+                Application.Current.OnUnhandledException(e, false);
+            } 
+#endif
         }
 #endif
-            }
+    }
         }

src/Runtime/Runtime/PublicAPI/Interop/OnCallBack.cs
@@ -346,79 +346,82 @@ namespace CSHTML5.Internal
         {
             Action action = () =>
             {
-                //----------------------------------
-                // Get the C# callback from its ID:
-                //----------------------------------
-                Delegate callback = _dictionary[callbackId];
+                CSHTML5.Internal.INTERNAL_SimulatorExecuteJavaScript.RunActionThenExecutePendingAsyncJSCodeExecutedDuringThatAction
+                   (() =>
+                   {
+                       //----------------------------------
+                       // Get the C# callback from its ID:
+                       //----------------------------------
+                       Delegate callback = _dictionary[callbackId];
 
-                Type callbackType = callback.GetType();
-                Type[] callbackGenericArgs = null;
-                if (callbackType.IsGenericType)
-                {
-                    callbackGenericArgs = callbackType.GetGenericArguments();
-                    callbackType = callbackType.GetGenericTypeDefinition();
-                }
-                var callbackArgs = callbackArgsObject;
+                       Type callbackType = callback.GetType();
+                       Type[] callbackGenericArgs = null;
+                       if (callbackType.IsGenericType)
+                       {
+                           callbackGenericArgs = callbackType.GetGenericArguments();
+                           callbackType = callbackType.GetGenericTypeDefinition();
+                       }
+                       var callbackArgs = callbackArgsObject;
 
-                //--------------------
-                // Call the callback:
-                //--------------------
+                       //--------------------
+                       // Call the callback:
+                       //--------------------
 #if CATCH_INTEROP_EXCEPTIONS // Note: previously, when using the "Awesomium" browser control in the Simulator, we handled the exceptions because otherwise they got captured by the WebBrowser control, due to the fact that the callback is executed in the context of the WebBrowser control. This is no longer true with the "DotNetBrowser" control.
                 try
                 {
 #endif
-                try
-                {
-                    if (callbackType == typeof(Action))
-                    {
-                        DelegateDynamicInvoke(callback);
-                    }
-                    else if (callbackType == typeof(Action<>))
-                    {
-                        DelegateDynamicInvoke(callback, MakeArgumentsForCallback(1, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
-                    }
-                    else if (callbackType == typeof(Action<,>))
-                    {
-                        DelegateDynamicInvoke(callback, MakeArgumentsForCallback(2, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
-                    }
-                    else if (callbackType == typeof(Action<,,>))
-                    {
-                        DelegateDynamicInvoke(callback, MakeArgumentsForCallback(3, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
-                    }
-                    else if (callbackType == typeof(Action<,,,>))
-                    {
-                        DelegateDynamicInvoke(callback, MakeArgumentsForCallback(4, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
-                    }
-                    else if (callbackType == typeof(Action<,,,,>))
-                    {
-                        DelegateDynamicInvoke(callback, MakeArgumentsForCallback(5, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
-                    }
-                    else if (callbackType == typeof(Action<,,,,,>))
-                    {
-                        DelegateDynamicInvoke(callback, MakeArgumentsForCallback(6, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
-                    }
-                    else if (callbackType == typeof(Action<,,,,,,>))
-                    {
-                        DelegateDynamicInvoke(callback, MakeArgumentsForCallback(7, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
-                    }
-                    else if (callbackType == typeof(Action<,,,,,,,>))
-                    {
-                        DelegateDynamicInvoke(callback, MakeArgumentsForCallback(8, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
-                    }
-                    else if (callbackType == typeof(Action<,,,,,,,,>))
-                    {
-                        DelegateDynamicInvoke(callback, MakeArgumentsForCallback(9, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
-                    }
-                    else
-                        throw new Exception(string.Format("Callback type not supported: {0}  Please report this issue to support@cshtml5.com", callbackType.ToString()));
-                }
-                catch (Exception ex)
-                {
+                       try
+                       {
+                           if (callbackType == typeof(Action))
+                           {
+                               DelegateDynamicInvoke(callback);
+                           }
+                           else if (callbackType == typeof(Action<>))
+                           {
+                               DelegateDynamicInvoke(callback, MakeArgumentsForCallback(1, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
+                           }
+                           else if (callbackType == typeof(Action<,>))
+                           {
+                               DelegateDynamicInvoke(callback, MakeArgumentsForCallback(2, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
+                           }
+                           else if (callbackType == typeof(Action<,,>))
+                           {
+                               DelegateDynamicInvoke(callback, MakeArgumentsForCallback(3, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
+                           }
+                           else if (callbackType == typeof(Action<,,,>))
+                           {
+                               DelegateDynamicInvoke(callback, MakeArgumentsForCallback(4, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
+                           }
+                           else if (callbackType == typeof(Action<,,,,>))
+                           {
+                               DelegateDynamicInvoke(callback, MakeArgumentsForCallback(5, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
+                           }
+                           else if (callbackType == typeof(Action<,,,,,>))
+                           {
+                               DelegateDynamicInvoke(callback, MakeArgumentsForCallback(6, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
+                           }
+                           else if (callbackType == typeof(Action<,,,,,,>))
+                           {
+                               DelegateDynamicInvoke(callback, MakeArgumentsForCallback(7, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
+                           }
+                           else if (callbackType == typeof(Action<,,,,,,,>))
+                           {
+                               DelegateDynamicInvoke(callback, MakeArgumentsForCallback(8, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
+                           }
+                           else if (callbackType == typeof(Action<,,,,,,,,>))
+                           {
+                               DelegateDynamicInvoke(callback, MakeArgumentsForCallback(9, callbackId, idWhereCallbackArgsAreStored, callbackArgs, callbackGenericArgs));
+                           }
+                           else
+                               throw new Exception(string.Format("Callback type not supported: {0}  Please report this issue to support@cshtml5.com", callbackType.ToString()));
+                       }
+                       catch (Exception ex)
+                       {
 #if DEBUG
-                    Console.Error.WriteLine("DEBUG: OnCallBack: OnCallBackFromJavascript: " + ex);
+                           Console.Error.WriteLine("DEBUG: OnCallBack: OnCallBackFromJavascript: " + ex);
 #endif
-                    throw;
-                }
+                           throw;
+                       }
 #if CATCH_INTEROP_EXCEPTIONS // Note: previously, when using the "Awesomium" browser control in the Simulator, we handled the exceptions because otherwise they got captured by the WebBrowser control, due to the fact that the callback is executed in the context of the WebBrowser control. This is no longer true with the "DotNetBrowser" control.
                 }
                 catch (Exception ex) // We catch and handle the Exception because otherwise it gets captured by the WebBrowser control, due to the fact that the callback is executed in the context of the WebBrowser control.
@@ -448,6 +451,7 @@ namespace CSHTML5.Internal
                     }
                 }
 #endif
+                   });
             };
 
             if (Interop.IsRunningInTheSimulator_WorkAround)

src/Runtime/Runtime/Windows.UI.Xaml/WORKINPROGRESS/Application.cs
@@ -48,6 +48,14 @@ namespace Windows.UI.Xaml
         /// </summary>
         public event EventHandler<ApplicationUnhandledExceptionEventArgs> UnhandledException;
 
+        internal void OnUnhandledException(Exception exception, bool handled)
+        {
+            if(UnhandledException != null)
+            {
+                UnhandledException(this, new ApplicationUnhandledExceptionEventArgs(exception, handled));
+            }
+        }
+
         //        void RaiseUnhandledException(object jsElement)
         //        {
         //#if !MIGRATION

src/Runtime/Runtime/Windows.UI.Xaml/WORKINPROGRESS/ApplicationUnhandledExceptionEventArgs.cs
@@ -29,8 +29,9 @@ namespace Windows.UI.Xaml
         public bool Handled { get; set; } 
         
         public ApplicationUnhandledExceptionEventArgs(Exception ex, bool handled) 
-        { 
-        
+        {
+            ExceptionObject = ex;
+            Handled = handled;
         } 
     } 
 #endif

commit e98dcfa09c0abe3adfef339ec7417217a57b26ef

Fix potential regression introduced in commit be6924f.

src/Runtime/Scripts/OpenSilver.js
@@ -62,20 +62,11 @@ document.getElementsByTagName('head')[0].appendChild(velocityScript);
 
 window.onCallBack = {}
 window.onCallBack.OnCallbackFromJavaScript = function (callbackId, idWhereCallbackArgsAreStored, callbackArgsObject) {
-	try {
-		DotNet.invokeMethod("OpenSilver", "OnCallbackFromJavaScript", callbackId, idWhereCallbackArgsAreStored, callbackArgsObject);
+    try {
+        DotNet.invokeMethod("OpenSilver", "OnCallbackFromJavaScript", callbackId, idWhereCallbackArgsAreStored, callbackArgsObject);
     } catch (e) {
-        if (e.message.startsWith("System.ArgumentException: There is no loaded assembly with the name")) {
-            try {
-                DotNet.invokeMethod("OpenSilver.UWPCompatible", "OnCallbackFromJavaScript", callbackId, idWhereCallbackArgsAreStored, "");
-            } catch (ex) {
-                throw ex;
-            }
-        }
-        else {
-            throw e;
-        }
-	}	
+        DotNet.invokeMethod("OpenSilver.UWPCompatible", "OnCallbackFromJavaScript", callbackId, idWhereCallbackArgsAreStored, "");
+    }
 };
 window.onCallBack.OnCallbackFromJavaScriptError = function (idWhereCallbackArgsAreStored) {
     try {