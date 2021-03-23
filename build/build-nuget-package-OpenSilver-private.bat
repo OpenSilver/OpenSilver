8ba4086ec692483582ffee7848e019223965072b
Pierre Barjon <barjon.pierre@gmail.com>
2021-03-23 22:26:24 +0100

    Fix commit #9951f32e.

--- build/build-nuget-package-OpenSilver-private.bat
+++ build/build-nuget-package-OpenSilver-private.bat
@@ -35,7 +35,7 @@ msbuild ../src/OpenSilver.sln -p:Configuration=SL -restore
 echo. 
 echo %ESC%[95mPacking %ESC%[0mOpenSilver %ESC%[95mNuGet package%ESC%[0m
 echo. 
-nuget.exe pack nuspec\OpenSilver.nuspec -OutputDirectory "output/OpenSilver" -Properties "PackageId=OpenSilver;PackageVersion=1.0.0-private-%PackageVersion%;Configuration=SL;AssembliesPrefix=OpenSilver"
+nuget.exe pack nuspec\OpenSilver.nuspec -OutputDirectory "output/OpenSilver" -Properties "PackageId=OpenSilver;PackageVersion=1.0.0-private-%PackageVersion%;Configuration=SL;Target=OpenSilver"
 
 echo. 
 echo %ESC%[95mBuilding %ESC%[0mSL.WorkInProgress %ESC%[95mconfiguration%ESC%[0m
@@ -44,7 +44,7 @@ msbuild ../src/OpenSilver.sln -p:Configuration=SL.WorkInProgress
 echo. 
 echo %ESC%[95mPacking %ESC%[0mOpenSilver.WorkInProgress %ESC%[95mNuGet package%ESC%[0m
 echo. 
-nuget.exe pack nuspec\OpenSilver.nuspec -OutputDirectory "output/OpenSilver" -Properties "PackageId=OpenSilver.WorkInProgress;PackageVersion=1.0.0-private-%PackageVersion%;Configuration=SL.WorkInProgress;AssembliesPrefix=OpenSilver"
+nuget.exe pack nuspec\OpenSilver.nuspec -OutputDirectory "output/OpenSilver" -Properties "PackageId=OpenSilver.WorkInProgress;PackageVersion=1.0.0-private-%PackageVersion%;Configuration=SL.WorkInProgress;Target=OpenSilver"
 
 echo. 
 echo %ESC%[95mBuilding %ESC%[0mUWP %ESC%[95mconfiguration%ESC%[0m
@@ -53,7 +53,7 @@ msbuild ../src/OpenSilver.sln -p:Configuration=UWP
 echo. 
 echo %ESC%[95mPacking %ESC%[0mOpenSilver.UWPCompatible %ESC%[95mNuGet package%ESC%[0m
 echo. 
-nuget.exe pack nuspec\OpenSilver.nuspec -OutputDirectory "output/OpenSilver" -Properties "PackageId=OpenSilver.UWPCompatible;PackageVersion=1.0.0-private-%PackageVersion%;Configuration=UWP;AssembliesPrefix=OpenSilver.UWPCompatible"
+nuget.exe pack nuspec\OpenSilver.nuspec -OutputDirectory "output/OpenSilver" -Properties "PackageId=OpenSilver.UWPCompatible;PackageVersion=1.0.0-private-%PackageVersion%;Configuration=UWP;Target=OpenSilver.UwpCompatible"
 
 rem echo. 
 rem echo %ESC%[95mBuilding %ESC%[0mVSIX %ESC%[0m
