8e2308b404d8300e3ba848e41d6533c13541f273
tbancila-talentia <75732624+tbancila-talentia@users.noreply.github.com>
2021-04-02 19:09:50 +0300

    Add "ScrollBarAutomationPeer" class in WORKINPROGRESS

---
+++ src/Runtime/Runtime/Windows.UI.Xaml.Automation.Peers/WORKINPROGRESS/ScrollBarAutomationPeer.cs
@@ -0,0 +1,38 @@
+﻿#if WORKINPROGRESS
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if MIGRATION
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+#else
+using Windows.UI.Xaml.Automation.Provider;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives
+#endif
+
+#if MIGRATION
+namespace System.Windows.Automation.Peers
+#else
+namespace Windows.UI.Xaml.Automation.Peers
+#endif
+{
+    public class ScrollBarAutomationPeer : RangeBaseAutomationPeer
+    {
+        public ScrollBarAutomationPeer(ScrollBar owner)
+          : base((RangeBase)owner)
+        {
+        }
+
+        protected override string GetClassNameCore() => "ScrollBar";
+
+        protected override AutomationControlType GetAutomationControlTypeCore() => AutomationControlType.ScrollBar;
+
+        protected override Point GetClickablePointCore() => new Point(double.NaN, double.NaN);
+
+        protected override AutomationOrientation GetOrientationCore() => ((ScrollBar)this.Owner).Orientation != Orientation.Horizontal ? AutomationOrientation.Vertical : AutomationOrientation.Horizontal;
+    }
+}
+#endif
