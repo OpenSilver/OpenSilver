f595e8d1a452bcd74f5641fe577079f9a9421cdc
tbancila-talentia <75732624+tbancila-talentia@users.noreply.github.com>
2021-04-02 19:10:14 +0300

    Add "ElementNotAvailableException" class in WORKINPROGRESS

---
+++ src/Runtime/Runtime/Windows.UI.Xaml.Automation/WORKINPROGRESS/ElementNotAvailableException.cs
@@ -0,0 +1,19 @@
+﻿#if WORKINPROGRESS
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if MIGRATION
+namespace System.Windows.Automation
+#else
+namespace Windows.UI.Xaml.Automation
+#endif
+{
+	public partial class ElementNotAvailableException : Exception
+	{
+		public ElementNotAvailableException()
+		{
+		}
+	}
+}
+#endif
