5719673b0df3978f4280f808433c11e9cf13d4ec
tbancila-talentia <75732624+tbancila-talentia@users.noreply.github.com>
2021-04-02 15:28:51 +0300

    Add "Typography" class in WORKINPROGRESS

---
+++ src/Runtime/Runtime/Windows.UI.Xaml.Documents/WORKINPROGRESS/Typography.cs
@@ -0,0 +1,29 @@
+﻿#if WORKINPROGRESS
+
+#if MIGRATION
+namespace System.Windows.Documents
+#else
+namespace Windows.UI.Xaml.Documents
+#endif
+{
+    /// <summary>Provides access to a rich set of OpenType typography properties. This class cannot be inherited.</summary>
+    public static class Typography
+    {
+        public static readonly DependencyProperty CapitalsProperty = new DependencyProperty();
+
+        public static FontCapitals GetCapitals(DependencyObject element) => (FontCapitals)Typography.GetTypographyValue(element, Typography.CapitalsProperty);
+
+        public static void SetCapitals(DependencyObject element, FontCapitals value) => Typography.SetTypographyValue(element, Typography.CapitalsProperty, (object)value);
+
+        private static object GetTypographyValue(DependencyObject element, DependencyProperty property) => element != null ? element.GetValue(property) : throw new ArgumentNullException(nameof(element));
+
+        private static void SetTypographyValue(DependencyObject element, DependencyProperty property, object value)
+        {
+            if (element == null)
+                throw new ArgumentNullException(nameof(element));
+            element.SetValue(property, value);
+        }
+    }
+}
+
+#endif
