82e22279fb32dfbee52dba839d30ded679e51d5a
tbancila-talentia <75732624+tbancila-talentia@users.noreply.github.com>
2021-04-02 13:38:08 +0300

    Add "CustomPropertyValueEditor" into "System.Windows.Interactivity" namespace.

---
+++ src/Runtime/Runtime/System.Windows.Interactivity/CustomPropertyValueEditor.cs
@@ -0,0 +1,15 @@
+﻿#if MIGRATION
+
+namespace System.Windows.Interactivity
+{
+    public enum CustomPropertyValueEditor
+    {
+        Element,
+        Storyboard,
+        StateName,
+        ElementBinding,
+        PropertyBinding,
+    }
+}
+
+#endif
