c6c070cbad5975694cff52c6a1dd85364d068e29
Rezarick <41767224+Rezarick@users.noreply.github.com>
2021-04-13 16:37:32 +0200

    Implemented methods used by Telerik controls for determining the placement of popups. (#69)
    - Implemented TranslateTransform.Inverse.
    - Implemented TranslateTransform.TransformBounds.
    - Implemented TranslateTransform.TryTransform.
    - Implemented Rect.Intersect.
    - Added unit tests for the implemented methods.
    
    Co-authored-by: Rezarick <chatti.oussama@outlook.com>

--- src/Tests/Runtime.OpenSilver.Tests/Windows.UI.Xaml.Media/TranslateTransformTest.cs
+++ src/Tests/Runtime.OpenSilver.Tests/Windows.UI.Xaml.Media/TranslateTransformTest.cs
@@ -11,6 +11,10 @@
 \*====================================================================================*/
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -48,20 +52,27 @@ namespace Windows.UI.Xaml.Media.Tests
         public void TransformBounds()
         {
             var rect = new Rect(0, 0, 100, 100);
+
             var transform = new TranslateTransform { X = 100, Y = 200 };
+
             var result = transform.TransformBounds(rect);
+
             result.X.Should().Be(100);
             result.Y.Should().Be(200);
             result.Width.Should().Be(100);
             result.Height.Should().Be(100);
+            result.Width.Should().Be(100);
         }
 
         [TestMethod]
         public void TryTransform()
         {
             var point = new Point(0, 0);
+
             var transform = new TranslateTransform { X = 100, Y = 200 };
+
             var result = transform.TryTransform(point, out var outPoint);
+
             result.Should().BeTrue();
             outPoint.X.Should().Be(100);
             outPoint.Y.Should().Be(200);
