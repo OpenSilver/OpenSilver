8e9fb6e7d40c0ee59f21f82c477d3d14795760ea
Rezarick <chatti.oussama@outlook.com>
2021-04-14 10:12:42 +0200

    Changed the project Runtime.OpenSilver.Tests to target .NET framework 4.7.2 instead of .NET 5

---
+++ src/Tests/Runtime.OpenSilver.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Runtime.OpenSilver.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Runtime.OpenSilver.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b23b7a60-f774-4723-b25b-c25de4659cc6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
