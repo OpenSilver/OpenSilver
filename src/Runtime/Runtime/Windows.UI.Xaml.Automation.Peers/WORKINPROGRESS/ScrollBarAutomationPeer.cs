7f7344a8c945f84e9b175b0a7dd48241dd2f9cd9
Andi Vlad <andi.vlad@addedo.ch>
2021-04-15 20:20:35 +0300

    Revert "+ Fixed compilation error in the UWP configurations (UWP and UWP.WorkInProgress) in the files : "RangeBaseAutomationPeer.cs", "ScrollBarAutomationPeer.cs", "AutoCompleteBox.cs (Work in progress file)" and "Typography.cs""
    This reverts commit 45a9da073f1b9617607529c844f33ae842e23e88.

--- src/Runtime/Runtime/Windows.UI.Xaml.Automation.Peers/WORKINPROGRESS/ScrollBarAutomationPeer.cs
+++ src/Runtime/Runtime/Windows.UI.Xaml.Automation.Peers/WORKINPROGRESS/ScrollBarAutomationPeer.cs
@@ -8,10 +8,9 @@ using System.Windows.Automation.Provider;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 #else
-using Windows.Foundation;
 using Windows.UI.Xaml.Automation.Provider;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Controls.Primitives
 #endif
 
 #if MIGRATION
