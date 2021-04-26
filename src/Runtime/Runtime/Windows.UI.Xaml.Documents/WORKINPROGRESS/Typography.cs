6f70b21ace04f8ba5b0dc87d539a792d567279f1
Pierre Barjon <barjon.pierre@gmail.com>
2021-04-26 12:56:19 +0200

    Fix Typography.CapitalsProperty.

--- src/Runtime/Runtime/Windows.UI.Xaml.Documents/WORKINPROGRESS/Typography.cs
+++ src/Runtime/Runtime/Windows.UI.Xaml.Documents/WORKINPROGRESS/Typography.cs
@@ -8,25 +8,53 @@ namespace System.Windows.Documents
 namespace Windows.UI.Xaml.Documents
 #endif
 {
-    /// <summary>Provides access to a rich set of OpenType typography properties. This class cannot be inherited.</summary>
+    /// <summary>
+    /// Provides access to a rich set of OpenType typography properties. 
+    /// This class cannot be inherited.
+    /// </summary>
     [OpenSilver.NotImplemented]
     public static class Typography
     {
+        /// <summary>
+        /// Identifies the Typography.Capitals dependency property.
+        /// </summary>
         [OpenSilver.NotImplemented]
-        public static readonly DependencyProperty CapitalsProperty = new DependencyProperty();
+        public static readonly DependencyProperty CapitalsProperty =
+            DependencyProperty.RegisterAttached(
+                "Capitals",
+                typeof(FontCapitals),
+                typeof(Typography),
+                new PropertyMetadata(FontCapitals.Normal));
 
+        /// <summary>
+        /// Returns the value of the Typography.Capitals attached
+        /// property for a specified dependency object.
+        /// </summary>
         [OpenSilver.NotImplemented]
-        public static FontCapitals GetCapitals(DependencyObject element) => (FontCapitals)Typography.GetTypographyValue(element, Typography.CapitalsProperty);
+        public static FontCapitals GetCapitals(DependencyObject element)
+            => (FontCapitals)GetTypographyValue(element, Typography.CapitalsProperty);
 
+        /// <summary>
+        /// Sets the value of the Typography.Capitals attached property
+        /// for a specified dependency object.
+        /// </summary>
         [OpenSilver.NotImplemented]
-        public static void SetCapitals(DependencyObject element, FontCapitals value) => Typography.SetTypographyValue(element, Typography.CapitalsProperty, (object)value);
+        public static void SetCapitals(DependencyObject element, FontCapitals value) 
+            => Typography.SetTypographyValue(element, Typography.CapitalsProperty, (object)value);
 
-        private static object GetTypographyValue(DependencyObject element, DependencyProperty property) => element != null ? element.GetValue(property) : throw new ArgumentNullException(nameof(element));
+        private static object GetTypographyValue(DependencyObject element, DependencyProperty property)
+        {
+            if (element == null)
+                throw new ArgumentNullException(nameof(element));
+
+            return element.GetValue(property);
+        }
 
         private static void SetTypographyValue(DependencyObject element, DependencyProperty property, object value)
         {
             if (element == null)
                 throw new ArgumentNullException(nameof(element));
+
             element.SetValue(property, value);
         }
     }
