7b87803baff9dd3b59e73276102f1386d233837d
Maxime Duriez <maxime.duriez@cshtml5.com>
2021-05-04 17:38:58 +0200

    VSIX: Cleaned up OpenSilver templates.
    Made OpenSilver .Simulator templates compatible with the new way of using the Simulator.

---
+++ src/VSExtension/OpenSilverUwpApplicationTemplate/OpenSilverUwpApplication.Simulator/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using DotNetForHtml5.EmulatorWithoutJavascript;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			MainWindow mainWindow = new MainWindow();
+			mainWindow.Show();
+		}
+	}
+}
