6fbf1fe71e9b46ae6d873a7095fb34d676fee14f
obabaghayou <obabaghayou@opensilver.net>
2021-06-10 16:55:29 +0200

    Add partial classes to override abstract methods of  DataGridBoundColumn

---
+++ src/Runtime/Runtime/Windows.UI.Xaml.Controls/WORKINPROGRESS/DataGridComboBoxColumn.cs
@@ -0,0 +1,50 @@
+﻿
+
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+#if WORKINPROGRESS
+using System;
+using System.Collections.Generic;
+using CSHTML5.Native.Html.Controls;
+
+#if MIGRATION
+using System.Windows.Data;
+#else
+using Windows.UI.Xaml.Data;
+#endif
+
+#if MIGRATION
+namespace System.Windows.Controls
+#else
+namespace Windows.UI.Xaml.Controls
+#endif
+{
+    partial class DataGridComboBoxColumn 
+    {
+        protected override FrameworkElement GenerateEditingElement(DataGridCell cell, object dataItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override FrameworkElement GenerateElement(DataGridCell cell, object dataItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override object PrepareCellForEdit(FrameworkElement editingElement, RoutedEventArgs editingEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+#endif
---
+++ src/Runtime/Runtime/Windows.UI.Xaml.Controls/WORKINPROGRESS/DataGridHyperlinkColumn.cs
@@ -0,0 +1,63 @@
+﻿
+
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+#if WORKINPROGRESS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#if MIGRATION
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+#else
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
+#endif
+
+#if MIGRATION
+namespace System.Windows.Controls
+#else
+namespace Windows.UI.Xaml.Controls
+#endif
+{
+    /// <summary>
+    /// Represents a System.Windows.Controls.DataGrid column that hosts System.Uri
+    /// elements in its cells.
+    /// </summary>
+    public partial class  DataGridHyperlinkColumn
+    {
+        protected override FrameworkElement GenerateEditingElement(DataGridCell cell, object dataItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override FrameworkElement GenerateElement(DataGridCell cell, object dataItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override object PrepareCellForEdit(FrameworkElement editingElement, RoutedEventArgs editingEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+#endif
