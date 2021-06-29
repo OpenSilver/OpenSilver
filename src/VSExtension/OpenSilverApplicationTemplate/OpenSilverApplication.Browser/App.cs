08ffe66252b20713644d2be8267025620de8e0e5
Maxime Duriez <maxime.duriez@cshtml5.com>
2021-06-29 16:15:45 +0200

    refactor(VSIX): replaced .razor files with .cs files

---
+++ src/VSExtension/OpenSilverApplicationTemplate/OpenSilverApplication.Browser/App.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.CompilerServices;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace $safeprojectname$
+{
+    public class App : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenComponent<Router>(0);
+            builder.AddAttribute(1, "AppAssembly", RuntimeHelpers.TypeCheck(
+                typeof(Program).Assembly
+            ));
+            builder.AddAttribute(2, "PreferExactMatches", RuntimeHelpers.TypeCheck(
+                true
+            ));
+            builder.AddAttribute(3, "Found", (RenderFragment<RouteData>)(routeData => builder2 =>
+            {
+                builder2.OpenComponent<RouteView>(4);
+                builder2.AddAttribute(5, "RouteData", RuntimeHelpers.TypeCheck(
+                    routeData
+                ));
+                builder2.CloseComponent();
+            }
+                ));
+            builder.AddAttribute(7, "NotFound", (RenderFragment)(builder2 =>
+            {
+                builder2.OpenComponent<LayoutView>(8);
+                builder2.AddAttribute(9, "ChildContent", (RenderFragment)(builder3 =>
+                {
+                    builder3.AddMarkupContent(10, "<p>Sorry, there\'s nothing at this address.</p>");
+                }
+                    ));
+                builder2.CloseComponent();
+            }
+                ));
+            builder.CloseComponent();
+        }
+    }
+}
