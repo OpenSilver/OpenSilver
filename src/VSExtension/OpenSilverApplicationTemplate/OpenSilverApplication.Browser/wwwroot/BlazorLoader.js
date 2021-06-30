5fc6d059bc0ff2c3e5e6e0609a6a306b6b5591b5
Maxime Duriez <maxime.duriez@cshtml5.com>
2021-06-30 18:00:17 +0200

    feat(VSIX): Added Showcase's Silverlight loading screen to the templates

---
+++ src/VSExtension/OpenSilverApplicationTemplate/OpenSilverApplication.Browser/wwwroot/BlazorLoader.js
@@ -0,0 +1,28 @@
+﻿function onResourceLoaded(resourceIndex, totalResources)
+{
+    document.getElementById("silverlight-loading-percentage").innerHTML = Math.round((resourceIndex / totalResources) * 100) + "%";
+}
+
+var i = 0;
+var allResourcesBeingLoaded = [];
+Blazor.start({ // start manually with loadBootResource
+    loadBootResource: function (type, name, defaultUri, integrity) {
+        if (type == "dotnetjs")
+            return defaultUri;
+
+        var fetchResources = fetch(defaultUri, {
+            cache: 'no-cache',
+            integrity: integrity,
+            headers: { 'MyCustomHeader': 'My custom value' }
+        });
+
+
+        allResourcesBeingLoaded.push(fetchResources);
+        fetchResources.then((r) => {
+            i++;
+            var total = allResourcesBeingLoaded.length;
+            onResourceLoaded(i, total);
+        });
+        return fetchResources;
+    }
+});
