89d0eaa9b3a41aaa73bc1434c7d36b785761e958
Maxime Duriez <maxime.duriez@cshtml5.com>
2021-07-01 16:28:06 +0200

    feat(Simulator): Added a message box about disabling Screencast in DevTools

---
+++ src/Simulator/Simulator/Debugging/DevToolsScreencastInfoWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace DotNetForHtml5.EmulatorWithoutJavascript.Debugging
+{
+    /// <summary>
+    /// Interaction logic for DevToolsScreencastInfoWindow.xaml
+    /// </summary>
+    public partial class DevToolsScreencastInfoWindow : Window
+    {
+        public DevToolsScreencastInfoWindow(Window owner)
+        {
+            InitializeComponent();
+            Owner = owner;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
