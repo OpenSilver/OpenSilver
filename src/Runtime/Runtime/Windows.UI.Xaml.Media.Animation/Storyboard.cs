OpenSilver/OpenSilver

commit 13c85d8b53e106a4b98ac6b94d5a7c42e8662029

Fix cases where going in and out of a VisualState with animation Timelines that use Velocity with a duration of 0 would sometimes apply the resulting value after the next VisualState change, making it look like the short-lived VisualState is still applied.

src/Runtime/Runtime/Windows.UI.Xaml.Media.Animation/Storyboard.cs
@@ -43,6 +43,21 @@ namespace Windows.UI.Xaml.Media.Animation
         private Dictionary<Tuple<string, string>, Timeline> INTERNAL_propertiesChanged; //todo: change this into a Hashset.
 
         internal bool isUnApplied = false; // Note: we set this variable because the animation start is done inside a Dispatcher, so if the user Starts then Stops the animation immediately (in the same thread), we want to cancel the start of the animation.
+        internal bool IsUnapplied
+        {
+            get { return isUnApplied; }
+            set
+            {
+                isUnApplied = value;
+                foreach (Timeline tl in _children)
+                {
+                    if (tl is AnimationTimeline)
+                    {
+                        ((AnimationTimeline)tl)._isUnapplied = value;
+                    }
+                }
+            }
+        }
 
 
         private TimelineCollection _children = new TimelineCollection();

src/Runtime/Runtime/Windows.UI.Xaml.Media.Animation/AnimationTimeline.cs
@@ -37,6 +37,11 @@ namespace Windows.UI.Xaml.Media.Animation
         internal DependencyProperty _propDp;
         internal bool _isInitialized = false;
         internal bool _cancelledAnimation = false;
+        /// <summary>
+        /// This variable is here to let us know when the animation has been cancelled before it ended and so it should not apply the final value to the C# when receiving the callback from Velocity.
+        ///  An example where it is useful is with a Control with a "MouseOver" VisualState that sets the opacity of an element to 1 though a DoubleAnimation (with a duration of 0) and we move the cursor in and out of the Control very quickly.
+        /// </summary>
+        internal bool _isUnapplied = true;
         protected override Duration GetNaturalDurationCore()
         {
             return new TimeSpan(0, 0, 1);

src/Runtime/Runtime/Windows.UI.Xaml.Media.Animation/ColorAnimation.cs
@@ -201,11 +201,14 @@ namespace Windows.UI.Xaml.Media.Animation
         {
             return () =>
             {
-                if (isLastLoop && _animationID == callBackGuid)
+                if (!this._isUnapplied)
                 {
-                    AnimationHelpers.ApplyValue(target, propertyPath, value, parameters.IsVisualStateChange);
+                    if (isLastLoop && _animationID == callBackGuid)
+                    {
+                        AnimationHelpers.ApplyValue(target, propertyPath, value, parameters.IsVisualStateChange);
+                    }
+                    OnIterationCompleted(parameters);
                 }
-                OnIterationCompleted(parameters);
             };
         }
 

src/Runtime/Runtime/Windows.UI.Xaml.Media.Animation/DoubleAnimation.cs
@@ -167,11 +167,14 @@ namespace Windows.UI.Xaml.Media.Animation
         {
             return () =>
             {
-                if (isLastLoop && _animationID == callBackGuid)
+                if (!this._isUnapplied)
                 {
-                    AnimationHelpers.ApplyValue(target, propertyPath, value, parameters.IsVisualStateChange);
+                    if (isLastLoop && _animationID == callBackGuid)
+                    {
+                        AnimationHelpers.ApplyValue(target, propertyPath, value, parameters.IsVisualStateChange);
+                    }
+                    OnIterationCompleted(parameters);
                 }
-                OnIterationCompleted(parameters);
             };
         }
 

src/Runtime/Runtime/Windows.UI.Xaml.Media.Animation/DoubleAnimationUsingKeyFrames.cs
@@ -212,13 +212,16 @@ namespace Windows.UI.Xaml.Media.Animation
         {
             return () =>
             {
-                if (_animationID == callBackGuid)
+                if (!this._isUnapplied)
                 {
-                    AnimationHelpers.ApplyValue(target, propertyPath, value, parameters.IsVisualStateChange);
-                    _appliedKeyFramesCount++;
-                    if (!CheckTimeLineEndAndRaiseCompletedEvent(_parameters))
+                    if (_animationID == callBackGuid)
                     {
-                        ApplyKeyFrame(GetNextKeyFrame(), isLastLoop);
+                        AnimationHelpers.ApplyValue(target, propertyPath, value, parameters.IsVisualStateChange);
+                        _appliedKeyFramesCount++;
+                        if (!CheckTimeLineEndAndRaiseCompletedEvent(_parameters))
+                        {
+                            ApplyKeyFrame(GetNextKeyFrame(), isLastLoop);
+                        }
                     }
                 }
             };

commit e415cab8800bfb64a5a2c8df13eee2dfd16d16af

Fix regression introduced in commit 13c85d8.

src/Runtime/Runtime/Windows.UI.Xaml.Media.Animation/Storyboard.cs
@@ -42,13 +42,13 @@ namespace Windows.UI.Xaml.Media.Animation
     {
         private Dictionary<Tuple<string, string>, Timeline> INTERNAL_propertiesChanged; //todo: change this into a Hashset.
 
-        internal bool isUnApplied = false; // Note: we set this variable because the animation start is done inside a Dispatcher, so if the user Starts then Stops the animation immediately (in the same thread), we want to cancel the start of the animation.
-        internal bool IsUnapplied
+        private bool _isUnApplied = false; // Note: we set this variable because the animation start is done inside a Dispatcher, so if the user Starts then Stops the animation immediately (in the same thread), we want to cancel the start of the animation.
+        internal bool IsUnApplied
         {
-            get { return isUnApplied; }
+            get { return _isUnApplied; }
             set
             {
-                isUnApplied = value;
+                _isUnApplied = value;
                 foreach (Timeline tl in _children)
                 {
                     if (tl is AnimationTimeline)
@@ -184,7 +184,7 @@ namespace Windows.UI.Xaml.Media.Animation
 
         internal void Begin(FrameworkElement target, bool useTransitions, string visualStateGroupName, bool isVisualStateChange)
         {
-            this.isUnApplied = false; // Note: we set this variable because the animation start is done inside a Dispatcher, so if the user synchronously Starts then Stops then Starts an animation, we want it to be in the started state.
+            this.IsUnApplied = false; // Note: we set this variable because the animation start is done inside a Dispatcher, so if the user synchronously Starts then Stops then Starts an animation, we want it to be in the started state.
 #if MIGRATION
             Dispatcher
 #else
@@ -197,7 +197,7 @@ namespace Windows.UI.Xaml.Media.Animation
                 try
                 {
 #endif
-                    if (!this.isUnApplied) // Note: we use this variable because the animation start is done inside a Dispatcher, so if the user Starts then Stops the animation immediately (in the same thread), we want to cancel the start of the animation.
+                    if (!this._isUnApplied) // Note: we use this variable because the animation start is done inside a Dispatcher, so if the user Starts then Stops the animation immediately (in the same thread), we want to cancel the start of the animation.
                     {
                         Guid guid = Guid.NewGuid();
                         IterationParameters parameters = new IterationParameters()

src/Runtime/Runtime/Windows.UI.Xaml/VisualStateManager.cs
@@ -280,7 +280,7 @@ namespace Windows.UI.Xaml
                                 formerPropertiesChanged[formerProp].UnApply(control);
                             }
                         }
-                        lastState.Storyboard.isUnApplied = true;
+                        lastState.Storyboard.IsUnApplied = true;
                     }
                 }
 