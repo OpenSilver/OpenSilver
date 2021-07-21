e76465087fbfa17eb41b01f32c78e082e325845d
Maxime Duriez <maxime.duriez@cshtml5.com>
2021-07-21 15:06:29 +0200

    refactor(VSIX): replaced hard-coded OpenSilver version in csprojs with wizard variable

--- src/VSExtension/OpenSilver.TemplateWizards/AppCustomizationWizard.cs
+++ src/VSExtension/OpenSilver.TemplateWizards/AppCustomizationWizard.cs
@@ -89,6 +89,8 @@ namespace OpenSilver.TemplateWizards
                         break;
                 }
             }
+
+            replacementsDictionary.Add("$opensilverpackageversion$", "1.0.0-alpha-019");
         }
 
         public bool ShouldAddProjectItem(string filePath)
