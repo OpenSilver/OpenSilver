OpenSilver/OpenSilver

commit 64f0285ff1b415b6959695c4d89a468aa8208764

perf: optimize JavaScript interops (#160)

src/Runtime/Scripts/cshtml5.js
@@ -171,6 +171,101 @@ document.getElementByIdSafe = function (id) {
     return element;
 }
 
+document.setGridCollapsedDuetoHiddenColumn = function (id) {
+    const element = document.getElementById(id);
+    if (!element)
+        return;
+
+	if (element.getAttribute('data-isCollapsedDueToHiddenColumn' == true)){
+		element.style.overflow = 'visible';
+		element.setAttribute('data-isCollapsedDueToHiddenColumn', false);
+	}
+}
+
+document.setDisplayTableCell = function (id) {
+    const element = document.getElementById(id);
+    if (!element || element.tagName != 'SPAN')
+        return;
+
+    element.style.display = 'table-cell';
+}
+
+document.getActualWidthAndHeight = function (element) {
+	return (typeof element === 'undefined' || element === null) ? '0|0' : element['offsetWidth'].toFixed(3) + '|' + element['offsetHeight'].toFixed(3);
+}
+
+document.createElementSafe = function (tagName, id, parentElement, index) {
+	const newElement = document.createElement(tagName);
+
+	newElement.setAttribute("id", id);
+	if(index < 0 || index >= parentElement.children.length)	{
+		parentElement.appendChild(newElement);
+	}
+	else {
+		var nextSibling = parentElement.children[index];
+		parentElement.insertBefore(newElement, nextSibling);
+	}
+}
+
+document.set2dContextProperty = function (id, propertyName, propertyValue) {
+    const element = document.getElementById(id);
+    if (!element || element.tagName !== 'CANVAS')
+        return;
+
+    element.getContext('2d')[propertyName] = propertyValue;
+}
+
+document.invoke2dContextMethod = function (id, methodName, args) {
+    const element = document.getElementById(id);
+    if (!element || element.tagName !== 'CANVAS')
+        return undefined;
+    return CanvasRenderingContext2D.prototype[methodName].apply(element.getContext('2d'),
+        args.split(',')
+            .map(Function.prototype.call, String.prototype.trim)
+            .filter(i => i.length > 0));
+}
+
+document.setDomStyleProperty = function (id, propertyName, value) {
+    const element = document.getElementById(id);
+    if (!element)
+        return;
+
+    element.style[propertyName] = value;
+}
+
+document.removeEventListenerSafe = function (element, method, func) {
+	if (element){
+		element.removeEventListener(method, func);
+	}
+}
+
+document.addEventListenerSafe = function (element, method, func) {
+	if (element){
+		element.addEventListener(method, func);
+	}
+}
+
+document.eventCallback = function (callbackId, arguments) {
+	const argsArray = arguments;
+	const idWhereCallbackArgsAreStored = "callback_args_" + document.callbackCounterForSimulator++;
+	document.jsSimulatorObjectReferences[idWhereCallbackArgsAreStored] = argsArray;
+	setTimeout(
+		function() 
+		{{
+		   window.onCallBack.OnCallbackFromJavaScript(callbackId, idWhereCallbackArgsAreStored, argsArray);
+		}}
+		, 1);
+}
+
+document.errorCallback = function (error, IndexOfNextUnmodifiedJSCallInList) {
+	const idWhereErrorCallbackArgsAreStored = "callback_args_" + document.callbackCounterForSimulator++;
+	const argsArr = [];
+	argsArr[0] = error.message;
+	argsArr[1] = IndexOfNextUnmodifiedJSCallInList;
+	document.jsSimulatorObjectReferences[idWhereErrorCallbackArgsAreStored] = argsArr;
+	window.onCallBack.OnCallbackFromJavaScriptError(idWhereErrorCallbackArgsAreStored);
+}
+
 window.ViewInteropErrors = function () {
     for (var key in document.interopErrors) {
         console.log(`Unable to find element with id '${key}' (${document.interopErrors[key]} time(s)).`);
@@ -648,7 +743,7 @@ if (!Array.from) {
 
             // 16. Let k be 0.
             var k = 0;
-            // 17. Repeat, while k < len… (also steps a - h)
+            // 17. Repeat, while k < lenÂ… (also steps a - h)
             var kValue;
             while (k < len) {
                 kValue = items[k];
@@ -818,4 +913,3 @@ var jsilConfig = {
       "index"
     ]
 };
-

src/Runtime/Runtime/Core/Events/INTERNAL_EventsHelper.cs
@@ -55,8 +55,7 @@ namespace CSHTML5.Internal
         static void AttachEvent(string eventName, object domElementRef, HtmlEventProxy newProxy, Action<object> originalEventHandler)
         {
 #if !BUILDINGDOCUMENTATION
-
-            Interop.ExecuteJavaScriptAsync(@"$0.addEventListener($1, $2)", domElementRef, eventName, (Action<object>)newProxy.OnEvent);
+            Interop.ExecuteJavaScriptAsync(@"document.addEventListenerSafe($0, $1, $2)", domElementRef, eventName, (Action<object>)newProxy.OnEvent);
 
             /*
             DOMEventType eventType;
@@ -85,12 +84,7 @@ namespace CSHTML5.Internal
         internal static void DetachEvent(string eventName, object domElementRef, HtmlEventProxy proxy, Action<object> originalEventHandler)
         {
 #if !BUILDINGDOCUMENTATION
-
-            Interop.ExecuteJavaScriptAsync(@"
-                var element = $0;
-                if (element) {
-                    element.removeEventListener($1, $2)
-                }", domElementRef, eventName, (Action<object>)proxy.OnEvent);
+            Interop.ExecuteJavaScriptAsync(@"document.removeEventListenerSafe($0, $1, $2)", domElementRef, eventName, (Action<object>)proxy.OnEvent);
 
             /*
             DOMEventType eventType;

src/Runtime/Runtime/Core/Main/INTERNAL_SimulatorExecuteJavaScript.cs
@@ -231,11 +231,17 @@ namespace CSHTML5.Internal
 #endif
         static object PerformActualInteropCall(string javaScriptToExecute, string reasonForPerformingTheCallNow)
         {
-            javaScriptToExecute = "//---- START INTEROP (" + reasonForPerformingTheCallNow + ") ----"
-                + Environment.NewLine
-                + javaScriptToExecute
-                + Environment.NewLine
-                + "//---- END INTEROP (" + reasonForPerformingTheCallNow + ") ----";
+            if (EnableInteropLogging)
+            {
+                javaScriptToExecute = "//---- START INTEROP (" + reasonForPerformingTheCallNow + ") ----"
+                    + Environment.NewLine
+                    + javaScriptToExecute
+                    + Environment.NewLine
+                    + "//---- END INTEROP (" + reasonForPerformingTheCallNow + ") ----";
+            }
+            else
+                javaScriptToExecute = Environment.NewLine + javaScriptToExecute + Environment.NewLine;
+
             try
             {
 #if CSHTML5BLAZOR

src/Runtime/Runtime/Core/Rendering/INTERNAL_Html2dContextReference.cs
@@ -73,7 +73,7 @@ namespace CSHTML5.Internal
 
         void SetPropertyValue(string propertyName, string propertyValue)
         {
-            string javaScriptCodeToExecute = "var element = document.getElementByIdSafe(\"" + _domElementUniqueIdentifier + "\");if (element) { element.getContext('2d')." + propertyName + " = \"" + propertyValue + "\"; } ";
+            string javaScriptCodeToExecute = "document.set2dContextProperty(\"" + _domElementUniqueIdentifier + "\",\"" + propertyName + "\",\"" + propertyValue + "\")";
             //INTERNAL_SimulatorPerformanceOptimizer.QueueJavaScriptCode(javaScriptCodeToExecute);
             INTERNAL_SimulatorExecuteJavaScript.ExecuteJavaScriptAsync(javaScriptCodeToExecute);
         }
@@ -82,13 +82,8 @@ namespace CSHTML5.Internal
         {
             string methodArgumentsFormattedForJavaScript = string.Join(", ", args.Select(x => INTERNAL_HtmlDomManager.ConvertToStringToUseInJavaScriptCode(x)));
             string javaScriptCodeToExecute =
-                string.Format(@"
-var element = document.getElementByIdSafe(""{0}"");
-var returnValue;
-if (element) {{
-    returnValue = element.getContext('2d')[""{1}""]({2});
-}} 
-returnValue;", _domElementUniqueIdentifier, methodName, methodArgumentsFormattedForJavaScript);
+                string.Format(@"document.invoke2dContextMethod(""{0}"" , ""{1}"", ""{2}"")", 
+                        _domElementUniqueIdentifier, methodName, methodArgumentsFormattedForJavaScript);
             var result = CSHTML5.Interop.ExecuteJavaScriptAsync(javaScriptCodeToExecute);
             return result;
             //INTERNAL_SimulatorPerformanceOptimizer.QueueJavaScriptCode(javaScriptCodeToExecute);

src/Runtime/Runtime/Core/Rendering/INTERNAL_HtmlDomManager.cs
@@ -918,43 +918,15 @@ function(){
 
             string uniqueIdentifier = INTERNAL_HtmlDomUniqueIdentifiers.CreateNew();
 
+            Interop.ExecuteJavaScriptAsync(@"document.createElementSafe($0, $1, $2, $3)", domElementTag, uniqueIdentifier, parentRef, index);
+            INTERNAL_idsToUIElements.Add(uniqueIdentifier, associatedUIElement);
+
             if (parentRef is INTERNAL_HtmlDomElementReference)
             {
-                string parentUniqueIdentifier = ((INTERNAL_HtmlDomElementReference)parentRef).UniqueIdentifier;
-                string javaScriptToExecute = string.Format(@"
-var newElement = document.createElement(""{0}"");
-newElement.setAttribute(""id"", ""{1}"");
-var parentElement = document.getElementByIdSafe(""{2}"");
-if({3} < 0 || {3} >= parentElement.children.length)
-{{
-    parentElement.appendChild(newElement);
-}}
-else
-{{
-    var nextSibling = parentElement.children[{3}];
-    parentElement.insertBefore(newElement,nextSibling);
-}}
-", domElementTag, uniqueIdentifier, parentUniqueIdentifier, index);
-                ExecuteJavaScript(javaScriptToExecute);
-                INTERNAL_idsToUIElements.Add(uniqueIdentifier, associatedUIElement);
                 return new INTERNAL_HtmlDomElementReference(uniqueIdentifier, (INTERNAL_HtmlDomElementReference)parentRef);
             }
             else
             {
-                Interop.ExecuteJavaScriptAsync(@"
-var newElement = document.createElement($0);
-newElement.setAttribute(""id"", $1);
-if($3 < 0 || $3 >= $2.children.length)
-{
-    $2.appendChild(newElement);
-}
-else
-{
-    var nextSibling = $2.children[$3];
-    $2.insertBefore(newElement,nextSibling);
-}
-", domElementTag, uniqueIdentifier, parentRef,index);
-                INTERNAL_idsToUIElements.Add(uniqueIdentifier, associatedUIElement);
                 return new INTERNAL_HtmlDomElementReference(uniqueIdentifier, null); //todo: this breaks for the root control, but the whole logic will be replaced with simple "ExecuteJavaScript" calls in the future, so it will not be a problem.
             }
         }

src/Runtime/Runtime/Core/Rendering/INTERNAL_HtmlDomStyleReference.cs
@@ -103,7 +103,7 @@ namespace CSHTML5.Internal
 
         void SetStylePropertyValue(string propertyName, string propertyValue)
         {
-            string javaScriptCodeToExecute = "var element = document.getElementByIdSafe(\"" + Uid + "\");if (element) { element.style." + propertyName + " = \"" + propertyValue + "\"; } ";
+            string javaScriptCodeToExecute = string.Format(@"document.setDomStyleProperty(""{0}"", ""{1}"", ""{2}"")", Uid, propertyName, propertyValue);
             INTERNAL_SimulatorExecuteJavaScript.ExecuteJavaScriptAsync(javaScriptCodeToExecute);
         }
 

src/Runtime/Runtime/PublicAPI/Interop/INTERNAL_InteropImplementation.cs
@@ -152,17 +152,7 @@ namespace CSHTML5
 
                     // Change the JS code to point to that callback:
                     javascript = javascript.Replace("$" + i.ToString(), string.Format(
-                                       @"(function() {{
-                        var argsArray = {1};
-                        var idWhereCallbackArgsAreStored = ""callback_args_"" + document.callbackCounterForSimulator++;
-                        document.jsSimulatorObjectReferences[idWhereCallbackArgsAreStored] = argsArray;
-                        setTimeout(
-                            function() 
-                            {{
-                               window.onCallBack.OnCallbackFromJavaScript({0}, idWhereCallbackArgsAreStored, argsArray);
-                            }}
-                            , 1);
-                      }})", callbackId,
+                                       @"(function() {{ document.eventCallback({0}, {1});}})", callbackId,
 #if OPENSILVER
                                        Interop.IsRunningInTheSimulator_WorkAround ? "arguments" : "Array.prototype.slice.call(arguments)"
 #elif BRIDGE
@@ -207,13 +197,7 @@ namespace CSHTML5
 
             // Change the JS code to call ShowErrorMessage in case of error:
             string errorCallBack = string.Format(
-            @"var idWhereErrorCallbackArgsAreStored = ""callback_args_"" + document.callbackCounterForSimulator++;
-                var argsArr = [];
-                argsArr[0] = error.message;
-                argsArr[1] = {0};
-                document.jsSimulatorObjectReferences[idWhereErrorCallbackArgsAreStored] = argsArr;
-                window.onCallBack.OnCallbackFromJavaScriptError(idWhereErrorCallbackArgsAreStored);"
-                , IndexOfNextUnmodifiedJSCallInList);
+            @"document.errorCallback(error, {0})", IndexOfNextUnmodifiedJSCallInList);
             ++IndexOfNextUnmodifiedJSCallInList;
 
             // Surround the javascript code with some code that will store the
@@ -223,14 +207,11 @@ namespace CSHTML5
             javascript = string.Format(
 @"
 try {{
-var result = eval(""{0}"");
-document.jsSimulatorObjectReferences[""{1}""] = result;
-result;
+document.jsSimulatorObjectReferences[""{1}""] = eval(""{0}"");
 }}
 catch (error) {{
-    eval(""{2}"");
+    {2};
 }}
-result;
 ", INTERNAL_HtmlDomManager.EscapeStringForUseInJavaScript(javascript), referenceId, INTERNAL_HtmlDomManager.EscapeStringForUseInJavaScript(errorCallBack));
 
             // Execute the javascript code:

src/Runtime/Runtime/Windows.UI.Xaml.Controls/Grid_CSSVersion.cs
@@ -168,11 +168,9 @@ namespace Windows.UI.Xaml.Controls
                     else
                     {
                         // Note: we set to Visible only if it was previously Hidden due to the fact that a Grid column is hidden, to avoid conflicts such as replacing the "overflow" property set by the ScrollViewer or by the "ClipToBounds" property.
-                        CSHTML5.Interop.ExecuteJavaScriptAsync(@"
-if ($0.getAttribute('data-isCollapsedDueToHiddenColumn' == true)){
-    $0.style.overflow = 'visible';
-    $0.setAttribute('data-isCollapsedDueToHiddenColumn', false);
-}", uiElement.INTERNAL_OuterDomElement);
+                        //setAttribute('data-isCollapsedDueToHiddenColumn', false)
+                        CSHTML5.Interop.ExecuteJavaScriptAsync(@"document.setGridCollapsedDuetoHiddenColumn($0)", 
+                            ((INTERNAL_HtmlDomElementReference)uiElement.INTERNAL_OuterDomElement).UniqueIdentifier);
                     }
                 }
             }

commit 26cbfcbbced919224d7c5028f9d3ed77194930b6

Fix regression introduced in commit 64f0285.

src/Runtime/Scripts/cshtml5.js
@@ -184,7 +184,7 @@ document.setGridCollapsedDuetoHiddenColumn = function (id) {
 
 document.setDisplayTableCell = function (id) {
     const element = document.getElementById(id);
-    if (!element || element.tagName != 'SPAN')
+    if (!element || element.tagName == 'SPAN')
         return;
 
     element.style.display = 'table-cell';