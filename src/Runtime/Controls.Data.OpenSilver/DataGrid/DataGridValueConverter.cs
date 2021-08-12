98d077f4b6cf5c1ad8aa24a2bbe415b8fd151744
obabaghayou <obabaghayou@opensilver.net>
2021-08-12 23:05:48 +0200

    Move System.Controls.data to separated project

---
+++ src/Runtime/Controls.Data.OpenSilver/DataGrid/DataGridValueConverter.cs
@@ -0,0 +1,35 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace System.Windows.Controls
+{
+    internal class DataGridValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        // This suppresses a warning saying that we should use String.IsNullOrEmpty instead of a string
+        // comparison, but in this case we want to explicitly check for Empty and not Null.
+        [SuppressMessage("Microsoft.Performance", "CA1820:TestForEmptyStringsUsingStringLength")]
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != null && targetType.IsNullableType())
+            {
+                String strValue = value as String;
+                if (strValue == String.Empty)
+                {
+                    return null;
+                }
+            }
+            return value;
+        }
+    }
+}
