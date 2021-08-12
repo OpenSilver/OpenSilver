ed57defb80ee1ec717d53f7ade0a34c1dd3993a4
obabaghayou <obabaghayou@opensilver.net>
2021-08-12 23:05:48 +0200

    Move System.Controls.data to separated project

---
+++ src/Runtime/Controls.Data.OpenSilver/DataGrid/Automation/DataGridRowGroupHeaderAutomationPeer.cs
@@ -0,0 +1,52 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+using System.Windows.Controls;
+
+namespace System.Windows.Automation.Peers
+{
+    /// <summary>
+    /// AutomationPeer for DataGridRowGroupHeader
+    /// </summary>
+    /// <QualityBand>Preview</QualityBand>
+    public class DataGridRowGroupHeaderAutomationPeer : FrameworkElementAutomationPeer
+    {
+        #region Constructors
+
+        /// <summary>
+        /// AutomationPeer for DataGridRowGroupHeader
+        /// </summary>
+        /// <param name="owner">DataGridRowGroupHeader</param>
+        public DataGridRowGroupHeaderAutomationPeer(DataGridRowGroupHeader owner)
+            : base(owner)
+        {
+        }
+
+        #endregion
+
+        #region AutomationPeer Overrides
+
+        /// <summary>
+        /// Gets the control type for the element that is associated with the UI Automation peer.
+        /// </summary>
+        /// <returns>The control type.</returns>
+        protected override AutomationControlType GetAutomationControlTypeCore()
+        {
+            return AutomationControlType.Group;
+        }
+
+        /// <summary>
+        /// Called by GetClassName that gets a human readable name that, in addition to AutomationControlType, 
+        /// differentiates the control represented by this AutomationPeer.
+        /// </summary>
+        /// <returns>The string that contains the name.</returns>
+        protected override string GetClassNameCore()
+        {
+            return Owner.GetType().Name;
+        }
+
+        #endregion
+    }
+}
