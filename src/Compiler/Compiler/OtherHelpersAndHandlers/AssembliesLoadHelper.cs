03340c1483b78ede7f1d90e158aba47a8944ae2b
Rezarick <chatti.oussama@outlook.com>
2021-09-14 11:05:03 +0200

    Fixed build issues in CSHTML5 and OpenSilver
    - Added XmlnsDefinition attribute in the OpenSilver.Xaml project
    - Added all files that where moved to OpenSilver.Xaml, as links in Runtime.CSHTML5.

--- src/Compiler/Compiler/OtherHelpersAndHandlers/AssembliesLoadHelper.cs
+++ src/Compiler/Compiler/OtherHelpersAndHandlers/AssembliesLoadHelper.cs
@@ -32,7 +32,9 @@ namespace DotNetForHtml5.Compiler
         /// </summary>
         internal static IReadOnlyCollection<string> CoreAssembliesNames { get; } = new Collection<string>()
         {
+#if CSHTML5BLAZOR
             Constants.OPENSILVER_XAML_ASSEMBLY_NAME,
+#endif
             GetCoreAssemblyName(),
         };
 
@@ -41,6 +43,31 @@ namespace DotNetForHtml5.Compiler
             var fileName = Path.GetFileNameWithoutExtension(assemblyPath);
 
             return CoreAssembliesNames.Any(assemblyName => assemblyName.Equals(fileName, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        /// <summary>
+        /// Ensures that the core CSHTML5 assembly is the first among the provided list of assemblies.
+        /// This is useful to ensure that types such as "XmlnsDefinitionAttribute" are resolved.
+        /// </summary>
+        /// <param name="assemblyPaths">An enumerable that contains the paths of some assemblies</param>
+        /// <returns>An enumerable that contains the same items as the original enumerable but in a different order</returns>
+        internal static IEnumerable<string> EnsureCoreAssemblyIsFirstInList(IEnumerable<string> assemblyPaths)
+        {
+            List<string> result = new List<string>();
+            string coreAssemblyNameLowercase = GetCoreAssemblyName().ToLower();
+            foreach (string assemblyPath in assemblyPaths)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(assemblyPath);
+                if (fileName.ToLower() == coreAssemblyNameLowercase)
+                {
+                    result.Insert(0, assemblyPath);
+                }
+                else
+                {
+                    result.Add(assemblyPath);
+                }
+            }
+            return result;
         }
 
         internal static string GetCoreAssemblyName()
