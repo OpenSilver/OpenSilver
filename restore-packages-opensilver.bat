b0ae62171b4fc9bdd4f63801cbe7e4f145f3d174
Pierre Barjon <barjon.pierre@gmail.com>
2021-09-21 13:29:51 +0200

    chore: bump package used to build runtime to version 1.0.0-beta.1.22

--- restore-packages-opensilver.bat
+++ restore-packages-opensilver.bat
@@ -1,3 +1,3 @@
-build\nuget.exe install OpenSilver -Version 1.0.0-alpha-019 -OutputDirectory src\packages -Source https://api.nuget.org/v3/index.json
-build\nuget.exe install OpenSilver.UwpCompatible -Version 1.0.0-alpha-019 -OutputDirectory src\packages -Source https://api.nuget.org/v3/index.json
+build\nuget.exe install OpenSilver -Version 1.0.0-beta.1.22 -OutputDirectory src\packages -Source https://api.nuget.org/v3/index.json
+build\nuget.exe install OpenSilver.UwpCompatible -Version 1.0.0-beta.1.22 -OutputDirectory src\packages -Source https://api.nuget.org/v3/index.json
 pause
