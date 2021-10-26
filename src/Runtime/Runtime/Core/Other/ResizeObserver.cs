3a865ffd7659e3abe49738130f14b2c9784bc144
Pierre Barjon <barjon.pierre@gmail.com>
2021-10-26 22:45:02 +0200

    chore: fix UWP configuration

--- src/Runtime/Runtime/Core/Other/ResizeObserver.cs
+++ src/Runtime/Runtime/Core/Other/ResizeObserver.cs
@@ -1,7 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+using System;
+
+#if MIGRATION
 using System.Windows;
+#else
+using Windows.Foundation;
+using Windows.UI.Xaml;
+#endif
 
 namespace CSHTML5.Internal
 {
