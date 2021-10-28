355d734f30de2c91e715658e57774a13d1c2cb9c
Pierre Barjon <barjon.pierre@gmail.com>
2021-10-28 13:59:44 +0200

    chore: add navigation controls to the default xaml namespace

--- src/Runtime/Controls.Navigation/Properties/AssemblyInfo.cs
+++ src/Runtime/Controls.Navigation/Properties/AssemblyInfo.cs
@@ -42,9 +42,17 @@ using System.Windows.Markup;
 [assembly: XmlnsPrefix("http://schemas.microsoft.com/winfx/2006/xaml/presentation/sdk", "sdk")]
 
 #if MIGRATION
+// Note: Silverlight does not define an XmlnsDefinition for the default xaml namespace, this
+// is only here for backward compatibility reasons
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Controls")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Navigation")]
+
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation/sdk", "System.Windows.Controls")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation/sdk", "System.Windows.Navigation")]
 #else
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "Windows.UI.Xaml.Controls")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "Windows.UI.Xaml.Navigation")]
+
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation/sdk", "Windows.UI.Xaml.Controls")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation/sdk", "Windows.UI.Xaml.Navigation")]
 #endif
