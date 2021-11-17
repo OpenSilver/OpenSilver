eec7ba7dfd8e6f20ebcee0ae6c3e5aa21e92bc3e
Bozhidar Stoyneff <bstoyneff@outlook.com>
2021-11-17 09:43:51 +0200

    FullScreenOptions created

---
+++ src/Runtime/Runtime/Windows.UI.Xaml/FullScreenOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+#if MIGRATION
+#if WORKINPROGRESS
+namespace System.Windows.Interop
+#else
+namespace System.Windows // Note: we didn't use the "Interop" namespace to avoid conflicts with CSHTML5.Interop
+#endif
+#else
+namespace Windows.UI.Xaml // Note: we didn't use the "Interop" namespace to avoid conflicts with CSHTML5.Interop
+#endif
+{
+	/// <summary>
+	/// Defines constants that indicate the behavior of full-screen mode. 
+	/// </summary>
+	[Flags]
+	public enum FullScreenOptions
+	{
+		/// <summary>
+		/// The application uses the default full-screen behavior.
+		/// </summary>
+		None = 0,
+		/// <summary>
+		/// The application does not exit full-screen mode when other applications gain focus. 
+		/// </summary>
+		StaysFullScreenWhenUnfocused = 1 << 0
+	}
+}
