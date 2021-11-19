cda9048225a8a63b170ceaca34d82c37854a000e
atolucky6 <61114829+atolucky6@users.noreply.github.com>
2021-11-20 00:07:20 +0700

    Replace App.cs to App.razor and App.razor.cs
    Change to razor file for easy implement another razor component to App

---
+++ src/VSExtension/OpenSilverApplicationTemplate/OpenSilverApplication.Browser/App.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.CompilerServices;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace $safeprojectname$
+{
+    public partial class App : ComponentBase
+    {
+    }
+}
