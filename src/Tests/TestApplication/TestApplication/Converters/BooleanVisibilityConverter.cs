6c6aeb9009c2617fe6e8e4b70aab3c9e6292df01
Iakov Lilo <jlilo@hwdtech.com>
2021-12-14 15:23:58 +0600

    fix: use default value for bindings with a broken path

---
+++ src/Tests/TestApplication/TestApplication/Converters/BooleanVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TestApplication.OpenSilver.Converters
+{
+    /// <summary>
+    /// A type converter for visibility and boolean values.
+    /// </summary>
+    public class BooleanVisibilityConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            var visibility = (bool)value;
+
+            return visibility ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            return ((Visibility)value == Visibility.Visible);
+        }
+    }
+}
