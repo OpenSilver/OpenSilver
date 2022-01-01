10c449542795badb7cb24b5258e4b7c5ada0b73c
KeyurJP <patel.keyur1123@gmail.com>
2022-01-01 14:12:42 +0530

    Missing properties of autocompletebox and datagridrowheader.

--- src/Runtime/Runtime/System.Windows.Controls.Primitives/WORKINPROGRESS/DataGridRowHeader.cs
+++ src/Runtime/Runtime/System.Windows.Controls.Primitives/WORKINPROGRESS/DataGridRowHeader.cs
@@ -13,8 +13,10 @@
 \*====================================================================================*/
 
 #if MIGRATION
+using System.Windows.Media;
 namespace System.Windows.Controls.Primitives
 #else
+using Windows.UI.Xaml.Media;
 namespace Windows.UI.Xaml.Controls.Primitives
 #endif
 {
@@ -24,5 +26,44 @@ namespace Windows.UI.Xaml.Controls.Primitives
     [OpenSilver.NotImplemented]
     public class DataGridRowHeader : ContentControl
     {
+        /// <summary>
+        /// Identifies the <see cref="P:System.Windows.Controls.Primitives.DataGridColumnHeader.SeparatorBrush" /> dependency property.
+        /// </summary>
+        /// <returns>
+        /// The identifier for the <see cref="P:System.Windows.Controls.Primitives.DataGridColumnHeader.SeparatorBrush" /> dependency property.
+        /// </returns>
+        [OpenSilver.NotImplemented]
+        public static readonly DependencyProperty SeparatorBrushProperty = DependencyProperty.Register(nameof(SeparatorBrush), typeof(Brush), typeof(DataGridColumnHeader), null);
+
+        /// <summary>
+        /// Gets or sets the <see cref="T:System.Windows.Media.Brush" /> used to paint the column header separator lines.
+        /// </summary>
+        /// <returns>
+        /// The brush used to paint column header separator lines.
+        /// </returns>
+        [OpenSilver.NotImplemented]
+        public Brush SeparatorBrush
+        {
+            get => GetValue(SeparatorBrushProperty) as Brush;
+            set => SetValue(SeparatorBrushProperty, value);
+        }
+
+        [OpenSilver.NotImplemented]
+        public static readonly DependencyProperty SeparatorVisibilityProperty = DependencyProperty.Register("SeparatorVisibility", typeof(Visibility), typeof(DataGridRowHeader), null);
+
+        [OpenSilver.NotImplemented]
+        /// <summary>Gets or sets a value indicating whether the row header separator lines are visible.</summary>
+        /// <returns>One of the enumeration values that specifies the visibility of the row header separator lines. The default is <see cref="F:System.Windows.Visibility.Visible" />.</returns>
+        public Visibility SeparatorVisibility
+        {
+            get
+            {
+                return (Visibility)GetValue(SeparatorVisibilityProperty);
+            }
+            set
+            {
+                SetValue(SeparatorVisibilityProperty, value);
+            }
+        }
     }
 }
