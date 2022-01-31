d158bf7a14c6bdf89f506b116eafa34a4c70ad5b
Andrey Arekhva <a.arekhva@gmail.com>
2022-01-31 17:11:11 +0300

    Add date validation error event

--- src/Runtime/Runtime/System.Windows.Controls/WORKINPROGRESS/DatePicker.cs
+++ src/Runtime/Runtime/System.Windows.Controls/WORKINPROGRESS/DatePicker.cs
@@ -89,7 +89,7 @@ namespace Windows.UI.Xaml.Controls
         //     Occurs when the drop-down System.Windows.Controls.Calendar is closed.
         [OpenSilver.NotImplemented]
         public event RoutedEventHandler CalendarClosed;
-        
+
 
         private static void OnDisplayDateStartChanged(
             DependencyObject d,
@@ -104,6 +104,13 @@ namespace Windows.UI.Xaml.Controls
         /// </summary>
         [OpenSilver.NotImplemented]
         public event RoutedEventHandler CalendarOpened;
+
+        /// <summary>
+        /// Occurs when <see cref="P:System.Windows.Controls.DatePicker.Text" /> is assigned
+        /// a value that cannot be interpreted as a date.
+        /// </summary>
+        [OpenSilver.NotImplemented]
+        public event EventHandler<DatePickerDateValidationErrorEventArgs> DateValidationError;
 
         #region DisplayDate
         /// <summary>
