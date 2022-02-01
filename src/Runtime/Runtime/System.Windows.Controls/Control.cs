5562d66149ae3971a91ce1b65830c845d1d0c169
roman-pikulenkor <82944181+roman-pikulenkor@users.noreply.github.com>
2022-02-02 09:33:42 +1000

    feat: implement Validation states at the Control level (#362)

--- src/Runtime/Runtime/System.Windows.Controls/Control.cs
+++ src/Runtime/Runtime/System.Windows.Controls/Control.cs
@@ -18,17 +18,18 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
-using System.Windows.Controls.Primitives;
 
 #if MIGRATION
 using System.Windows.Media;
 using System.Windows.Input;
 using System.Windows.Data;
+using System.Windows.Controls.Primitives;
 #else
 using Windows.UI.Text;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Input;
 using Windows.Foundation;
+using Windows.UI.Xaml.Controls.Primitives;
 #endif
 
 #if MIGRATION
@@ -900,12 +901,20 @@ namespace Windows.UI.Xaml.Controls
         {
             _isFocused = false;
             UpdateVisualStatesForFocus();
+
+            if (!this._isInvalid)
+                return;
+            this.UpdateValidationState();
         }
 
         void Control_GotFocus(object sender, RoutedEventArgs e)
         {
             _isFocused = true;
             UpdateVisualStatesForFocus();
+
+            if (!this._isInvalid)
+                return;
+            this.UpdateValidationState();
         }
 
         bool _isPointerOver = false;
@@ -1027,6 +1036,36 @@ void Control_PointerReleased(object sender, Input.PointerRoutedEventArgs e)
         internal void GoToState(string state)
         {
             VisualStateManager.GoToState(this, state, true);
+        }
+
+        private bool _isInvalid;
+
+        internal void ShowValidationError()
+        {
+            this._isInvalid = true;
+            this.UpdateValidationState();
+        }
+
+        internal void HideValidationError()
+        {
+            this._isInvalid = false;
+            this.UpdateValidationState();
+        }
+
+        internal void UpdateValidationState()
+        {
+            if (!this._isInvalid)
+                VisualStateManager.GoToState(this, "Valid", true);
+            else
+            {
+                VisualStateManager.GoToState(this, _isFocused ? "InvalidFocused" : "InvalidUnfocused", true);
+
+                Popup popup = this.INTERNAL_ValidationErrorPopup;
+                if (popup != null)
+                {
+                    popup.IsOpen = _isFocused;
+                }
+            }
         }
 
         [OpenSilver.NotImplemented]
