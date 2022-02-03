6393b67a1bb061c5e424bc3a5c3b168bc1e82552
Andrey Arekhva <a.arekhva@gmail.com>
2022-02-03 16:24:15 +0300

    chore: add stubs for InkPresenter control (#387)

---
+++ src/Runtime/Runtime/System.Windows.Controls/WORKINPROGRESS/InkPresenter.cs
@@ -0,0 +1,29 @@
+﻿#if MIGRATION
+using System.Windows.Ink;
+
+namespace System.Windows.Controls
+{
+    /// <summary>
+    /// Implements a rectangular surface that displays ink strokes.
+    /// </summary>
+    [OpenSilver.NotImplemented]
+    public class InkPresenter : Canvas
+    {
+        /// <summary>
+        /// Gets or sets the strokes that the <see cref="InkPresenter"/> displays.
+        /// </summary>
+        /// <returns>
+        /// The collection of ink strokes that are displayed by the <see cref="InkPresenter"/>.
+        /// </returns>
+        [OpenSilver.NotImplemented]
+        public StrokeCollection Strokes
+        {
+            get { return (StrokeCollection)GetValue(StrokesProperty); }
+            set { SetValue(StrokesProperty, value); }
+        }
+
+        public static readonly DependencyProperty StrokesProperty =
+            DependencyProperty.Register("Strokes", typeof(StrokeCollection), typeof(InkPresenter), new PropertyMetadata());
+    }
+}
+#endif
