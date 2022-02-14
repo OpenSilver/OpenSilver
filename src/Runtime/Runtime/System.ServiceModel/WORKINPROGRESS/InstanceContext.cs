d57b0e3389e6fedbb0fce2fe6be0a955ba50f0e7
Diego Villabrille Seca <diego.villabrille.seca@userware.onmicrosoft.com>
2022-02-14 14:13:15 +0100

    chore: add stub for CSHTML5_DuplexClientBase
    fix

---
+++ src/Runtime/Runtime/System.ServiceModel/WORKINPROGRESS/InstanceContext.cs
@@ -0,0 +1,64 @@
+﻿using System.ServiceModel.Channels;
+
+namespace System.ServiceModel
+{
+    public class InstanceContext : CommunicationObject, IExtensibleObject<InstanceContext>
+    {
+        [OpenSilver.NotImplemented]
+        public InstanceContext(object implementation)
+        {
+          
+        }
+
+        [OpenSilver.NotImplemented]
+        public IExtensionCollection<InstanceContext> Extensions => throw new NotImplementedException();
+
+        [OpenSilver.NotImplemented]
+        protected override TimeSpan DefaultCloseTimeout => throw new NotImplementedException();
+
+        [OpenSilver.NotImplemented]
+        protected override TimeSpan DefaultOpenTimeout => throw new NotImplementedException();
+        
+        [OpenSilver.NotImplemented]
+        protected override void OnAbort()
+        {
+
+        }
+
+        [OpenSilver.NotImplemented]
+        protected override IAsyncResult OnBeginClose(TimeSpan timeout, AsyncCallback callback, object state)
+        {
+            throw new NotImplementedException();
+        }
+
+        [OpenSilver.NotImplemented]
+        protected override IAsyncResult OnBeginOpen(TimeSpan timeout, AsyncCallback callback, object state)
+        {
+            throw new NotImplementedException();
+        }
+
+        [OpenSilver.NotImplemented]
+        protected override void OnClose(TimeSpan timeout)
+        {
+
+        }
+
+        [OpenSilver.NotImplemented]
+        protected override void OnEndClose(IAsyncResult result)
+        {
+
+        }
+
+        [OpenSilver.NotImplemented]
+        protected override void OnEndOpen(IAsyncResult result)
+        {
+
+        }
+
+        [OpenSilver.NotImplemented]
+        protected override void OnOpen(TimeSpan timeout)
+        {
+
+        }
+    }
+}
