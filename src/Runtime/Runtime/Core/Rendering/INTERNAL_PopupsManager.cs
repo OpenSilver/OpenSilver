3ef7285477db23c2df56d79e30c6b96782ff3cb2
mbaykalos <mehmet.baykal@userware.onmicrosoft.com>
2022-02-15 10:05:30 +0300

    feat: add custum size support for popup

--- src/Runtime/Runtime/Core/Rendering/INTERNAL_PopupsManager.cs
+++ src/Runtime/Runtime/Core/Rendering/INTERNAL_PopupsManager.cs
@@ -260,7 +260,7 @@ $1.removeChild(popupRoot);
             return new Point();
         }
 
-        public static void EnsurePopupStaysWithinScreenBounds(Popup popup)
+        public static void EnsurePopupStaysWithinScreenBounds(Popup popup, Size size = new Size())
         {
             if (popup.IsOpen
                 && popup.PopupRoot != null
@@ -268,7 +268,7 @@ $1.removeChild(popupRoot);
             {
                 // Determine the size of the popup:
                 FrameworkElement content = (FrameworkElement)popup.PopupRoot.Content;
-                double popupActualWidth = content.ActualWidth;
+                double popupActualWidth = size.Width != double.NaN ? size.Width : content.ActualWidth;
                 double popupActualHeight = content.ActualHeight;
                 if (!double.IsNaN(popupActualWidth)
                     && !double.IsNaN(popupActualHeight)
