c11635c5f45381d1afda2a2fbe090ecd5711cf88
Diego Villabrille Seca <44496227+dvs99@users.noreply.github.com>
2022-02-15 12:30:53 +0100

    chore: add PollingDuplexHttpBinding stubs (#405)

---
+++ src/Runtime/Runtime/System.ServiceModel.Channels/WORKINPROGRESS/PollingDuplexMode.cs
@@ -0,0 +1,35 @@
+﻿
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+namespace System.ServiceModel.Channels
+{
+    /// <summary>
+    /// Specifies the format of server responses to client polling when the communication
+    /// is configured for duplex polling.
+    /// </summary>
+    public enum PollingDuplexMode
+    {
+        /// <summary>
+        /// Specifies the server will return one message every time the client polls for
+        /// a message and then close the polling connection. This is the default value.
+        /// </summary>
+        SingleMessagePerPoll,
+
+        /// <summary>
+        /// Specifies the server keeps the connection open for as long there are messages
+        /// ready to be sent back to the client and sends as many messages back to the client
+        /// as it can over a chunked HTTP response.
+        /// </summary>
+        MultipleMessagesPerPoll
+    }
+}
---
+++ src/Runtime/Runtime/System.ServiceModel/WORKINPROGRESS/PollingDuplexHttpSecurityMode.cs
@@ -0,0 +1,50 @@
+﻿
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+using System.ServiceModel.Channels;
+
+namespace System.ServiceModel
+{
+    /// <summary>
+    /// Specifies the types of security that can be used with the system-provided <see cref="PollingDuplexHttpBinding"/>.
+    /// </summary>
+    public enum PollingDuplexHttpSecurityMode
+    {
+        /// <summary>
+        /// The SOAP message is not secured during transfer. This is the default behavior.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Security is provided using HTTPS. The service must be configured with SSL certificates.
+        /// The SOAP message is protected as a whole using HTTPS. The service is authenticated
+        /// by the client using the service’s SSL certificate.
+        /// </summary>
+        Transport = 1,
+
+        /// <summary>
+        /// This mode provides integrity, confidentiality, and server authentication using
+        /// HTTPS. The service must be configured with a certificate. Client authentication
+        /// is provided by SOAP message security.This mode is applicable when the user is
+        /// authenticating with a UserName or Certificate credential and there is an existing
+        /// HTTPS deployment for securing message transfer.
+        /// </summary>
+        TransportWithMessageCredential = 3,
+
+        /// <summary>
+        /// This mode provides only HTTP-based client authentication. It does not provide
+        /// message integrity or confidentiality.
+        /// </summary>
+        TransportCredentialOnly = 4,
+    }
+}
