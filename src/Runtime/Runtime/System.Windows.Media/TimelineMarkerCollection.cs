b3bd3477daf7e18d5f943c30ebb57119bab3a419
Diego Villabrille Seca <44496227+dvs99@users.noreply.github.com>
2022-02-18 11:11:20 +0100

    chore: add MediaElement stubs (#412)

---
+++ src/Runtime/Runtime/System.Windows.Media/TimelineMarkerCollection.cs
@@ -0,0 +1,62 @@
+﻿
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+using System;
+
+#if MIGRATION
+namespace System.Windows.Media
+#else
+namespace Windows.UI.Xaml.Media
+#endif
+{
+    /// <summary>
+    ///Represents a collection of <see cref="TimelineMarker"/> objects that can be individually accessed by index.
+    /// </summary>
+    public sealed class TimelineMarkerCollection : PresentationFrameworkCollection<TimelineMarker>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimelineMarkerCollection"/> class.
+        /// </summary>
+        public TimelineMarkerCollection() : base(false) { }
+
+        internal override void AddOverride(TimelineMarker value)
+        {
+            this.AddDependencyObjectInternal(value);
+        }
+
+        internal override void ClearOverride()
+        {
+            this.ClearDependencyObjectInternal();
+        }
+
+        internal override TimelineMarker GetItemOverride(int index)
+        {
+            return this.GetItemInternal(index);
+        }
+
+        internal override void InsertOverride(int index, TimelineMarker value)
+        {
+            this.InsertDependencyObjectInternal(index, value);
+        }
+
+        internal override void RemoveAtOverride(int index)
+        {
+            this.RemoveAtDependencyObjectInternal(index);
+        }
+
+        internal override void SetItemOverride(int index, TimelineMarker value)
+        {
+            this.SetItemDependencyObjectInternal(index, value);
+        }
+    }
+}
---
+++ src/Runtime/Runtime/System.Windows.Media/WORKINPROGRESS/TimelineMarker.cs
@@ -0,0 +1,100 @@
+﻿
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+using System;
+
+#if MIGRATION
+namespace System.Windows.Media
+#else
+namespace Windows.UI.Xaml.Media
+#endif
+{
+    /// <summary>
+    /// Represents metadata associated with a specific point in a media file.
+    /// </summary>
+    [OpenSilver.NotImplemented]
+    public sealed class TimelineMarker : DependencyObject
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimelineMarker"/> class.
+        /// </summary>
+        [OpenSilver.NotImplemented]
+        public TimelineMarker() { }
+
+        /// <summary>
+        /// Identifies the <see cref="Text"/> dependency property.
+        /// </summary>
+        [OpenSilver.NotImplemented]
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(
+                nameof(Text),
+                typeof(string),
+                typeof(TimelineMarker),
+                new PropertyMetadata((object)null));
+
+        /// <summary>
+        /// The text value of the <see cref="TimelineMarker"/>. 
+        /// The default value is null.
+        /// </summary>
+        [OpenSilver.NotImplemented]
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="Time"/> dependency property.
+        /// </summary>
+        [OpenSilver.NotImplemented]
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register(
+                nameof(Time),
+                typeof(TimeSpan),
+                typeof(TimelineMarker),
+                new PropertyMetadata(TimeSpan.Zero));
+
+        /// <summary>
+        /// The time at which the <see cref="TimelineMarker"/> is reached. 
+        /// The default value is <see cref="TimeSpan.Zero"/>.
+        /// </summary>
+        [OpenSilver.NotImplemented]
+        public TimeSpan Time
+        {
+            get { return (TimeSpan)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="Type"/> dependency property.
+        /// </summary>
+        [OpenSilver.NotImplemented]
+        public static readonly DependencyProperty TypeProperty =
+            DependencyProperty.Register(
+                nameof(Type),
+                typeof(string),
+                typeof(TimelineMarker),
+                new PropertyMetadata((object)null));
+
+        /// <summary>
+        /// A string that describes the type of this <see cref="TimelineMarker"/>. 
+        /// The default value is null.
+        /// </summary>
+        [OpenSilver.NotImplemented]
+        public string Type
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
