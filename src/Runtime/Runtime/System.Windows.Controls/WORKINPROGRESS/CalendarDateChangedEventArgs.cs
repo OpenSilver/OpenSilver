49f2749026a013b95198d57b91a5408f4bdd204e
Boopesh <tboopeshkumar@gmail.com>
2022-03-04 21:48:36 +0400

    Adding some missing Calendar control APIs.

---
+++ src/Runtime/Runtime/System.Windows.Controls/WORKINPROGRESS/CalendarDateChangedEventArgs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+#if MIGRATION
+namespace System.Windows.Controls
+#else
+namespace Windows.UI.Xaml.Controls
+#endif
+{
+	/// <summary>
+	/// Provides data for the <see cref="System.Windows.Controls.Calendar.DisplayDateChanged" /> event. 
+	/// </summary>	
+	public class CalendarDateChangedEventArgs : RoutedEventArgs
+	{
+		/// <summary>
+		/// Getsor sets the date that was previously displayed.
+		/// </summary>
+		/// <returns>
+		/// The date previously displayed. 
+		/// </returns>		
+		public DateTime? RemovedDate { get; private set; }
+
+		/// <summary>
+		/// Gets or sets the date to be newly displayed.</summary>
+		/// <returns>
+		/// The new date to display.
+		/// </returns>		
+		public DateTime? AddedDate { get; private set; }
+		
+		internal CalendarDateChangedEventArgs(DateTime? removedDate, DateTime? addedDate)
+		{
+			this.RemovedDate = removedDate;
+			this.AddedDate = addedDate;
+		}
+	}
+}
---
+++ src/Runtime/Runtime/System.Windows.Controls/WORKINPROGRESS/CalendarModeChangedEventArgs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+#if MIGRATION
+namespace System.Windows.Controls
+#else
+namespace Windows.UI.Xaml.Controls
+#endif
+{
+	/// <summary>
+	/// Provides data for the <see cref="System.Windows.Controls.Calendar.DisplayModeChanged" /> event. 
+	/// </summary>	
+	public class CalendarModeChangedEventArgs : RoutedEventArgs
+	{
+        /// <summary>
+        /// Gets the previous mode of the <see cref="System.Windows.Controls.Calendar" />.
+        /// /summary>
+        /// <returns>
+        /// A <see cref="System.Windows.Controls.CalendarMode" /> representing the previous mode. 
+        /// </returns>		
+        public CalendarMode OldMode { get; private set; }
+
+		/// <summary>
+		/// Gets the new mode of the <see cref="System.Windows.Controls.Calendar" />.
+		/// </summary>
+		/// <returns>A <see cref="System.Windows.Controls.CalendarMode" /> representing the new mode. 
+		/// </returns>		
+		public CalendarMode NewMode { get; private set; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="System.Windows.Controls.CalendarModeChangedEventArgs" /> class. 
+		/// </summary>
+		/// <param name="oldMode">The previous mode.</param>
+		/// <param name="newMode">The new mode.</param>		
+		public CalendarModeChangedEventArgs(CalendarMode oldMode, CalendarMode newMode)
+		{
+			this.OldMode = oldMode;
+			this.NewMode = newMode;
+		}
+	}
+}
