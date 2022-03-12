8862046b994f6526b98017947ce768fa94350630
Ahmed Salem <ahmed.salem.me@outlook.com>
2022-03-12 22:04:27 +0200

    tests-for-keydown-textinput-SL-vs-OS

---
+++ src/Tests/TestApplication/TestApplication/Tests/Events/TextInputTest.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TestApplication.Tests.Events
+{
+    public partial class TextInputTest : Page
+    {
+        public TextInputTest()
+        {
+            this.InitializeComponent();
+
+            box1.TextInput += Box1_TextInput;
+            box2.AddHandler(TextInputEvent, new TextCompositionEventHandler(Box2_TextInput), true);
+            box3.AddHandler(KeyDownEvent, new KeyEventHandler(Box3_KeyDown), false);
+            box3.AddHandler(TextInputEvent, new TextCompositionEventHandler(Box3_TextInput), true);
+            box4.TextInput += Box4_TextInput;
+            box5.AddHandler(TextInputEvent, new TextCompositionEventHandler(Box5_TextInput), true);
+            box6.AddHandler(TextInputEvent, new TextCompositionEventHandler(Box6_TextInput), true);
+            box7.AddHandler(TextInputEvent, new TextCompositionEventHandler(Box7_TextInput), true);
+
+            button2.AddHandler(TextInputEvent, new TextCompositionEventHandler(button2_TextInput), true);
+            button3.AddHandler(TextInputEvent, new TextCompositionEventHandler(button3_TextInput), true);
+            button1.Click += Button1_Click;
+        }
+
+        private void button3_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            blockMsg.Text = "should still fire with OnTextInput and even with e.Handled=true before calling base";
+        }
+
+        private void button2_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            blockMsg.Text = "fired for non editable elements";
+        }
+
+        private void Box7_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            blockMsg.Text = "event should fire";
+
+        }
+
+        private void Box6_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            blockMsg.Text = "event should fire";
+        }
+
+        private void Box5_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void Box4_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            blockMsg.Text = "event fires for Esc and Enter without handler handledeventstoo=true";
+        }
+
+        private void Box3_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            blockMsg.Text = "even shouldn't fire when the keydown event e.Handled=true even if this handler set with handledeventstoo=true";
+        }
+
+        private void Box2_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            blockMsg.Text = "event fires with handler handledeventstoo=true";
+        }
+
+        private void Box1_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            blockMsg.Text = "even shouldn't fire without handler handledeventstoo=true";
+        }
+
+        private void Button1_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            blockMsg.Text = "";
+        }
+
+        private void Box3_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
---
+++ src/Tests/TestApplication/TestApplication/Tests/Events/XTextBox.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TestApplication.Tests.Events
+{
+    public class XTextBox : TextBox
+    {
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            var block = (Parent as StackPanel).FindName("blockMsg") as TextBlock;
+
+            if (e.Key == Key.Right || e.Key == Key.Left)
+                block.Text = "Right or Left arrow key pressed with OnKeyDown";
+        }
+
+        protected override void OnTextInput(TextCompositionEventArgs e)
+        {
+            if (Name == "box7")
+                return;
+
+            e.Handled = true;  
+            base.OnTextInput(e);
+        }
+    }
+
+    public class XButton : Button
+    {
+        protected override void OnTextInput(TextCompositionEventArgs e)
+        {
+            e.Handled = true;
+            base.OnTextInput(e);
+        }
+    }
+}
