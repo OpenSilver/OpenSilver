aa0c2b45866dbe30b6a3ff36c2aeef6da9ea18ee
Ashot Khachatryan <ashotkhachatryanh@gmail.com>
2022-03-15 18:00:28 +0900

    feat: implement SelectedDate for Calendar (#441)

--- src/Runtime/Runtime/System.Windows.Controls/WORKINPROGRESS/Calendar.cs
+++ src/Runtime/Runtime/System.Windows.Controls/WORKINPROGRESS/Calendar.cs
@@ -41,42 +41,6 @@ namespace Windows.UI.Xaml.Controls
         public CalendarBlackoutDatesCollection BlackoutDates { get; private set; }
 
         /// <summary>
-        /// Identifies the <see cref="P:System.Windows.Controls.Calendar.SelectedDate" /> dependency property.
-        /// </summary>
-        /// <returns>
-        /// The identifier for the <see cref="P:System.Windows.Controls.Calendar.SelectedDate" /> dependency property.
-        /// </returns>
-        [OpenSilver.NotImplemented]
-        public static readonly DependencyProperty SelectedDateProperty = DependencyProperty.Register(nameof(SelectedDate), typeof(DateTime?), typeof(Calendar), new PropertyMetadata(OnSelectedDateChanged));
-
-        /// <summary>
-        /// Gets or sets the currently selected date.
-        /// </summary>
-        /// <returns>
-        /// The date currently selected. The default is null.
-        /// </returns>
-        /// <exception cref="T:System.ArgumentOutOfRangeException">
-        /// The given date is outside the range specified by <see cref="P:System.Windows.Controls.Calendar.DisplayDateStart" /> and <see cref="P:System.Windows.Controls.Calendar.DisplayDateEnd" />
-        /// -or-The given date is in the <see cref="P:System.Windows.Controls.Calendar.BlackoutDates" /> collection.
-        /// </exception>
-        /// <exception cref="T:System.InvalidOperationException">
-        /// If set to anything other than null when <see cref="P:System.Windows.Controls.Calendar.SelectionMode" /> is set to <see cref="F:System.Windows.Controls.CalendarSelectionMode.None" />.
-        /// </exception>
-        [OpenSilver.NotImplemented]
-        public DateTime? SelectedDate
-        {
-            get => (DateTime?)GetValue(SelectedDateProperty);
-            set => SetValue(SelectedDateProperty, value);
-        }
-
-        private static void OnSelectedDateChanged(
-          DependencyObject d,
-          DependencyPropertyChangedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
         /// Identifies the <see cref="P:System.Windows.Controls.Calendar.DisplayDate" /> dependency property.
         /// </summary>
         /// <returns>
