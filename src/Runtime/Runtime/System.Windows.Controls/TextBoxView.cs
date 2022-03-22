589fcf2bc38c76df91a012e3ac90d6026dc02433
roman-pikulenkor <roman@opensilver.net>
2022-03-22 09:55:39 -0400

    TextMeasurement uses innerHTML instead of passing text

--- src/Runtime/Runtime/System.Windows.Controls/TextBoxView.cs
+++ src/Runtime/Runtime/System.Windows.Controls/TextBoxView.cs
@@ -1008,7 +1008,7 @@ var range,selection;
         protected override Size MeasureOverride(Size availableSize)
         {
             string uniqueIdentifier = ((INTERNAL_HtmlDomElementReference)this.INTERNAL_OuterDomElement).UniqueIdentifier;
-            Size TextSize = Application.Current.TextMeasurementService.MeasureTextBlock(uniqueIdentifier, Host.Text, Host.TextWrapping, Margin, availableSize.Width);
+            Size TextSize = Application.Current.TextMeasurementService.MeasureTextBlock(uniqueIdentifier, Host.TextWrapping, Margin, availableSize.Width);
             return TextSize;
         }
     }
