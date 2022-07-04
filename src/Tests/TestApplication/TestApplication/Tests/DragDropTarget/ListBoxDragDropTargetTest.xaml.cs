959b7830750b1415811b05afca15b4471ded4ccf
Thiago Tanaka <thiago.tanaka@userware.onmicrosoft.com>
2022-07-03 21:40:18 -0300

    feat: implement DragDropTarget for ListBox and TreeView

---
+++ src/Tests/TestApplication/TestApplication/Tests/DragDropTarget/ListBoxDragDropTargetTest.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace TestApplication.OpenSilver.Tests.DragDropTarget
+{
+    public partial class ListBoxDragDropTargetTest : Page
+    {
+        public ListBoxDragDropTargetTest()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/Tests/TestApplication/TestApplication/Tests/DragDropTarget/PanelDragDropTargetTest.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace TestApplication.OpenSilver.Tests.DragDropTarget
+{
+    public partial class PanelDragDropTargetTest : Page
+    {
+        public PanelDragDropTargetTest()
+        {
+            this.InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
