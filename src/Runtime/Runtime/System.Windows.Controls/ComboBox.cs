1709cf055dceafe7b94de5e60512231e9e4c71b3
Andrey Arekhva <a.arekhva@gmail.com>
2022-07-25 10:17:50 +0300

    Revert "Do not clear combobox content when the popup is open"
    This reverts commit cdb11c68797454a5e0c2cbffda2f07b3b8fd80c0.

--- src/Runtime/Runtime/System.Windows.Controls/ComboBox.cs
+++ src/Runtime/Runtime/System.Windows.Controls/ComboBox.cs
@@ -312,6 +312,16 @@ namespace Windows.UI.Xaml.Controls
                     //-----------------------------
                     // Show the Popup
                     //-----------------------------
+
+                    // Empty the ContentPresenter so that, in case it is needed, the same item can be placed in the popup:
+                    if (comboBox._contentPresenter != null)
+                    {
+                        comboBox._contentPresenter.Content = null;
+                        comboBox.SelectionBoxItem = null;
+                        comboBox.SelectionBoxItemTemplate = null;
+                    }
+
+                    // Show the popup:
                     if (comboBox._popup != null)
                     {
                         // add removed 
