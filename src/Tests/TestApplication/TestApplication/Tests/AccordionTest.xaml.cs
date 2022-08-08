c9f8132e7495f05e3b0502502c71bb89a4a4e098
Thiago Tanaka <88004331+thiagoftanaka@users.noreply.github.com>
2022-08-08 08:43:51 -0300

    fix: set Accordion ExpandableContentControl's size to ActualWidth/Height (#566)

---
+++ src/Tests/TestApplication/TestApplication/Tests/AccordionTest.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace TestApplication.Tests
+{
+    public partial class AccordionTest : Page
+    {
+        public AccordionTest()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
