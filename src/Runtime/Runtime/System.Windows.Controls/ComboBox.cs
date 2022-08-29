ef2c32afcba1fc7c403c0a1ab70762a31fe96119
Timothé Louzier <66303379+Startick28@users.noreply.github.com>
2022-08-29 17:48:52 +0200

    fix: prevent null reference exception in ComboBox (#593)

--- src/Runtime/Runtime/System.Windows.Controls/ComboBox.cs
+++ src/Runtime/Runtime/System.Windows.Controls/ComboBox.cs
@@ -18,11 +18,9 @@ using System;
 using System.Threading.Tasks;
 
 #if MIGRATION
-using System.Windows.Input;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 #else
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Input;
 #endif
@@ -172,9 +170,18 @@ namespace Windows.UI.Xaml.Controls
                     _popup.CustomLayout = true;
                 }
                 _popup.MaxHeight = this.MaxDropDownHeight;
+                
+                //todo: once we will have made the following properties (PlacementTarget and Placement) Dependencyproperties, unset it here and set it in the default style.
+                _popup.PlacementTarget = this;
+                _popup.Placement = PlacementMode.Bottom;
+                _popup.INTERNAL_PopupMoved += _popup_INTERNAL_PopupMoved;
+
+                // Make sure the popup gets closed when the user clicks outside the combo box, and listen to the Closed event in order to update the drop-down toggle:
+                _popup.StayOpen = false;
+                _popup.OutsideClick += OnOutsideClick;
+                _popup.ClosedDueToOutsideClick += Popup_ClosedDueToOutsideClick;
             }
 
-            _dropDownToggle = GetTemplateChild("DropDownToggle") as ToggleButton;
             _contentPresenter = GetTemplateChild("ContentPresenter") as ContentPresenter;
             if (_contentPresenter != null)
             {
@@ -186,25 +193,13 @@ namespace Windows.UI.Xaml.Controls
                 _emptyContent = _contentPresenter.Content as FrameworkElement;
             }
 
-            //todo: once we will have made the following properties (PlacementTarget and Placement) Dependencyproperties, unset it here and set it in the default style.
-            _popup.PlacementTarget = this;
-            _popup.Placement = PlacementMode.Bottom;
-            _popup.INTERNAL_PopupMoved += _popup_INTERNAL_PopupMoved;
-
+            _dropDownToggle = GetTemplateChild("DropDownToggle") as ToggleButton;
             if (_dropDownToggle != null)
             {
                 _dropDownToggle.Checked += DropDownToggle_Checked;
                 _dropDownToggle.Unchecked += DropDownToggle_Unchecked;
             }
 
-            // Make sure the popup gets closed when the user clicks outside the combo box, and listen to the Closed event in order to update the drop-down toggle:
-            if (_popup != null)
-            {
-                _popup.StayOpen = false;
-                _popup.OutsideClick += OnOutsideClick;
-                _popup.ClosedDueToOutsideClick += Popup_ClosedDueToOutsideClick;
-            }
-            
             UpdatePresenter();
         }
 
