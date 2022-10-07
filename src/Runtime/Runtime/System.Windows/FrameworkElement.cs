b9f5a0f3b01b78a01ad593a6060353d2da678a23
Thiago Tanaka <88004331+thiagoftanaka@users.noreply.github.com>
2022-10-07 18:59:03 -0300

    fix: prevent TextBlock from overflowing out of its parent (#583)

--- src/Runtime/Runtime/System.Windows/FrameworkElement.cs
+++ src/Runtime/Runtime/System.Windows/FrameworkElement.cs
@@ -1215,18 +1215,9 @@ namespace Windows.UI.Xaml
             finalSize = size.Combine(finalSize).Bounds(MinSize, MaxSize);
 
             Size arrangedSize = ArrangeOverride(finalSize);
-            
-            double w, h;
-            if (this is TextBlock)
-            {
-                w = Math.Max(0, arrangedSize.Width);
-                h = Math.Max(0, arrangedSize.Height);
-            }
-            else
-            {
-                w = Math.Max(0, Math.Min(finalSize.Width, arrangedSize.Width));
-                h = Math.Max(0, Math.Min(finalSize.Height, arrangedSize.Height));
-            }
+
+            double w = Math.Max(0, Math.Min(finalSize.Width, arrangedSize.Width));
+            double h = Math.Max(0, Math.Min(finalSize.Height, arrangedSize.Height));
             arrangedSize = new Size(w, h);
 
             arrangedSize = size.Combine(arrangedSize).Bounds(MinSize, MaxSize);
