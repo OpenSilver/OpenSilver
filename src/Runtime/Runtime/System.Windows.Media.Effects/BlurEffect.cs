d5d78f559fe80b8ebf9299bd958c2699eff5d314
Pierre Barjon <barjon.pierre@gmail.com>
2022-10-08 02:19:38 +0200

    cleanup + improve behavior

--- src/Runtime/Runtime/System.Windows.Media.Effects/BlurEffect.cs
+++ src/Runtime/Runtime/System.Windows.Media.Effects/BlurEffect.cs
@@ -1,27 +1,68 @@
-#if !MIGRATION
-using Windows.UI.Xaml;
-#endif
+
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
 
 using CSHTML5.Internal;
 using OpenSilver.Internal;
 
+#if !MIGRATION
+using Windows.UI.Xaml;
+#endif
+
 namespace System.Windows.Media.Effects
 {
-    //
-    // Summary:
-    //     Represents an effect that you can apply to an object that simulates looking at
-    //     the object through an out-of-focus lens.
-    public sealed partial class BlurEffect : Effect
+    /// <summary>
+    /// Represents an effect that you can apply to an object that simulates looking at
+    /// the object through an out-of-focus lens.
+    /// </summary>
+    public sealed class BlurEffect : Effect
     {
-        //
-        // Summary:
-        //     Identifies the System.Windows.Media.Effects.BlurEffect.Radius dependency property.
-        //
-        // Returns:
-        //     The identifier for the System.Windows.Media.Effects.BlurEffect.Radius dependency
-        //     property.
-        public static readonly DependencyProperty RadiusProperty = DependencyProperty.Register("Radius", typeof(double), typeof(BlurEffect), new PropertyMetadata(5d, ManageChange));
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BlurEffect"/> class.
+        /// </summary>
+        public BlurEffect() { }
 
+        /// <summary>
+        /// Identifies the <see cref="Radius"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty RadiusProperty =
+            DependencyProperty.Register(
+                nameof(Radius),
+                typeof(double),
+                typeof(BlurEffect),
+                new PropertyMetadata(5.0, OnRadiusChanged));
+
+        /// <summary>
+        /// Gets or sets the amount of blurring applied by the <see cref="BlurEffect"/>.
+        /// </summary>
+        /// <returns>
+        /// The radius used in the blur, as a pixel-based factor. The default is 5.
+        /// </returns>
+        public double Radius
+        {
+            get => (double)GetValue(RadiusProperty);
+            set => SetValue(RadiusProperty, value);
+        }
+
+        private static void OnRadiusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            double radius = (double)e.NewValue;
+            if (double.IsNaN(radius) || double.IsInfinity(radius))
+            {
+                throw new ArgumentException();
+            }
+
+            ((BlurEffect)d).RefreshEffect();
+        }
 
         internal override void SetParentUIElement(UIElement newParent)
         {
@@ -29,38 +70,17 @@ namespace System.Windows.Media.Effects
             RefreshEffect();
         }
 
-        private static void ManageChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            ((BlurEffect)d).RefreshEffect();
-        }
-
-        private void RefreshEffect() //todo: if we add support for multiple effects on a same element, return the string instead of directly setting the property.
+        private void RefreshEffect()
         {
             if (_parentUIElement != null && INTERNAL_VisualTreeManager.IsElementInVisualTree(_parentUIElement))
             {
-                var domStyle = OpenSilver.Interop.GetDiv(_parentUIElement);
-                INTERNAL_HtmlDomManager.SetDomElementStyleProperty(domStyle, new Collections.Generic.List<string>() { "filter" }, "blur(" + Radius.ToInvariantString() + "px)");
+                var domStyle = INTERNAL_HtmlDomManager.GetFrameworkElementOuterStyleForModification(_parentUIElement);
+
+                // This gets a result very similar to Silverlight. Using the Radius directly
+                // makes the element much more blurry than Silverlight.
+                double cssRadius = Math.Min(Math.Floor(Math.Max(Radius, 0) / 2.0), 6.0);
+                domStyle.filter = $"blur({cssRadius.ToInvariantString()}px)";
             }
-        }
-
-        //
-        // Summary:
-        //     Initializes a new instance of the System.Windows.Media.Effects.BlurEffect class.
-        public BlurEffect()
-        {
-
-        }
-
-        //
-        // Summary:
-        //     Gets or sets the amount of blurring applied by the System.Windows.Media.Effects.BlurEffect.
-        //
-        // Returns:
-        //     The radius used in the blur, as a pixel-based factor. The default is 5.
-        public double Radius
-        {
-            get { return (double)GetValue(RadiusProperty); }
-            set { SetValue(RadiusProperty, value); }
         }
     }
 }
