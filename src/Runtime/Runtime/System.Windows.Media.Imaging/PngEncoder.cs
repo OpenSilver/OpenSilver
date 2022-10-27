84e10dcd4a810b37a0149c1dfd696a7f0e09d420
kernel-coder <kernel.writer@gmail.com>
2022-10-27 07:31:09 -0400

    Fixed PngEncoder gamma issue

--- src/Runtime/Runtime/System.Windows.Media.Imaging/PngEncoder.cs
+++ src/Runtime/Runtime/System.Windows.Media.Imaging/PngEncoder.cs
@@ -62,7 +62,7 @@ namespace Windows.UI.Xaml.Media.Imaging
             WriteChunk(ms, _IHDR, _ARGB);
 
             // Set gamma = 1
-            size = BitConverter.GetBytes(1 * 100000);
+            size = BitConverter.GetBytes((uint)Math.Round(1.0 / 2.2 * 100000));
             _4BYTEDATA[0] = size[3];
             _4BYTEDATA[1] = size[2];
             _4BYTEDATA[2] = size[1];
