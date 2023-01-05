53b661331cb59e54500223a33824cf98f07261fc
roman-pikulenkor <82944181+roman-pikulenkor@users.noreply.github.com>
2023-01-05 23:40:21 +1000

    fix: update arrange for visibility checking (#681)

--- src/Runtime/Runtime/System.Windows/UIElement.cs
+++ src/Runtime/Runtime/System.Windows/UIElement.cs
@@ -1578,7 +1578,8 @@ document.ondblclick = null;
 
                 LayoutManager.Current.RemoveArrange(this);
 
-                if (Visibility != Visibility.Visible) {
+                if (this.IsVisible == false)
+                {
                     IsRendered = false;
                     return;
                 }
