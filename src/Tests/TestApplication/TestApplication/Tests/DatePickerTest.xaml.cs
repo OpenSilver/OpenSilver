d4c99c39cf581f4a590f1c280d1f8faba809b7fd
Thiago Tanaka <88004331+thiagoftanaka@users.noreply.github.com>
2023-03-02 19:58:51 -0300

    feat: update flatpickr dependency to v4.6.13 (#715)

---
+++ src/Tests/TestApplication/TestApplication/Tests/DatePickerTest.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace TestApplication.Tests
+{
+    public partial class DatePickerTest : Page
+    {
+        public DatePickerTest()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
