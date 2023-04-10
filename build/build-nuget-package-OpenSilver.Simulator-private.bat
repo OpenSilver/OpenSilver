e2181748733f0614d8fb86ee21268ebae9808d46
Shahazan Ali <kernel.writer@gmail.com>
2023-04-10 14:00:26 +0600

    Wrote compiler script to build and use in OSF runtime

--- build/build-nuget-package-OpenSilver.Simulator-private.bat
+++ build/build-nuget-package-OpenSilver.Simulator-private.bat
@@ -15,8 +15,11 @@ rem Gets the directory where this bat is located, without the trailing "\"
 set batDirectory=%~dp0
 set batDirectory=%batDirectory:~0,-1%
 
+rem Reading version information from version_info.txt file
+for /f "delims== tokens=1,2" %%G in (version_info.txt) do set %%G=%%H
+
 rem Define the "%PackageVersion%" variable:
-set /p PackageVersion="%ESC%[92mOpenSilver version:%ESC%[0m 1.1.0-private-"
+set /p PackageVersion="%ESC%[92mOpenSilver version:%ESC%[0m %CURRENT_VERSION_PREFIX%"
 
 rem Get the current date and time:
 for /F "tokens=2" %%i in ('date /t') do set currentdate=%%i
@@ -24,7 +27,9 @@ set currenttime=%time%
 
 rem Create a Version.txt file with the date:
 md temp
-@echo OpenSilver.Simulator 1.1.0-private-%PackageVersion% (%currentdate% %currenttime%)> temp/Version.txt
+@echo OpenSilver.Simulator %CURRENT_VERSION_PREFIX%%PackageVersion% (%currentdate% %currenttime%)> temp/Version.txt
+
+taskkill /f /im "msbuild.exe" >NUL
 
 echo. 
 echo %ESC%[95mRestoring NuGet packages%ESC%[0m
@@ -35,7 +40,7 @@ echo.
 echo %ESC%[95mBuilding and packaging %ESC%[0mOpenSilver.Simulator %ESC%[0m
 echo. 
 msbuild slnf/OpenSilver.Simulator.slnf -p:Configuration=SL -clp:ErrorsOnly
-nuget.exe pack nuspec\OpenSilver.Simulator.nuspec -OutputDirectory "output/OpenSilver" -BasePath "%batDirectory%" -Properties "PackageVersion=1.1.0-private-%PackageVersion%;RepositoryUrl=https://github.com/OpenSilver/OpenSilver"
+nuget.exe pack nuspec\OpenSilver.Simulator.nuspec -OutputDirectory "output/OpenSilver" -BasePath "%batDirectory%" -Properties "PackageVersion=%CURRENT_VERSION_PREFIX%%PackageVersion%;RepositoryUrl=https://github.com/OpenSilver/OpenSilver"
 
 explorer "output\OpenSilver"
 
