d387f450ead51c040419824c4c2909ad2fa07dc8
Pierre Barjon <barjon.pierre@gmail.com>
2023-05-02 13:55:11 +0200

    revert: "fix: adjust pointer events of ScrollViewer"
    This reverts commit daf35dee84d38ec8f732effb776c11781d9cf398.

--- src/Runtime/Runtime/System.Windows.Controls/ScrollViewer.cs
+++ src/Runtime/Runtime/System.Windows.Controls/ScrollViewer.cs
@@ -99,10 +99,15 @@ namespace Windows.UI.Xaml.Controls
             }
         }
 
-        // Note: we need to handle this here because templates are disabled for ScrollViewers.
-        // We check the background because this condition is true with the default ScrollViewer's
-        // template of Silverlight. With custom templates, this might still be an issue.
-        internal override bool EnablePointerEventsCore => Background is not null;
+        // Note: we need to force this to true because template are disabled
+        // for ScrollViewers.
+        internal override bool EnablePointerEventsCore
+        {
+            get
+            {
+                return true;
+            }
+        }
 
         /// <summary> 
         /// Reference to the ScrollContentPresenter child.
