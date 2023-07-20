a7c9d64611353bcf1b89f78b47f94a2d3095f384
Pierre Barjon <barjon.pierre@gmail.com>
2023-07-20 12:18:19 +0200

    feat: implement RichTextBlock

---
+++ src/Runtime/Runtime/OpenSilver/Internal/Documents/TextContainer.cs
@@ -0,0 +1,58 @@
+﻿
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+using System;
+
+#if MIGRATION
+using System.Windows;
+using System.Windows.Documents;
+#else
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Documents;
+#endif
+
+namespace OpenSilver.Internal.Documents;
+
+internal abstract class TextContainer<T> : ITextContainer
+    where T : DependencyObject
+{
+    protected TextContainer(T parent)
+    {
+        Parent = parent ?? throw new ArgumentNullException(nameof(parent));
+    }
+
+    internal T Parent { get; }
+
+    public abstract string Text { get; }
+
+    public virtual void BeginChange() { }
+
+    public virtual void EndChange() { }
+
+    public void OnTextAdded(TextElement textElement) => OnTextAddedOverride(textElement);
+
+    public void OnTextRemoved(TextElement textElement) => OnTextRemovedOverride(textElement);
+
+    protected abstract void OnTextAddedOverride(TextElement textElement);
+
+    protected abstract void OnTextRemovedOverride(TextElement textElement);
+}
+
+internal interface ITextContainer
+{
+    string Text { get; }
+    void BeginChange();
+    void EndChange();
+    void OnTextAdded(TextElement textElement);
+    void OnTextRemoved(TextElement textElement);
+}
---
+++ src/Runtime/Runtime/OpenSilver/Internal/Documents/TextContainerRichTextBlock.cs
@@ -0,0 +1,62 @@
+﻿
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+using CSHTML5.Internal;
+
+#if MIGRATION
+using System.Windows.Controls;
+using System.Windows.Documents;
+#else
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Documents;
+#endif
+
+namespace OpenSilver.Internal.Documents;
+
+internal sealed class TextContainerRichTextBlock : TextContainer<RichTextBlock>
+{
+    public TextContainerRichTextBlock(RichTextBlock parent)
+        : base(parent)
+    {
+    }
+
+    public override string Text
+    {
+        get
+        {
+            string text = "";
+            foreach (var block in Parent.Blocks)
+            {
+                text += block.GetContainerText();
+            }
+
+            return text;
+        }
+    }
+
+    protected override void OnTextAddedOverride(TextElement textElement)
+    {
+        if (INTERNAL_VisualTreeManager.IsElementInVisualTree(Parent))
+        {
+            INTERNAL_VisualTreeManager.AttachVisualChildIfNotAlreadyAttached(textElement, Parent);
+        }
+    }
+
+    protected override void OnTextRemovedOverride(TextElement textElement)
+    {
+        if (INTERNAL_VisualTreeManager.IsElementInVisualTree(Parent))
+        {
+            INTERNAL_VisualTreeManager.DetachVisualChildIfNotNull(textElement, Parent);
+        }
+    }
+}
