b0524d26fc2f3353a9ed069fb77d526d27e3b1a9
Pierre Barjon <barjon.pierre@gmail.com>
2023-08-02 15:08:33 +0200

    feat: implement CharacterSpacing property

--- src/Runtime/Scripts/cshtml5.js
+++ src/Runtime/Scripts/cshtml5.js
@@ -778,6 +778,7 @@ document.measureTextBlock = function (measureElementId, uid, whiteSpace, overflo
         element.style.fontFamily = computedStyle.fontFamily;
         element.style.fontStyle = computedStyle.fontStyle;
         element.style.lineHeight = computedStyle.lineHeight;
+        element.style.letterSpacing = computedStyle.letterSpacing;
 
         element.style.whiteSpace = whiteSpace;
         element.style.overflowWrap = overflowWrap;
@@ -1245,6 +1246,7 @@ document.textboxHelpers = (function () {
             view.style.fontSize = 'inherit';
             view.style.fontFamily = 'inherit';
             view.style.color = 'inherit';
+            view.style.letterSpacing = 'inherit';
             view.style.resize = 'none';
             view.style.outline = 'none';
             view.style.border = 'none';
