b870dc5192dcf88a1876d75a52388114f756b071
Fabio Silva <fabio.silva@userware.onmicrosoft.com>
2023-09-23 21:32:13 -0300

    chore: update opensilver version

--- src/VSExtension/OpenSilverApplicationTemplate/OpenSilverApplication.Browser/Interop/UnmarshalledJavaScriptExecutionHandler.cs
+++ src/VSExtension/OpenSilverApplicationTemplate/OpenSilverApplication.Browser/Interop/UnmarshalledJavaScriptExecutionHandler.cs
@@ -6,7 +6,7 @@ namespace $safeprojectname$.Interop
 {
     public class UnmarshalledJavaScriptExecutionHandler : IWebAssemblyExecutionHandler
     {
-        private const string MethodName = "callJSUnmarshalled";
+        private const string MethodName = "callJSUnmarshalled_v2";
         private readonly WebAssemblyJSRuntime _runtime;
 
         public UnmarshalledJavaScriptExecutionHandler(IJSRuntime runtime)
