279a018bce5f3210bef4c20d4018d35fdc861b13
Pierre Barjon <barjon.pierre@gmail.com>
2023-09-28 10:45:08 +0200

    Cleanup project templates

--- src/VSExtension/OpenSilver.TemplateWizards/AppCustomizationWizard.cs
+++ src/VSExtension/OpenSilver.TemplateWizards/AppCustomizationWizard.cs
@@ -135,7 +135,7 @@ namespace OpenSilver.TemplateWizards
                 CopyNugetConfig(replacementsDictionary);
             }
 
-            replacementsDictionary.Add("$opensilverpackageversion$", "2.0.0");
+            replacementsDictionary.Add("$opensilverpackageversion$", "1.2.3");
         }
 
         public bool ShouldAddProjectItem(string filePath)
