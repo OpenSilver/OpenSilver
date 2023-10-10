6295ca65aa6c9121b7ab7cd1701cf678aaec9ec7
Iakov Lilo <jlilo@hwdtech.com>
2023-10-10 19:48:55 +1100

    chore: add an attribute about compatible XAML Designer version (#823)

---
+++ src/Runtime/Runtime/OpenSilver/XamlDesignerAttribute.cs
@@ -0,0 +1,30 @@
+﻿
+/*===================================================================================
+*
+*   Copyright (c) Userware/OpenSilver.net
+*
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*
+\*====================================================================================*/
+
+using System;
+using System.ComponentModel;
+
+namespace OpenSilver
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public sealed class XamlDesignerAttribute : Attribute
+    {
+        public XamlDesignerAttribute(int version)
+        {
+            Version = version;
+        }
+
+        public int Version { get; }
+    }
+}
