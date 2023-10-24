3b70d0e9c5acf27c23493ac7b3a8010354e6a48e
roman-pikulenkor <roman@opensilver.net>
2023-10-24 05:44:08 -0400

    Fix measurement of text starting with "<"

--- src/Runtime/Scripts/cshtml5.js
+++ src/Runtime/Scripts/cshtml5.js
@@ -732,6 +732,7 @@ document.measureTextBlock = function (measureElementId, uid, whiteSpace, overflo
             let text = elToMeasure.value.length == 0 ? emptyVal : elToMeasure.value;
             // if the text ends with a new line, we need to add one more or it will not be measured
             if (text.endsWith('\n')) text += '\n';
+            if (text.startsWith('<')) text = "&lt;" + text.substring(1);
             element.innerHTML = text;
         } else {
             element.innerHTML = elToMeasure.innerHTML.length == 0 ? emptyVal : elToMeasure.innerHTML;
