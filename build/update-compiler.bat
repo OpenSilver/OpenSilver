f9757f00810631f58d84fd7c4063a678834b519d
Pierre Barjon <barjon.pierre@gmail.com>
2024-01-02 11:02:30 +0100

    chore: make update-compiler.bat independent from execution folder

--- build/update-compiler.bat
+++ build/update-compiler.bat
@@ -1,46 +1,41 @@
-@echo off
+@ECHO off
 
-IF NOT EXIST "slnf/Compiler.slnf" (
-echo Wrong working directory. Please navigate to the folder that contains the BAT file before executing it.
-PAUSE
-EXIT
-)
+REM Define the escape character for colored text
+FOR /F %%a IN ('"prompt $E$S & echo on & for %%b in (1) do rem"') DO SET "ESC=%%a"
 
-rem Define the escape character for colored text
-for /F %%a in ('"prompt $E$S & echo on & for %%b in (1) do rem"') do set "ESC=%%a"
+SET CFG=Release
+SET BUILD_DIR=%~dp0
+SET SRC_DIR=%~dp0..\src
 
-rem Reading version information from version_info.txt file
-for /f "delims== tokens=1,2" %%G in (version_info.txt) do set %%G=%%H
+REM Reading version information from version_info.txt file
+FOR /F "delims== tokens=1,2" %%G IN (%BUILD_DIR%\version_info.txt) DO SET %%G=%%H
 
-set config=Release
-set repoPath=%~dp0..
-set sourceBase=%repoPath%\src\Compiler
-set compilerDIR=%repoPath%\src\packages\OpenSilver.%STABLE_VERSION%
+SET COMPILER_DIR=%SRC_DIR%\packages\OpenSilver.%STABLE_VERSION%
 
-echo. 
-echo %ESC%[95mCopying targets.%ESC%[0m
-echo.
+ECHO. 
+ECHO %ESC%[95mCopying targets.%ESC%[0m
+ECHO.
 
-copy "%repoPath%\src\Targets\OpenSilver.targets" "%compilerDIR%\build"
-copy "%repoPath%\src\Targets\OpenSilver.Common.targets" "%compilerDIR%\build"
-copy "%repoPath%\src\Targets\OpenSilver.GenerateAssemblyInfo.targets" "%compilerDIR%\build"
+COPY "%SRC_DIR%\Targets\OpenSilver.targets" "%COMPILER_DIR%\build"
+COPY "%SRC_DIR%\Targets\OpenSilver.Common.targets" "%COMPILER_DIR%\build"
+COPY "%SRC_DIR%\Targets\OpenSilver.GenerateAssemblyInfo.targets" "%COMPILER_DIR%\build"
 
 taskkill /f /im "msbuild.exe" 1>NUL 2>NUL
 
-echo. 
-echo %ESC%[95mRestoring NuGet packages%ESC%[0m
-nuget restore ../src/OpenSilver.sln -v quiet
+ECHO. 
+ECHO %ESC%[95mRestoring NuGet packages%ESC%[0m
+%BUILD_DIR%\nuget restore %SRC_DIR%\OpenSilver.sln -v quiet
 
-echo. 
-echo %ESC%[95mBuilding %ESC%[0m%config% %ESC%[95mconfiguration%ESC%[0m
-echo.
-msbuild slnf/Compiler.slnf -p:Configuration=%config% -clp:ErrorsOnly -restore
+ECHO. 
+ECHO %ESC%[95mBuilding %ESC%[0m%CFG% %ESC%[95mconfiguration%ESC%[0m
+ECHO.
+msbuild %BUILD_DIR%\slnf\Compiler.slnf -p:Configuration=%CFG% -clp:ErrorsOnly -restore
 
 taskkill /f /im "msbuild.exe" 1>NUL 2>NUL
 
-echo. 
-echo %ESC%[95mCopying Compiler DLLs.%ESC%[0m
-echo.
+ECHO. 
+ECHO %ESC%[95mCopying Compiler DLLs.%ESC%[0m
+ECHO.
 
 CALL :copyDll Compiler OpenSilver.Compiler
 CALL :copyDll Compiler.Common OpenSilver.Compiler.Common
@@ -54,6 +49,6 @@ CALL :copyDll Compiler Mono.Cecil.Rocks
 
 EXIT /B 0
 :copyDll
-del "%compilerDIR%\tools\%~2.dll" 1>NUL 2>NUL
-copy "%sourceBase%\%~1\bin\%config%\net461\%~2.dll" "%compilerDIR%\tools"
+DEL "%COMPILER_DIR%\tools\%~2.dll"
+COPY "%SRC_DIR%\Compiler\%~1\bin\%CFG%\net461\%~2.dll" "%COMPILER_DIR%\tools"
 EXIT /B 0
