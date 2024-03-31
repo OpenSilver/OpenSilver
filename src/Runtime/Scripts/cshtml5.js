6c0e2777cea6b2f0b2b486d9d6b8024292d87df7
Pierre Barjon <barjon.pierre@gmail.com>
2024-03-31 19:28:15 +0200

    revert: "fix: do not hide elements that have not been measured"
    This reverts commit 2ea5041e9c70636b860142050a9adbafc7b363f7.

--- src/Runtime/Scripts/cshtml5.js
+++ src/Runtime/Scripts/cshtml5.js
@@ -116,7 +116,7 @@ document.createLayout = function (tagName, id, parentId, isKeyboardFocusable) {
 
 document._createLayout = function (tagName, id, isKeyboardFocusable) {
     const element = document._createElement(tagName, id);
-    element.classList.add('opensilver-uielement');
+    element.classList.add('opensilver-uielement', 'uielement-unarranged');
     document.inputManager.addListeners(element, isKeyboardFocusable);
     return element;
 };
@@ -222,7 +222,7 @@ document.createShape = function (svgTagName, svgId, shapeId, defsId, parentId) {
     if (!parent) return;
 
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.classList.add('opensilver-uielement', 'opensilver-shape');
+    svg.classList.add('opensilver-uielement', 'opensilver-shape', 'uielement-unarranged');
     svg.setAttribute('id', svgId);
     Object.defineProperty(svg, 'xamlid', {
         value: svgId,
@@ -813,6 +813,7 @@ document.arrange = function (id, left, top, width, height, clip, clipLeft, clipT
         } else {
             element.style.clip = '';
         }
+        element.classList.remove('uielement-unarranged');
     }
 };
 
