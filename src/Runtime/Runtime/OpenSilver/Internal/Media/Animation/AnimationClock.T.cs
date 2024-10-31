700b46f2a449468ec6ddeea417bbcffd9caf9492
Pierre Barjon <barjon.pierre@gmail.com>
2024-10-31 14:15:41 +0100

    perf: optimize resolution of animation path for known properties

---
+++ src/Runtime/Runtime/OpenSilver/Internal/Media/Animation/AnimationClock.T.cs
@@ -0,0 +1,131 @@
+﻿
+/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*  
+\*====================================================================================*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media.Animation;
+using OpenSilver.Internal.Data;
+
+namespace OpenSilver.Internal.Media.Animation;
+
+internal sealed class AnimationClock<TValue> : AnimationClock
+{
+    private readonly IValueAnimator<TValue> _animator;
+
+    private DependencyObject _target;
+    private DependencyProperty _dp;
+    private TValue _initialValue;
+
+    public AnimationClock(AnimationTimeline owner, bool isRoot, IValueAnimator<TValue> animator)
+        : base(owner, isRoot)
+    {
+        Debug.Assert(animator is not null);
+        _animator = animator;
+    }
+
+    public new AnimationTimeline Timeline => (AnimationTimeline)base.Timeline;
+
+    public override void SetContext(DependencyObject rootTarget, PropertyPath targetProperty)
+    {
+        Initialize(rootTarget, targetProperty);
+        _target.AttachAnimationClock(_dp, this);
+    }
+
+    public override object GetCurrentValue() => _animator.GetCurrentValue(_initialValue, _dp, this);
+
+    protected override void OnFrameCore() => _target.RefreshAnimation(_dp, this);
+
+    protected override void OnStopCore() => _target.DetachAnimationClock(_dp, this);
+
+    public override Duration IterationDuration
+    {
+        get
+        {
+            Duration duration = Timeline.Duration;
+            if (duration == Duration.Automatic)
+            {
+                duration = Timeline.NaturalDuration;
+            }
+
+            return duration;
+        }
+    }
+
+    private void Initialize(DependencyObject rootTarget, PropertyPath targetProperty)
+    {
+        if (targetProperty.DependencyProperty is not null)
+        {
+            _target = rootTarget;
+            _dp = targetProperty.DependencyProperty;
+            return;
+        }
+
+        IReadOnlyList<SourceValueInfo> parts = targetProperty.SVI;
+        if (parts.Count > 0)
+        {
+            DependencyObject target = rootTarget;
+            for (int i = 0; i < parts.Count - 1; i++)
+            {
+                SourceValueInfo svi = parts[i];
+                switch (svi.type)
+                {
+                    case PropertyNodeType.Property:
+                        DependencyProperty dp = DPFromName(svi.propertyName, svi.typeName, target.GetType());
+                        var value = AsDependencyObject(target.GetValue(dp));
+                        if (i == 0 && value is ICloneOnAnimation<DependencyObject> cloneable && !cloneable.IsClone)
+                        {
+                            value = cloneable.Clone();
+                            target.SetValueInternal(dp, value);
+                        }
+                        target = value;
+                        break;
+
+                    case PropertyNodeType.Indexed:
+                        if (target is not IList list)
+                        {
+                            throw new InvalidOperationException($"'{target}' must implement IList.");
+                        }
+                        if (!int.TryParse(svi.param, out int index))
+                        {
+                            throw new InvalidOperationException($"'{svi.param}' can't be converted to an integer value.");
+                        }
+
+                        target = AsDependencyObject(list[index]);
+                        break;
+
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+
+            _target = target;
+            _dp = DPFromName(parts[parts.Count - 1].propertyName, parts[parts.Count - 1].typeName, target.GetType());
+            _initialValue = (TValue)_target.GetValue(_dp);
+            return;
+        }
+
+        throw new InvalidOperationException();
+    }
+
+    private static DependencyObject AsDependencyObject(object o) =>
+        o as DependencyObject ??
+        throw new InvalidOperationException($"'{o}' must be a DependencyObject.");
+
+    private static DependencyProperty DPFromName(string propertyName, string typeName, Type ownerType) =>
+        GetKnownProperty(propertyName, typeName) ??
+        DependencyProperty.FromName(propertyName, ownerType) ??
+        throw new InvalidOperationException($"No DependencyProperty named '{propertyName}' could be found in '{ownerType}'.");
+}
--- src/Runtime/Runtime/OpenSilver/Internal/Media/Animation/AnimationClock.cs
+++ src/Runtime/Runtime/OpenSilver/Internal/Media/Animation/AnimationClock.cs
@@ -11,15 +11,12 @@
 *  
 \*====================================================================================*/
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Media;
 using System.Windows.Media.Animation;
-using OpenSilver.Internal.Data;
+using System.Windows.Shapes;
 
 namespace OpenSilver.Internal.Media.Animation;
 
@@ -31,126 +28,56 @@ internal abstract class AnimationClock : TimelineClock
     }
 
     public abstract object GetCurrentValue();
-}
 
-internal sealed class AnimationClock<TValue> : AnimationClock
-{
-    private readonly IValueAnimator<TValue> _animator;
-
-    private DependencyObject _target;
-    private DependencyProperty _dp;
-    private TValue _initialValue;
-
-    public AnimationClock(AnimationTimeline owner, bool isRoot, IValueAnimator<TValue> animator)
-        : base(owner, isRoot)
-    {
-        Debug.Assert(animator is not null);
-        _animator = animator;
-    }
-
-    public new AnimationTimeline Timeline => (AnimationTimeline)base.Timeline;
-
-    public override void SetContext(DependencyObject rootTarget, PropertyPath targetProperty)
-    {
-        Initialize(rootTarget, targetProperty);
-        _target.AttachAnimationClock(_dp, this);
-    }
-
-    public override object GetCurrentValue() => _animator.GetCurrentValue(_initialValue, _dp, this);
-
-    protected override void OnFrameCore() => _target.RefreshAnimation(_dp, this);
-
-    protected override void OnStopCore() => _target.DetachAnimationClock(_dp, this);
-
-    public override Duration IterationDuration
-    {
-        get
-        {
-            Duration duration = Timeline.Duration;
-            if (duration == Duration.Automatic)
-            {
-                duration = Timeline.NaturalDuration;
-            }
-
-            return duration;
-        }
-    }
-
-    private void Initialize(DependencyObject rootTarget, PropertyPath targetProperty)
-    {
-        if (targetProperty.DependencyProperty is not null)
-        {
-            _target = rootTarget;
-            _dp = targetProperty.DependencyProperty;
-            return;
-        }
-
-        IReadOnlyList<SourceValueInfo> parts = targetProperty.SVI;
-        if (parts.Count > 0)
-        {
-            DependencyObject target = rootTarget;
-            for (int i = 0; i < parts.Count - 1; i++)
-            {
-                SourceValueInfo svi = parts[i];
-                switch (svi.type)
-                {
-                    case PropertyNodeType.Property:
-                        DependencyProperty dp = DPFromName(svi.propertyName, svi.typeName, target.GetType());
-                        var value = AsDependencyObject(target.GetValue(dp));
-                        if (i == 0 && value is ICloneOnAnimation<DependencyObject> cloneable && !cloneable.IsClone)
-                        {
-                            value = cloneable.Clone();
-                            target.SetValueInternal(dp, value);
-                        }
-                        target = value;
-                        break;
-
-                    case PropertyNodeType.Indexed:
-                        if (target is not IList list)
-                        {
-                            throw new InvalidOperationException($"'{target}' must implement IList.");
-                        }
-                        if (!int.TryParse(svi.param, out int index))
-                        {
-                            throw new InvalidOperationException($"'{svi.param}' can't be converted to an integer value.");
-                        }
-
-                        target = AsDependencyObject(list[index]);
-                        break;
-
-                    default:
-                        throw new InvalidOperationException();
-                }
-            }
-
-            _target = target;
-            _dp = DPFromName(parts[parts.Count - 1].propertyName, parts[parts.Count - 1].typeName, target.GetType());
-            _initialValue = (TValue)_target.GetValue(_dp);
-            return;
-        }
-
-        throw new InvalidOperationException();
-    }
-
-    private static DependencyObject AsDependencyObject(object o) =>
-        o as DependencyObject ??
-        throw new InvalidOperationException($"'{o}' must be a DependencyObject.");
-
-    private static DependencyProperty DPFromName(string propertyName, string typeName, Type ownerType) =>
-        GetKnownProperty(propertyName, typeName) ??
-        DependencyProperty.FromName(propertyName, ownerType) ??
-        throw new InvalidOperationException($"No DependencyProperty named '{propertyName}' could be found in '{ownerType}'.");
-
-    // Workaround to resolve some common attached properties
-    private static DependencyProperty GetKnownProperty(string propertyName, string typeName)
+    internal static DependencyProperty GetKnownProperty(string propertyName, string typeName)
     {
         return typeName switch
         {
+            nameof(UIElement) => GetKnownUIElementProperty(propertyName),
+            nameof(Border) => GetKnownBorderProperty(propertyName),
             nameof(Grid) => GetKnownGridProperty(propertyName),
             nameof(Canvas) => GetKnownCanvasProperty(propertyName),
             nameof(TextBlock) => GetKnownTextBlockProperty(propertyName),
             nameof(TextElement) or nameof(Inline) or nameof(Run) or nameof(Span) => GetKnownTextElementProperty(propertyName),
             nameof(Block) or nameof(Paragraph) => GetKnownBlockProperty(propertyName),
+            nameof(LinearGradientBrush) => GetKnownLinearGradientBrushProperty(propertyName),
+            nameof(RadialGradientBrush) => GetKnownRadialGradientBrushProperty(propertyName),
+            nameof(SolidColorBrush) => GetKnownSolidColorBrushProperty(propertyName),
+            nameof(GradientBrush) => GetKnownGradientBrushProperty(propertyName),
+            nameof(Brush) => GetKnownBrushProperty(propertyName),
+            nameof(GradientStop) => GetKnownGradientStopProperty(propertyName),
+            nameof(Rectangle) => GetKnownRectangleProperty(propertyName),
+            nameof(Line) => GetKnownLineProperty(propertyName),
+            nameof(Shape) or nameof(Path) or nameof(Ellipse) or nameof(Polygon) or nameof(Polyline) => GetKnownShapeProperty(propertyName),
+            nameof(TranslateTransform) => GetKnownTranslateTransformProperty(propertyName),
+            nameof(ScaleTransform) => GetKnownScaleTransformProperty(propertyName),
+            nameof(SkewTransform) => GetKnownSkewTransformProperty(propertyName),
+            nameof(RotateTransform) => GetKnownRotateTransformProperty(propertyName),
+            nameof(CompositeTransform) => GetKnownCompositeTransformProperty(propertyName),
+            nameof(TransformGroup) => GetKnownTransformGroupProperty(propertyName),
+            _ => null,
+        };
+    }
+
+    private static DependencyProperty GetKnownUIElementProperty(string name)
+    {
+        return name switch
+        {
+            nameof(UIElement.Opacity) => UIElement.OpacityProperty,
+            nameof(UIElement.OpacityMask) => UIElement.OpacityMaskProperty,
+            nameof(UIElement.Visibility) => UIElement.VisibilityProperty,
+            nameof(UIElement.RenderTransform) => UIElement.RenderTransformProperty,
+            _ => null,
+        };
+    }
+
+    private static DependencyProperty GetKnownBorderProperty(string name)
+    {
+        return name switch
+        {
+            nameof(Border.Background) => Border.BackgroundProperty,
+            nameof(Border.BorderBrush) => Border.BorderBrushProperty,
+            nameof(Border.BorderThickness) => Border.BorderThicknessProperty,
             _ => null,
         };
     }
@@ -219,6 +146,168 @@ internal sealed class AnimationClock<TValue> : AnimationClock
             nameof(Block.LineStackingStrategy) => Block.LineStackingStrategyProperty,
             nameof(Block.TextAlignment) => Block.TextAlignmentProperty,
             _ => GetKnownTextElementProperty(name),
+        };
+    }
+
+    private static DependencyProperty GetKnownLinearGradientBrushProperty(string name)
+    {
+        return name switch
+        {
+            nameof(LinearGradientBrush.StartPoint) => LinearGradientBrush.StartPointProperty,
+            nameof(LinearGradientBrush.EndPoint) => LinearGradientBrush.EndPointProperty,
+            _ => GetKnownGradientBrushProperty(name),
+        };
+    }
+
+    private static DependencyProperty GetKnownRadialGradientBrushProperty(string name)
+    {
+        return name switch
+        {
+            nameof(RadialGradientBrush.RadiusX) => RadialGradientBrush.RadiusXProperty,
+            nameof(RadialGradientBrush.RadiusY) => RadialGradientBrush.RadiusYProperty,
+            nameof(RadialGradientBrush.Center) => RadialGradientBrush.CenterProperty,
+            _ => GetKnownGradientBrushProperty(name),
+        };
+    }
+
+    private static DependencyProperty GetKnownGradientBrushProperty(string name)
+    {
+        return name switch
+        {
+            nameof(GradientBrush.GradientStops) => GradientBrush.GradientStopsProperty,
+            _ => GetKnownBrushProperty(name),
+        };
+    }
+
+    private static DependencyProperty GetKnownSolidColorBrushProperty(string name)
+    {
+        return name switch
+        {
+            nameof(SolidColorBrush.Color) => SolidColorBrush.ColorProperty,
+            _ => GetKnownBrushProperty(name),
+        };
+    }
+
+    private static DependencyProperty GetKnownBrushProperty(string name)
+    {
+        return name switch
+        {
+            nameof(Brush.Opacity) => Brush.OpacityProperty,
+            _ => null,
+        };
+    }
+
+    private static DependencyProperty GetKnownRectangleProperty(string name)
+    {
+        return name switch
+        {
+            nameof(Rectangle.RadiusX) => Rectangle.RadiusXProperty,
+            nameof(Rectangle.RadiusY) => Rectangle.RadiusYProperty,
+            _ => GetKnownShapeProperty(name),
+        };
+    }
+
+    private static DependencyProperty GetKnownLineProperty(string name)
+    {
+        return name switch
+        {
+            nameof(Line.X1) => Line.X1Property,
+            nameof(Line.X2) => Line.X2Property,
+            nameof(Line.Y1) => Line.Y1Property,
+            nameof(Line.Y2) => Line.Y2Property,
+            _ => GetKnownShapeProperty(name),
+        };
+    }
+
+    private static DependencyProperty GetKnownShapeProperty(string name)
+    {
+        return name switch
+        {
+            nameof(Shape.Fill) => Shape.FillProperty,
+            nameof(Shape.Stroke) => Shape.StrokeProperty,
+            nameof(Shape.StrokeThickness) => Shape.StrokeThicknessProperty,
+            _ => null,
+        };
+    }
+
+    private static DependencyProperty GetKnownTranslateTransformProperty(string name)
+    {
+        return name switch
+        {
+            nameof(TranslateTransform.X) => TranslateTransform.XProperty,
+            nameof(TranslateTransform.Y) => TranslateTransform.YProperty,
+            _ => null,
+        };
+    }
+
+    private static DependencyProperty GetKnownRotateTransformProperty(string name)
+    {
+        return name switch
+        {
+            nameof(RotateTransform.Angle) => RotateTransform.AngleProperty,
+            nameof(RotateTransform.CenterX) => RotateTransform.CenterXProperty,
+            nameof(RotateTransform.CenterY) => RotateTransform.CenterYProperty,
+            _ => null,
+        };
+    }
+
+    private static DependencyProperty GetKnownScaleTransformProperty(string name)
+    {
+        return name switch
+        {
+            nameof(ScaleTransform.ScaleX) => ScaleTransform.ScaleXProperty,
+            nameof(ScaleTransform.ScaleY) => ScaleTransform.ScaleYProperty,
+            nameof(ScaleTransform.CenterX) => ScaleTransform.CenterXProperty,
+            nameof(ScaleTransform.CenterY) => ScaleTransform.CenterYProperty,
+            _ => null,
+        };
+    }
+
+    private static DependencyProperty GetKnownSkewTransformProperty(string name)
+    {
+        return name switch
+        {
+            nameof(SkewTransform.AngleX) => SkewTransform.AngleXProperty,
+            nameof(SkewTransform.AngleY) => SkewTransform.AngleYProperty,
+            nameof(SkewTransform.CenterX) => SkewTransform.CenterXProperty,
+            nameof(SkewTransform.CenterY) => SkewTransform.CenterYProperty,
+            _ => null,
+        };
+    }
+
+    private static DependencyProperty GetKnownCompositeTransformProperty(string name)
+    {
+        return name switch
+        {
+            nameof(CompositeTransform.TranslateX) => CompositeTransform.TranslateXProperty,
+            nameof(CompositeTransform.TranslateY) => CompositeTransform.TranslateYProperty,
+            nameof(CompositeTransform.ScaleX) => CompositeTransform.ScaleXProperty,
+            nameof(CompositeTransform.ScaleY) => CompositeTransform.ScaleYProperty,
+            nameof(CompositeTransform.SkewX) => CompositeTransform.SkewXProperty,
+            nameof(CompositeTransform.SkewY) => CompositeTransform.SkewYProperty,
+            nameof(CompositeTransform.Rotation) => CompositeTransform.RotationProperty,
+            nameof(CompositeTransform.CenterX) => CompositeTransform.CenterXProperty,
+            nameof(CompositeTransform.CenterY) => CompositeTransform.CenterYProperty,
+            _ => null,
+        };
+    }
+
+    private static DependencyProperty GetKnownTransformGroupProperty(string name)
+    {
+        return name switch
+        {
+            nameof(TransformGroup.Children) => TransformGroup.ChildrenProperty,
+            _ => null,
+        };
+    }
+
+    private static DependencyProperty GetKnownGradientStopProperty(string name)
+    {
+        return name switch
+        {
+            nameof(GradientStop.Offset) => GradientStop.OffsetProperty,
+            nameof(GradientStop.Color) => GradientStop.ColorProperty,
+            _ => null,
         };
     }
 }
