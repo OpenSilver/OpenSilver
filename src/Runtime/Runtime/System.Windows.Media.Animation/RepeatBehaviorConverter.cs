a8596229b4e2694fa8dcb799b87501b91f6839ce
Pierre Barjon <barjon.pierre@gmail.com>
2025-03-13 15:02:37 +0100

    fix(Compiler): fix RepeatBehavior converter

--- src/Runtime/Runtime/System.Windows.Media.Animation/RepeatBehaviorConverter.cs
+++ src/Runtime/Runtime/System.Windows.Media.Animation/RepeatBehaviorConverter.cs
@@ -21,7 +21,7 @@ namespace System.Windows.Media.Animation;
 /// </summary>
 internal sealed class RepeatBehaviorConverter : TypeConverter
 {
-    private static readonly char[] _iterationCharacter = new char[] { 'x' };
+    private static readonly char[] _iterationCharacter = ['x', 'X'];
 
     /// <summary>
     /// CanConvertFrom - Returns whether or not this class can convert from a given type
@@ -52,14 +52,13 @@ internal sealed class RepeatBehaviorConverter : TypeConverter
         if (stringValue != null)
         {
             stringValue = stringValue.Trim();
-            stringValue = stringValue.ToLowerInvariant();
 
-            if (stringValue == "forever")
+            if (string.Equals(stringValue, "Forever", StringComparison.OrdinalIgnoreCase))
             {
                 return RepeatBehavior.Forever;
             }
-            else if (stringValue.Length > 0
-                && stringValue[stringValue.Length - 1] == _iterationCharacter[0])
+            else if (stringValue.Length > 0 &&
+                     char.ToLowerInvariant(stringValue[stringValue.Length - 1]) == _iterationCharacter[0])
             {
                 string stringDoubleValue = stringValue.TrimEnd(_iterationCharacter);
 
