a79c417e90dc1d2f10c6f0d57c71d58ffc619acc
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2025-09-05 22:55:42 +0600

    chore: increase required XAML Designer version (#994)

--- src/Runtime/Runtime/Properties/AssemblyInfo.cs
+++ src/Runtime/Runtime/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Windows.Markup;
 
 [assembly: InternalsVisibleTo("Telerik.Windows.Controls.GridView")]
 
-[assembly: OpenSilver.XamlDesigner(6)]
+[assembly: OpenSilver.XamlDesigner(7)]
 
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Controls")]
