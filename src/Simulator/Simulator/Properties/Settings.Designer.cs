d4a3db344bc5f685ed6cb0c3c7cb01084d349d4a
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2025-09-08 16:22:41 +0600

    fix: make simulator respect web security config setting

--- src/Simulator/Simulator/Properties/Settings.Designer.cs
+++ src/Simulator/Simulator/Properties/Settings.Designer.cs
@@ -61,7 +61,7 @@ namespace OpenSilver.Simulator.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool IsBypassCORSErrors {
             get {
                 return ((bool)(this["IsBypassCORSErrors"]));
