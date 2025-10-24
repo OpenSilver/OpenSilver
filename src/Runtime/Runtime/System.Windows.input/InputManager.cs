68c8bb7b9c2f3d64120e3886e625490f9611e5c8
barjonp <barjon.pierre@gmail.com>
2025-10-24 12:39:31 +0200

    fix: prevent null reference exception in UIElement.CaptureMouse()

--- src/Runtime/Runtime/System.Windows.input/InputManager.cs
+++ src/Runtime/Runtime/System.Windows.input/InputManager.cs
@@ -158,11 +158,11 @@ internal sealed class InputManager
 
     internal bool CaptureMouse(UIElement uie)
     {
-        if (Pointer.Captured is null && _mouseLeftDown)
+        if (Pointer.Captured is null && _mouseLeftDown && uie.OuterDiv is { } outerDiv)
         {
             Pointer.Captured = uie;
 
-            string sDiv = OpenSilver.Interop.GetVariableStringForJS(uie.OuterDiv);
+            string sDiv = OpenSilver.Interop.GetVariableStringForJS(outerDiv);
             OpenSilver.Interop.ExecuteJavaScriptVoid($"document.inputManager.captureMouse({sDiv});");
 
             return true;
