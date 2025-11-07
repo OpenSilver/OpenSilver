de79c8768aa4784cf4768c0cf4a75fe0ff22cf85
giovanni-a <8248552+giovanni-a@users.noreply.github.com>
2025-11-07 09:25:58 +0100

    feat: add [assembly: InternalsVisibleTo("OpenSilver.MauiSyntax")]

--- src/Runtime/Runtime/Properties/AssemblyInfo.cs
+++ src/Runtime/Runtime/Properties/AssemblyInfo.cs
@@ -19,6 +19,7 @@ using System.Windows.Markup;
 [assembly: InternalsVisibleTo("Runtime.OpenSilver.Tests")]
 [assembly: InternalsVisibleTo("OpenSilver.ControlsKit.FastControls")]
 [assembly: InternalsVisibleTo("XRSharp")]
+[assembly: InternalsVisibleTo("OpenSilver.MauiSyntax")]
 
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml", "System.Windows.Markup")] // This is used for example in the {x:Static ...} markup extension.
 
