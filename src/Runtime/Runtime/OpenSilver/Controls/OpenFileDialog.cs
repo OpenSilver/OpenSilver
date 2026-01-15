0842a929fbd0f5df065d6441bdcf8b1ef2f68ae3
thiagoftanaka <thiago.tanaka@userware.dev>
2026-01-15 15:56:42 -0300

    chore: add FilterIndex and InitialDirectory stubs to OpenFileDialog

--- src/Runtime/Runtime/OpenSilver/Controls/OpenFileDialog.cs
+++ src/Runtime/Runtime/OpenSilver/Controls/OpenFileDialog.cs
@@ -159,6 +159,32 @@ public sealed class OpenFileDialog
     public IEnumerable<MemoryFileInfo> Files => _files;
 
     /// <summary>
+    /// Gets or sets the index of the selected item in the <see cref="OpenFileDialog"/>
+    /// filter drop-down list.
+    /// </summary>
+    /// <returns>
+    /// The index of the selected item in the <see cref="OpenFileDialog"/>
+    /// filter drop-down list. The default is 1.
+    /// </returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// The filter index is less than 1.
+    /// </exception>
+    [NotImplemented]
+    public int FilterIndex { get; set; }
+
+    /// <summary>
+    /// Gets or sets the directory displayed when the dialog starts.
+    /// </summary>
+    /// <returns>
+    /// The directory displayed when the dialog starts. The default is an empty string.
+    /// </returns>
+    /// <exception cref="ArgumentException">
+    /// The directory specified is not a valid file path.
+    /// </exception>
+    [NotImplemented]
+    public string InitialDirectory { get; set; }
+
+    /// <summary>
     /// Opens the default browser file dialog. This returns a Task, differently from Silverlight.
     /// This is because it is not possible to wait for the dialog to conclude, since the process is single-threaded.
     /// </summary>
