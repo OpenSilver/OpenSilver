fc33a739e7fd97be72239fbeb7df91a813d13c01
barjonp <barjon.pierre@gmail.com>
2026-02-24 11:25:42 +0100

    chore: update `restore-packages-opensilver.bat`

--- restore-packages-opensilver.bat
+++ restore-packages-opensilver.bat
@@ -1,4 +1,4 @@
 %~dp0\build\nuget.exe install OpenSilver -Version 3.3.0 -OutputDirectory %~dp0\src\packages -Source https://api.nuget.org/v3/index.json
 %~dp0\build\nuget.exe install OpenSilver.WebAssembly -Version 3.3.0 -OutputDirectory %~dp0\src\packages -Source https://api.nuget.org/v3/index.json
 %~dp0\build\nuget.exe install OpenSilver.Simulator -Version 3.3.0 -OutputDirectory %~dp0\src\packages -Source https://api.nuget.org/v3/index.json
-dotnet workload restore %~dp0\src\OpenSilver.sln
+dotnet workload restore %~dp0\src\OpenSilver.slnx
