6ce5d60f441e7f26009c206b642fdd42972ec35e
Andrey Arekhva <a.arekhva@gmail.com>
2026-04-27 17:05:00 +0300

    Ensure event handlers are initialized before property sets

--- src/Compiler/Compiler/2_ConvertingXamlToCSharp/GeneratingCSharpCode.Pass2.cs
+++ src/Compiler/Compiler/2_ConvertingXamlToCSharp/GeneratingCSharpCode.Pass2.cs
@@ -534,6 +534,8 @@ namespace OpenSilver.Compiler
                     parameters.AppendLine($"{XamlDesignerBridgeClass}.SetFilePath({elementUid}, @\"{_sourceFile}\");");
                 }
 
+                var deferredInitializers = new List<Action>();
+
                 foreach (XAttribute attribute in element.Attributes())
                 {
                     //-------------
@@ -651,117 +653,124 @@ namespace OpenSilver.Compiler
                         case MemberKind.Property:
                         case MemberKind.Field:
                             {
-                                string value = null;
+                                deferredInitializers.Add(() =>
+                                {
+                                    string value = null;
 
-                                if (elementType == $"{KnownNamespaces.SystemWindows}.Setter")
-                                {
-                                    value = memberName switch
+                                    if (elementType == $"{KnownNamespaces.SystemWindows}.Setter")
                                     {
-                                        "Property" => GenerateCodeForSetterProperty(attribute),
-                                        "Value" => GenerateCodeForSetterValue(attribute),
-                                        "TargetName" => _settings.SystemTypes.ConvertToString(attributeValue),
-                                        _ => throw new XamlParseException(
-                                            "The '<Setter />' element cannot have attributes other than 'Property', 'Value' and 'TargetName'.",
-                                            element),
-                                    };
-                                }
-                                else if (elementType == $"{KnownNamespaces.SystemWindows}.Trigger")
-                                {
-                                    value = memberName switch
-                                    {
-                                        "Property" => GenerateCodeForTriggerProperty(attribute),
-                                        "Value" => GenerateCodeForTriggerValue(attribute),
-                                        "SourceName" => _settings.SystemTypes.ConvertToString(attributeValue),
-                                        _ => throw new XamlParseException(
-                                            "The '<Trigger />' element cannot have attributes other than 'Property', 'Value' and 'SourceName'.",
-                                            element),
-                                    };
-                                }
-                                else if (elementType == $"{KnownNamespaces.SystemWindows}.Condition")
-                                {
-                                    value = memberName switch
-                                    {
-                                        "Property" => GenerateCodeForConditionProperty(attribute),
-                                        "Value" => GenerateCodeForConditionValue(attribute),
-                                        "SourceName" => _settings.SystemTypes.ConvertToString(attributeValue),
-                                        _ => throw new XamlParseException(
-                                            "The '<Condition />' element cannot have attributes other than 'Property', 'Value' and 'SourceName'.",
-                                            element),
-                                    };
-                                }
-                                else if (elementType == $"{KnownNamespaces.SystemWindowsData}.Binding" && memberName == "Path")
-                                {
-                                    if (TryResolvePathForBinding(attributeValue, element, attribute, out string resolvedPath))
-                                    {
-                                        string xamlPath = _settings.SystemTypes.ConvertToString(resolvedPath);
-                                        parameters.AppendLine($"{elementUid}.XamlPath = {xamlPath};");
+                                        value = memberName switch
+                                        {
+                                            "Property" => GenerateCodeForSetterProperty(attribute),
+                                            "Value" => GenerateCodeForSetterValue(attribute),
+                                            "TargetName" => _settings.SystemTypes.ConvertToString(attributeValue),
+                                            _ => throw new XamlParseException(
+                                                "The '<Setter />' element cannot have attributes other than 'Property', 'Value' and 'TargetName'.",
+                                                element),
+                                        };
                                     }
-
-                                    value = GenerateCodeForInstantiatingAttributeValue(
-                                        memberName,
-                                        memberReference,
-                                        declaringType,
-                                        memberType,
-                                        elementTypeDefinition,
-                                        attributeValue,
-                                        element,
-                                        attribute);
-                                }
-                                else if (elementType == $"{KnownNamespaces.SystemWindows}.TemplateBindingExtension" && memberName == "Path")
-                                {
-                                    ResolvePathForTemplateBinding(attributeValue, element, out TypeDefinition ownerType, out string propertyName);
-                                    parameters.AppendLine(
-                                        $"{elementUid}.DependencyPropertyName = {_settings.SystemTypes.ConvertToString(propertyName)};");
-
-                                    if (ownerType is not null)
+                                    else if (elementType == $"{KnownNamespaces.SystemWindows}.Trigger")
                                     {
+                                        value = memberName switch
+                                        {
+                                            "Property" => GenerateCodeForTriggerProperty(attribute),
+                                            "Value" => GenerateCodeForTriggerValue(attribute),
+                                            "SourceName" => _settings.SystemTypes.ConvertToString(attributeValue),
+                                            _ => throw new XamlParseException(
+                                                "The '<Trigger />' element cannot have attributes other than 'Property', 'Value' and 'SourceName'.",
+                                                element),
+                                        };
+                                    }
+                                    else if (elementType == $"{KnownNamespaces.SystemWindows}.Condition")
+                                    {
+                                        value = memberName switch
+                                        {
+                                            "Property" => GenerateCodeForConditionProperty(attribute),
+                                            "Value" => GenerateCodeForConditionValue(attribute),
+                                            "SourceName" => _settings.SystemTypes.ConvertToString(attributeValue),
+                                            _ => throw new XamlParseException(
+                                                "The '<Condition />' element cannot have attributes other than 'Property', 'Value' and 'SourceName'.",
+                                                element),
+                                        };
+                                    }
+                                    else if (elementType == $"{KnownNamespaces.SystemWindowsData}.Binding" && memberName == "Path")
+                                    {
+                                        if (TryResolvePathForBinding(attributeValue, element, attribute, out string resolvedPath))
+                                        {
+                                            string xamlPath = _settings.SystemTypes.ConvertToString(resolvedPath);
+                                            parameters.AppendLine($"{elementUid}.XamlPath = {xamlPath};");
+                                        }
+
+                                        value = GenerateCodeForInstantiatingAttributeValue(
+                                            memberName,
+                                            memberReference,
+                                            declaringType,
+                                            memberType,
+                                            elementTypeDefinition,
+                                            attributeValue,
+                                            element,
+                                            attribute);
+                                    }
+                                    else if (elementType == $"{KnownNamespaces.SystemWindows}.TemplateBindingExtension" && memberName == "Path")
+                                    {
+                                        ResolvePathForTemplateBinding(attributeValue, element, out TypeDefinition ownerType, out string propertyName);
                                         parameters.AppendLine(
-                                            $"{elementUid}.DependencyPropertyOwnerType = typeof(global::{_settings.TypeReferenceHelper.ConvertToString(ownerType)});");
+                                            $"{elementUid}.DependencyPropertyName = {_settings.SystemTypes.ConvertToString(propertyName)};");
+
+                                        if (ownerType is not null)
+                                        {
+                                            parameters.AppendLine(
+                                                $"{elementUid}.DependencyPropertyOwnerType = typeof(global::{_settings.TypeReferenceHelper.ConvertToString(ownerType)});");
+                                        }
+
+                                        value = null;
                                     }
-
-                                    value = null;
-                                }
-                                else
-                                {
-                                    value = GenerateCodeForInstantiatingAttributeValue(
-                                        memberName,
-                                        memberReference,
-                                        declaringType,
-                                        memberType,
-                                        elementTypeDefinition,
-                                        attributeValue,
-                                        element,
-                                        attribute);
-                                }
-
-                                if (value is not null)
-                                {
-                                    using (parameters.CreateLineScope(attribute))
+                                    else
                                     {
-                                        parameters.AppendLine($"{elementUid}.{memberName} = {value};");
+                                        value = GenerateCodeForInstantiatingAttributeValue(
+                                            memberName,
+                                            memberReference,
+                                            declaringType,
+                                            memberType,
+                                            elementTypeDefinition,
+                                            attributeValue,
+                                            element,
+                                            attribute);
                                     }
-                                }
+
+                                    if (value is not null)
+                                    {
+                                        using (parameters.CreateLineScope(attribute))
+                                        {
+                                            parameters.AppendLine($"{elementUid}.{memberName} = {value};");
+                                        }
+                                    }
+                                });
+
                             }
                             break;
 
                         case MemberKind.AttachedPropertySet:
                             {
-                                string ownerType = _settings.TypeReferenceHelper.ConvertToString(declaringType);
-                                string value = GenerateCodeForInstantiatingAttributeValue(
-                                    memberName,
-                                    memberReference,
-                                    declaringType,
-                                    memberType,
-                                    elementTypeDefinition,
-                                    attributeValue,
-                                    element,
-                                    attribute);
-
-                                using (parameters.CreateLineScope(attribute))
+                                deferredInitializers.Add(() =>
                                 {
-                                    parameters.AppendLine(
-                                        $"global::{ownerType}.Set{memberName}({elementUid}, {value});");
-                                }
+                                    string ownerType = _settings.TypeReferenceHelper.ConvertToString(declaringType);
+                                    string value = GenerateCodeForInstantiatingAttributeValue(
+                                        memberName,
+                                        memberReference,
+                                        declaringType,
+                                        memberType,
+                                        elementTypeDefinition,
+                                        attributeValue,
+                                        element,
+                                        attribute);
+
+                                    using (parameters.CreateLineScope(attribute))
+                                    {
+                                        parameters.AppendLine(
+                                            $"global::{ownerType}.Set{memberName}({elementUid}, {value});");
+                                    }
+                                });
                             }
                             break;
 
@@ -796,6 +805,11 @@ namespace OpenSilver.Compiler
                                     attribute);
                             }
                     }
+                }
+
+                foreach (var initializer in deferredInitializers)
+                {
+                    initializer();
                 }
             }
 
---
+++ src/Compiler/Tests/Compiler.Tests/XamlEventOrderingTest.cs
@@ -0,0 +1,109 @@
+﻿/*===================================================================================
+*
+*   Copyright (c) Userware/OpenSilver.net
+*
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*
+\*====================================================================================*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Experimental;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mono.Cecil;
+using OpenSilver.Compiler;
+
+namespace Compiler.Tests;
+
+[TestClass]
+public class XamlEventOrderingTest
+{
+    [TestMethod]
+    public void Convert_Should_Connect_Event_Handlers_Before_Setting_Properties()
+    {
+        using var inspector = new AssembliesInspector("Compiler.Tests", SupportedLanguage.CSharp);
+        inspector.LoadAssembly(typeof(IList).Assembly.Location);
+        LoadAssemblyAndDependencies(inspector, typeof(ClassWithMembers).Assembly.GetName().Name + ".dll");
+
+        var settings = new ConversionSettings(
+            "Compiler.Tests",
+            inspector,
+            new CoreTypesConverterCS(inspector, "Compiler.Tests"),
+            SystemTypesHelper.CSharp,
+            TypeReferenceHelper.CSharp,
+            XamlPreprocessorOptions.Auto);
+
+        string xaml = """
+<controls:UserControl x:Class="Compiler.Tests.EventOrderingPage"
+                      xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+                      xmlns:controls="clr-namespace:System.Windows.Controls;assembly=OpenSilver">
+    <controls:RadioButton x:Name="InitialRadioButton"
+                          IsChecked="True"
+                          Checked="InitialRadioButton_Checked" />
+</controls:UserControl>
+""";
+
+        string generatedCode = ConvertingXamlToCSharp.Convert(
+            xaml,
+            "EventOrderingPage.xaml",
+            "EventOrderingPage.xaml",
+            settings,
+            isFirstPass: false);
+
+        int nameConnectionIndex = generatedCode.IndexOf("XamlContext_SetConnectionId", StringComparison.Ordinal);
+        int checkedConnectionIndex = generatedCode.IndexOf("XamlContext_SetConnectionId", nameConnectionIndex + 1, StringComparison.Ordinal);
+        int checkedHandlerIndex = generatedCode.IndexOf("Checked += this.InitialRadioButton_Checked", StringComparison.Ordinal);
+        int isCheckedAssignmentIndex = generatedCode.IndexOf(".IsChecked = true;", StringComparison.Ordinal);
+
+        Assert.IsTrue(nameConnectionIndex >= 0, generatedCode);
+        Assert.IsTrue(checkedConnectionIndex >= 0, generatedCode);
+        Assert.IsTrue(checkedHandlerIndex >= 0, generatedCode);
+        Assert.IsTrue(isCheckedAssignmentIndex >= 0, generatedCode);
+        Assert.IsTrue(nameConnectionIndex < checkedConnectionIndex, generatedCode);
+        Assert.IsTrue(checkedConnectionIndex < isCheckedAssignmentIndex, generatedCode);
+    }
+
+    private static void LoadAssemblyAndDependencies(AssembliesInspector inspector, string assemblyPath)
+    {
+        var loadedAssemblyNames = new HashSet<string>();
+        var resolver = new DefaultAssemblyResolver();
+        var queue = new Queue<AssemblyDefinition>();
+
+        queue.Enqueue(inspector.LoadAssembly(assemblyPath));
+        while (queue.Count > 0)
+        {
+            var assembly = queue.Dequeue();
+            loadedAssemblyNames.Add(assembly.Name.Name);
+
+            foreach (var referencedAssembly in assembly.MainModule.AssemblyReferences)
+            {
+                if (loadedAssemblyNames.Contains(referencedAssembly.Name))
+                {
+                    continue;
+                }
+
+                string assemblyFullPath = Path.Combine(Path.GetDirectoryName(assemblyPath) ?? "", referencedAssembly.Name + ".dll");
+                if (File.Exists(assemblyFullPath))
+                {
+                    queue.Enqueue(inspector.LoadAssembly(assemblyFullPath));
+                    continue;
+                }
+
+                try
+                {
+                    queue.Enqueue(resolver.Resolve(referencedAssembly));
+                }
+                catch
+                {
+                    // Some framework assemblies are not needed for this XAML generation test.
+                }
+            }
+        }
+    }
+}
