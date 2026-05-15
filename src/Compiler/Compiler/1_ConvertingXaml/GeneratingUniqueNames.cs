ce5fab39f5a84cbedc3d57b6f2a9bae4cdc73586
Andrey Arekhva <a.arekhva@gmail.com>
2026-05-15 12:01:19 +0300

    perf(Compiler): reduce length of generated unique names (#1070)

--- src/Compiler/Compiler/1_ConvertingXaml/GeneratingUniqueNames.cs
+++ src/Compiler/Compiler/1_ConvertingXaml/GeneratingUniqueNames.cs
@@ -1,64 +1,46 @@
 ﻿
-
 /*===================================================================================
 * 
-*   Copyright (c) Userware (OpenSilver.net, CSHTML5.com)
+*   Copyright (c) Userware/OpenSilver.net
 *      
-*   This file is part of both the OpenSilver Compiler (https://opensilver.net), which
-*   is licensed under the MIT license (https://opensource.org/licenses/MIT), and the
-*   CSHTML5 Compiler (http://cshtml5.com), which is dual-licensed (MIT + commercial).
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
 *   
 *   As stated in the MIT license, "the above copyright notice and this permission
 *   notice shall be included in all copies or substantial portions of the Software."
 *  
 \*====================================================================================*/
 
-using System;
 using System.Xml.Linq;
 
-namespace OpenSilver.Compiler
+namespace OpenSilver.Compiler;
+
+internal static class GeneratingUniqueNames
 {
-    internal static class GeneratingUniqueNames
+    // Note: we use '.' to make sure this attribute is not colliding with
+    // any property defined by the user.
+    public static readonly XName UniqueNameAttribute = GeneratingCode.xNamespace.GetName("__.UniqueName.__");
+
+    public static void ProcessDocument(XDocument doc, ConversionSettings settings)
     {
-        // Note: we use '.' to make sure this attribute is not colliding with
-        // any property defined by the user.
-        public static readonly XName UniqueNameAttribute = GeneratingCode.xNamespace.GetName("__.UniqueName.__");
+        TraverseNextElement(doc.Root, settings);
+    }
 
-        public static void ProcessDocument(XDocument doc)
+    private static void TraverseNextElement(XElement element, ConversionSettings settings)
+    {
+        // If the current element is an object (rather than a property)
+        if (!XamlParser.IsMemberNode(element))
         {
-            TraverseNextElement(doc.Root);
+            // Generate unique name
+            string uniqueName = settings.NameProvider.GetName(element.Name.LocalName);
+
+            // Assign unique name
+            element.SetAttributeValue(UniqueNameAttribute, uniqueName);
         }
 
-        private static void TraverseNextElement(XElement currentElement)
+        foreach (var childElements in element.Elements())
         {
-            // If the current element is an object (rather than a property)
-            if (!currentElement.Name.LocalName.Contains("."))
-            {
-                // Generate unique name
-                string uniqueName = GenerateUniqueName(currentElement.Name.LocalName);
-
-                // Assign unique name
-                currentElement.SetAttributeValue(UniqueNameAttribute, uniqueName);
-            }
-
-            // Recursion:
-            foreach (var childElements in currentElement.Elements())
-            {
-                TraverseNextElement(childElements);
-            }
-        }
-
-        internal static string GenerateUniqueName(string str)
-        {
-            ReadOnlySpan<char> prefix = str.AsSpan();
-
-            if (prefix.Length > 30)
-            {
-                prefix = prefix.Slice(0, 30);
-            }
-
-            // Because of f# warning, makes the first letter lower case
-            return $"{char.ToLower(prefix[0])}{prefix.Slice(1)}_{Guid.NewGuid():N}"; // Example: Button_4541C363579C48A981219C392BF8ACD5
+            TraverseNextElement(childElements, settings);
         }
     }
 }
