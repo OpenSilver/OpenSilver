1801d5732bb99432c5c8ddb75f0892cdd4ef2bd1
barjonp <barjon.pierre@gmail.com>
2026-06-18 14:36:28 +0200

    Use ConcurrentDictionary

--- src/Runtime/Runtime/PublicAPI/Interop/JavascriptCallback.cs
+++ src/Runtime/Runtime/PublicAPI/Interop/JavascriptCallback.cs
@@ -24,7 +24,7 @@ namespace CSHTML5.Internal
 {
     internal sealed class JavaScriptCallback : IJavaScriptConvertible, IDisposable
     {
-        private static readonly SynchronyzedStore<JavaScriptCallback> _store = new(256);
+        private static readonly SynchronyzedStore<JavaScriptCallback> _store = new();
 
         private readonly int _id;
         private readonly Delegate _callback;
